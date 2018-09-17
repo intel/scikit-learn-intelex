693f8c9d288131e9505efc09b27877bf94424225
Frank Schlimbach <frank.schlimbach@intel.com>
2018-09-17 05:48:38 -0500

    adding examples

---
+++ examples/correlation_distance_batch.py
@@ -0,0 +1,43 @@
+#*******************************************************************************
+# Copyright 2014-2018 Intel Corporation
+# All Rights Reserved.
+#
+# This software is licensed under the Apache License, Version 2.0 (the
+# "License"), the following terms apply:
+#
+# You may not use this file except in compliance with the License.  You may
+# obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#*******************************************************************************
+
+# daal4py correlation distance example for shared memory systems
+
+import daal4py as d4p
+import numpy as np
+import os
+
+# let's try to use pandas' fast csv reader
+try:
+    import pandas
+    read_csv = lambda f, c: pandas.read_csv(f, usecols=c, delimiter=',').values
+except:
+    # fall back to numpy loadtxt
+    read_csv = lambda f, c: np.loadtxt(f, usecols=c, delimiter=',')
+
+
+def main():
+    # Create algorithm to compute correlation distance (no parameters)
+    algorithm = d4p.correlation_distance()
+
+    # Computed correlation distance
+    return algorithm.compute(os.path.join('data', 'batch', 'distance.csv'))
+
+
+if __name__ == "__main__":
+    res = main()
