54b5fbaa15e8eb065a8a7f293cdf8ee1960343f3
Frank Schlimbach <frank.schlimbach@intel.com>
2018-09-21 09:00:33 -0500

    disabling distribu^Cd mode in travis

--- examples/run_examples.py
+++ examples/run_examples.py
@@ -34,13 +34,15 @@ if 8 * struct.calcsize('P') == 32:
 else:
     logdir = jp(exdir, '_results', 'intel64')
 
-def get_exe_cmd(ex):
+def get_exe_cmd(ex, nodist):
     if 'batch' in ex:
         return '"' + sys.executable + '" "' + ex + '"'
+    elif nodist:
+        return None
     else:
         return 'mpirun -n 4 -genv DIST_CNC=MPI "' + sys.executable + '" "' + ex + '"'
 
-def run_all():
+def run_all(nodist=False):
     success = 0
     n = 0
     if not os.path.exists(logdir):
@@ -52,26 +54,31 @@ def run_all():
                 logfn = jp(logdir, script.replace('.py', '.res'))
                 with open(logfn, 'w') as logfile:
                     print('\n##### ' + jp(dirpath, script))
-                    execute_string = get_exe_cmd(jp(dirpath, script))
-                    proc = subprocess.Popen(execute_string if IS_WIN else ['/bin/bash', '-c', execute_string],
-                                            stdout=subprocess.PIPE,
-                                            stderr=subprocess.STDOUT,
-                                            shell=(True if IS_WIN else False))
-                    out = proc.communicate()[0]
-                    logfile.write(out.decode('ascii'))
-                    if proc.returncode:
-                        print(out)
-                        print(strftime("%H:%M:%S", gmtime()) + '\tFAILED\t' + script + '\twith errno\t' + str(proc.returncode))
+                    execute_string = get_exe_cmd(jp(dirpath, script), nodist)
+                    if execute_string:
+                        proc = subprocess.Popen(execute_string if IS_WIN else ['/bin/bash', '-c', execute_string],
+                                                stdout=subprocess.PIPE,
+                                                stderr=subprocess.STDOUT,
+                                                shell=(True if IS_WIN else False))
+                        out = proc.communicate()[0]
+                        logfile.write(out.decode('ascii'))
+                        if proc.returncode:
+                            print(out)
+                            print(strftime("%H:%M:%S", gmtime()) + '\tFAILED\t' + script + '\twith errno\t' + str(proc.returncode))
+                        else:
+                            success += 1
+                            print(strftime("%H:%M:%S", gmtime()) + '\tPASSED\t' + script)
                     else:
                         success += 1
-                        print(strftime("%H:%M:%S", gmtime()) + '\tPASSED\t' + script)
+                        print(strftime("%H:%M:%S", gmtime()) + '\tSKIPPED\t' + script)
+
     if success != n:
-        print('{}/{} examples passed, {} failed'.format(success,n, n - success))
+        print('{}/{} examples passed/skipped, {} failed'.format(success,n, n - success))
         print('Error(s) occured. Logs can be found in ' + logdir)
         return 4711
     else:
-        print('{}/{} examples passed'.format(success,n))
+        print('{}/{} examples passed/skipped'.format(success,n))
         return 0
 
 if __name__ == '__main__':
-    sys.exit(run_all())
+    sys.exit(run_all('nodist' in sys.argv))
