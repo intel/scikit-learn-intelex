9751b14acd39ed5e522194b67738c30962bfd380
Frank Schlimbach <frank.schlimbach@intel.com>
2018-09-24 02:51:03 -0500

    no <type_traits>

--- src/daal4py.h
+++ src/daal4py.h
@@ -35,7 +35,6 @@ using daal::step3Master;
 #include <vector>
 #include <limits>
 #include <string>
-#include <type_traits>
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include <numpy/arrayobject.h>
 
