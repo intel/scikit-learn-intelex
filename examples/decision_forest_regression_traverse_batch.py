0c1d94f2183f1bf43867026c18acf45175d2a40d
Frank Schlimbach <frank.schlimbach@intel.com>
2018-10-17 13:42:55 +0200

    check version

--- examples/decision_forest_regression_traverse_batch.py
+++ examples/decision_forest_regression_traverse_batch.py
@@ -43,6 +43,11 @@ def printTree(nodes, values):
 
 
 if __name__ == "__main__":
+    from daal4py import __daal_link_version__ as dv
+    daal_version = tuple(map(int, (dv[0:4], dv[4:8])))
+    if daal_version < (2019, 1):
+        print("Need Intel DAAL 2019.1 or later")
+        return 0
     # First get our result and model
     (train_result, _, _) = df_regression()
     # Retrieve and print all trees; encoded as in sklearn.ensamble.tree_.Tree
