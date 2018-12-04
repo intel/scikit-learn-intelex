9b0a75c1bf4eb80dc74fe52f26ffe08210ba2aec
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2018-12-04 16:12:41 -0600

    renamed decision forest class to drop Daal prefix

--- daal4py/sklearn/__init__.py
+++ daal4py/sklearn/__init__.py
@@ -15,10 +15,10 @@
 # limitations under the License.
 #******************************************************************************/
 
-from .decision_forest import (DaalRandomForestClassifier, DaalRandomForestRegressor)
+from .decision_forest import (RandomForestClassifier, RandomForestRegressor)
 from .monkeypatch.dispatcher import enable as patch_sklearn
 from .monkeypatch.dispatcher import disable as unpatch_sklearn
 from .monkeypatch.dispatcher import _patch_names as sklearn_patch_names
 
-__all__ = ["DaalRandomForestClassifier", "DaalRandomForestRegressor", 
+__all__ = ["RandomForestClassifier", "RandomForestRegressor", 
            "patch_sklearn", "unpatch_sklearn", "sklearn_patch_names"]
