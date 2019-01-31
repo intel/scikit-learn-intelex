0739d4e7ffb53f6b723d708a543573f2c6da6ce9
Frank Schlimbach <frank.schlimbach@intel.com>
2019-01-31 06:29:08 -0600

    disabling qr validation; computing assignments in kmeans_spmd; no artifacts

--- daal4py/__init__.py
+++ daal4py/__init__.py
@@ -1,2 +1,8 @@
-from _daal4py import *
-from _daal4py import __version__, __daal_link_version__, __daal_run_version__
+try:
+    from _daal4py import *
+    from _daal4py import __version__, __daal_link_version__, __daal_run_version__
+except ImportError as e:
+    s = str(e)
+    print(s)
+    if 'libfabric' in s:
+        print('Activating your conda environment or sourcing mpivars.sh/psxevars.sh may solve the issue.')
