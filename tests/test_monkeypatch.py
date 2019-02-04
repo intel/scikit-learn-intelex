92bf8dcb0db8da66655d316675e7e155e755364b
Frank Schlimbach <frank.schlimbach@intel.com>
2019-02-04 04:03:08 -0600

    using unittest

--- tests/test_monkeypatch.py
+++ tests/test_monkeypatch.py
@@ -1,28 +1,21 @@
-
+import unittest
 import daal4py.sklearn
 
-def assert_is_patched(mod, cls_name):
-    class_module = getattr(mod, cls_name).__module__
-    assert class_module.startswith('daal4py')
 
-def test_monkey_patching():
-    _tokens = daal4py.sklearn.sklearn_patch_names()
-    assert isinstance(_tokens, list) and len(_tokens) > 0
-    for t in _tokens:
-        daal4py.sklearn.unpatch_sklearn(t)
-    for t in _tokens:
-        daal4py.sklearn.patch_sklearn(t)
+class MonkeyPTest(unittest.TestCase):
+    def test_monkey_patching(self):
+        _tokens = daal4py.sklearn.sklearn_patch_names()
+        self.assertTrue(isinstance(_tokens, list) and len(_tokens) > 0)
+        for t in _tokens:
+            daal4py.sklearn.unpatch_sklearn(t)
+        for t in _tokens:
+            daal4py.sklearn.patch_sklearn(t)
 
-    try:
         import sklearn
-        import sklearn.decomposition as skde
-        assert_is_patched(skde, 'PCA')
-        import sklearn.linear_model as sklm
-        assert_is_patched(sklm, 'Ridge')
-        assert_is_patched(sklm, 'LinearRegression')
-        import sklearn.cluster as skcl
-        assert_is_patched(skcl, 'KMeans')
-        import sklearn.svm as sksvm
-        assert_is_patched(sksvm, 'SVC')
-    except ImportError:
-        pass
+        for a in [(sklearn.decomposition, 'PCA'),
+                  (sklearn.linear_model, 'Ridge'),
+                  (sklearn.linear_model, 'LinearRegression'),
+                  (sklearn.cluster, 'KMeans'),
+                  (sklearn.svm, 'SVC'),]:
+            class_module = getattr(a[0], a[1]).__module__
+            self.assertTrue(class_module.startswith('daal4py'))
