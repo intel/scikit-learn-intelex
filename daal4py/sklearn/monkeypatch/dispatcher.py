34ab4aa03c1778b2a02e8f5a0bd3a5aeda3adc15
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2019-05-14 15:17:45 -0500

    try to fix LooseVersion comparison

--- daal4py/sklearn/monkeypatch/dispatcher.py
+++ daal4py/sklearn/monkeypatch/dispatcher.py
@@ -89,7 +89,7 @@ def do_unpatch(name):
 def enable(name=None):
     if LooseVersion(sklearn_version) < LooseVersion("0.20.0"):
         raise NotImplementedError("daal4py patches apply  for scikit-learn >= 0.20.0 only ...")
-    elif LooseVersion(sklearn_version) > LooseVersion("0.21"):
+    elif LooseVersion(sklearn_version) > LooseVersion("0.21.0"):
         warn_msg = ("daal4py {daal4py_version} has only been tested " +
                    "with scikit-learn 0.21.0, found version: {sklearn_version}")
         warnings.warn(warn_msg.format(daal4py_version=daal4py_version, sklearn_version=sklearn_version))
