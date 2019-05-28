29ac08ced648196427577069d1decb3d06d6c581
amakarye <alexander.makaryev@intel.com>
2019-05-28 03:17:16 -0500

    the try to implement map_reduce_star_plus

--- src/map_reduce_star.h
+++ src/map_reduce_star.h
@@ -53,4 +53,58 @@ public:
 
 } // namespace map_reduce_star {
 
+namespace map_reduce_star_plus {
+
+    template<typename Algo>
+    class map_reduce_star_plus
+    {
+    public:
+        template<typename ... Ts>
+        typename Algo::iomstep3Local_type::result_type
+        static map_reduce(Algo & algo, Ts& ... inputs)
+        {
+            int rank = MPI4DAAL::rank();
+            int nRanks = MPI4DAAL::nRanks();
+
+            // run step1 and gather all partial results
+            auto s1Res = algo.run_step1Local(inputs...);
+            // we need to replace in some way exact daal::algorithms::svd::outputOfStep1ForStep2 by common definition
+            auto s1OutForStep2 = s1Res->get(daal::algorithms::svd::outputOfStep1ForStep2);
+            auto s2InFromStep1 = MPI4DAAL::gather(rank, nRanks, s1OutForStep2);
+
+            typename Algo::iomstep2Master_type::result_type res;
+            const int S23TAG = 4004; //what it should be? unique?
+            daal::data_management::DataCollectionPtr inputOfStep3FromStep2;
+            if(rank == 0) {
+                res = algo.run_step2Master(s2InFromStep1);
+                // get intputs for step3 and send them to all processes
+                auto outputOfStep2ForStep3 = std::get<1>(res)->get(daal::algorithms::svd::outputOfStep2ForStep3);
+                inputOfStep3FromStep2 = daal::services::staticPointerCast<daal::data_management::DataCollection>((*outputOfStep2ForStep3)[0]);
+                for(size_t i = 1; i < nRanks; i++) {
+                    MPI4DAAL::send((*outputOfStep2ForStep3)[i], i, S23TAG);
+                }
+            } else {
+                inputOfStep3FromStep2 = MPI4DAAL::recv<daal::data_management::DataCollectionPtr>(0, S23TAG);
+            }
+
+            // run step3
+            auto inputOfStep3FromStep1 = s1Res->get(daal::algorithms::svd::outputOfStep1ForStep3);
+            auto step3Output = algo.run_step3Local(inputOfStep3FromStep1, inputOfStep3FromStep2);
+
+            // we need to return std::get<0>(res) (rightSingularMatrix and singularValues) and step3Output (leftSingularMatrix) at the same time
+            // and them must be available in Python...
+            return step3Output;
+        }
+
+        template<typename ... Ts>
+        static typename Algo::iomstep3Local_type::result_type
+        compute(Algo & algo, Ts& ... inputs)
+        {
+            MPI4DAAL::init();
+            return map_reduce(algo, get_table(inputs)...);
+        }
+    };
+
+} // namespace map_reduce_star_plus {
+
 #endif // _MAP_REDUCE_STAR_INCLUDED_
