14fcf8373d6073ff5c0497a7ea46f81fa6628779
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2019-06-05 11:28:50 -0500

    use defaults and conda-forge channels for python packages to stay closer to upstream CI configuration

--- .circleci/setup_env.sh
+++ .circleci/setup_env.sh
@@ -6,10 +6,11 @@ hash -r
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda install -q conda-build
-conda create -q -n bld --override-channels -c intel -c conda-forge python=3.6 numpy scipy pytest daal tbb pandas numpydoc
+conda create -n bld --override-channels -c intel daal daal-devel tbb
+conda install -q -n bld --override-channels -c defaults python=3.6 numpy scipy pytest pandas pyyaml joblib
 conda install -q -n bld -c defaults --override-channels --no-deps libgcc-ng libstdcxx-ng
+conda install -q -n bld -c conda-forge --override-channels numpydocs
 conda install -q -n bld -c conda-forge --override-channels --no-deps mpi libgfortran mpich
-conda install -q -n bld -c defaults pyyaml joblib
 gcc -v
 g++ -v
 head /proc/cpuinfo
