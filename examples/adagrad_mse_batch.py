d7fb6c441d5e9377addffa78d2c0c192bdf4daa1
amakarye <alexander.makaryev@intel.com>
2019-07-01 06:29:25 -0500

    changed examples

--- examples/adagrad_mse_batch.py
+++ examples/adagrad_mse_batch.py
@@ -40,7 +40,7 @@ def main(readcsv=read_csv, method='defaultDense'):
 
     # configure a MSE object
     mse_algo = d4p.optimization_solver_mse(nVectors)
-    mse_algo.setup(data, dep_data, None)
+    mse_algo.setup(data, dep_data)
 
     # configure an AdaGrad object
     lr = np.array([[1.0]], dtype=np.double)
