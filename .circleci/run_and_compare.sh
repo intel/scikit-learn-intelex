7d14f19b755e733624e616adf330dde617377d20
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2019-09-30 13:58:26 -0500

    update reg-expr pattern to adapt to new version of pytest used

--- .circleci/run_and_compare.sh
+++ .circleci/run_and_compare.sh
@@ -6,8 +6,9 @@ echo "-m daal4py -m pytest ${DESELECTED_TESTS} -q -ra --disable-warnings --pyarg
 # It is important for pytest to work in a separate test folder to not discover tests in ~/miniconda folder
 cd && ((python -m daal4py -m pytest ${DESELECTED_TESTS} -ra --disable-warnings --pyargs sklearn | tee ~/d4p.out) || true)
 cd && ((python -m pytest ${DESELECTED_TESTS} -ra --disable-warnings --pyargs sklearn | tee ~/skl.out) || true)
-export D4P=`grep -e '\d* passed.* second' ~/d4p.out`
-export SKL=`grep -e '\d* passed.* second' ~/skl.out`
+# extract status strings
+export D4P=`grep -E "=(\s\d*\w*,?)+ in .*\s=" ~/d4p.out`
+export SKL=`grep -E "=(\s\d*\w*,?)+ in .*\s=" ~/skl.out`
 tar cjf $1 ~/d4p.out ~/skl.out
 echo "Summary of patched run: " $D4P
 echo "Summary of unpatched run: " $SKL
