1c8a442dc75b0afbdd0bb11af8b5656869aa2a5a
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2019-12-20 10:34:08 -0600

    in anticipation of bugfix 0.22.1 release of sklearn

--- daal4py/sklearn/monkeypatch/dispatcher.py
+++ daal4py/sklearn/monkeypatch/dispatcher.py
@@ -100,7 +100,7 @@ def do_unpatch(name):
 def enable(name=None, verbose=True):
     if LooseVersion(sklearn_version) < LooseVersion("0.20.0"):
         raise NotImplementedError("daal4py patches apply  for scikit-learn >= 0.20.0 only ...")
-    elif LooseVersion(sklearn_version) > LooseVersion("0.22"):
+    elif LooseVersion(sklearn_version) > LooseVersion("0.22.1"):
         warn_msg = ("daal4py {daal4py_version} has only been tested " +
                     "with scikit-learn 0.22.0, found version: {sklearn_version}")
         warnings.warn(warn_msg.format(
