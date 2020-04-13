754dd2953463ce45ce1ec78772f034cd3dc44902
Petrov, Nikolay A <nikolay.a.petrov@intel.com>
2020-04-13 19:50:32 +0300

    Enable device rules for examples

--- examples/run_examples.py
+++ examples/run_examples.py
@@ -99,6 +99,7 @@ def get_exe_cmd(ex, nodist, nostream):
             return None
         if not check_device(req_device["sycl/" + os.path.basename(ex)], availabe_devices):
             return None
+            
     if os.path.dirname(ex).endswith("examples"):
         if not check_version(req_version[os.path.basename(ex)], daal_version):
             return None
