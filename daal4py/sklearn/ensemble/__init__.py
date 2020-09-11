e68c29a6dd5253e087171611339495c7626de103
Pavel Yakovlev <35104773+Pahandrovich@users.noreply.github.com>
2020-09-11 17:24:46 +0300

    Add Decision Forest wrapper (#276)

--- daal4py/sklearn/ensemble/__init__.py
+++ daal4py/sklearn/ensemble/__init__.py
@@ -2,8 +2,11 @@
 The 'daal4py.sklearn.ensemble' module implements daal4py-based 
 RandomForestClassifier and RandomForestRegressor classes.
 """
-
-from .decision_forest import (RandomForestClassifier, RandomForestRegressor)
+from daal4py.sklearn._utils import daal_check_version
+if daal_check_version((2020, 3), (2021, 9)):
+    from .forest import (RandomForestClassifier, RandomForestRegressor)
+else:
+    from .decision_forest import (RandomForestClassifier, RandomForestRegressor)
 from .GBTDAAL import (GBTDAALClassifier, GBTDAALRegressor)
 from .AdaBoostClassifier import AdaBoostClassifier
 
