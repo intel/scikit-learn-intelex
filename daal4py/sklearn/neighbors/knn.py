fd71f024168cbade56056625efe20718436f2c07
ntimakin <nikita.timakin@intel.com>
2020-09-22 10:07:36 +0300

    isinstance fix

--- daal4py/sklearn/neighbors/knn.py
+++ daal4py/sklearn/neighbors/knn.py
@@ -203,7 +203,7 @@ class KNeighborsMixin(BaseKNeighborsMixin):
 
 class KNeighborsClassifier(BaseKNeighborsClassifier, KNeighborsMixin):
     def fit(self, X, y):
-        X_incorrect_type = isinstance(X, NeighborsBase) or isinstance(X, BallTree) or isinstance(X, KDTree)
+        X_incorrect_type = isinstance(X, (NeighborsBase, BallTree,BallTree))
 
         if not X_incorrect_type:
             X, y = self._validate_data(X, y, accept_sparse="csr", multi_output=True)
