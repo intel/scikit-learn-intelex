efc3fa5e35385b9d2392d3a605edfa30aef15b19
OnlyDeniko <denis.kulandin@intel.com>
2020-09-24 11:43:06 +0300

    check average metrics in RF tests & format code using PEP8

--- daal4py/sklearn/ensemble/tests/test_decision_forest.py
+++ daal4py/sklearn/ensemble/tests/test_decision_forest.py
@@ -1,5 +1,5 @@
 #
-#*******************************************************************************
+# ****************************************************************************
 # Copyright 2020 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,55 +13,91 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#******************************************************************************/
+# ****************************************************************************
 
 from daal4py.sklearn._utils import daal_check_version
 import pytest
-import random
 import numpy as np
+import random
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import RandomForestClassifier as SKRandomForestClassifier
-from daal4py.sklearn.ensemble import RandomForestClassifier as D4PRandomForestClassifier
-from sklearn.ensemble import RandomForestRegressor as SKRandomForestRegressor
-from daal4py.sklearn.ensemble import RandomForestRegressor as D4PRandomForestRegressor
+from daal4py.sklearn.ensemble \
+    import RandomForestClassifier as D4PRandomForestClassifier
+from sklearn.ensemble \
+    import RandomForestRegressor as SKRandomForestRegressor
+from daal4py.sklearn.ensemble \
+    import RandomForestRegressor as D4PRandomForestRegressor
+
+N_TRIES = 10
 
 CLASS_WEIGHTS_IRIS = [
     {0: 0, 1: 0, 2: 0},
     {0: 0, 1: 1, 2: 1},
     {0: 1, 1: 2, 2: 3},
     {0: 10, 1: 5, 2: 4},
-    {0: random.uniform(1, 50), 1: random.uniform(1, 50), 2: random.uniform(1, 50)},
-    {0: random.uniform(50, 100), 1: random.uniform(50, 100), 2: random.uniform(50, 100)},
-    {0: random.uniform(1, 1000), 1: random.uniform(1, 1000), 2: random.uniform(1, 1000)},
-    {0: random.uniform(1, 10), 1: random.uniform(50, 100), 2: random.uniform(1, 100)},
-    {0: random.uniform(1, 10), 1: random.uniform(1, 100), 2: random.uniform(1, 1000)},
-    {0: random.uniform(1, 2000), 1: random.uniform(1, 2000), 2: random.uniform(1, 2000)},
+    {
+        0: random.uniform(1, 50),
+        1: random.uniform(1, 50),
+        2: random.uniform(1, 50),
+    },
+    {
+        0: random.uniform(50, 100),
+        1: random.uniform(50, 100),
+        2: random.uniform(50, 100),
+    },
+    {
+        0: random.uniform(1, 1000),
+        1: random.uniform(1, 1000),
+        2: random.uniform(1, 1000),
+    },
+    {
+        0: random.uniform(1, 10),
+        1: random.uniform(50, 100),
+        2: random.uniform(1, 100),
+    },
+    {
+        0: random.uniform(1, 10),
+        1: random.uniform(1, 100),
+        2: random.uniform(1, 1000),
+    },
+    {
+        0: random.uniform(1, 2000),
+        1: random.uniform(1, 2000),
+        2: random.uniform(1, 2000),
+    },
     {0: 50, 1: 50, 2: 50},
     'balanced',
 ]
+
 
 @pytest.mark.parametrize('weight', CLASS_WEIGHTS_IRIS)
 def test_classifier_class_weight_iris(weight):
     if daal_check_version((2020, 3)):
         check_classifier_class_weight_iris(weight)
 
+
 def check_classifier_class_weight_iris(weight, check_ratio=0.85):
-    X, y = load_iris(return_X_y=True)
-    X_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=31)  
-    
-    SK_model = SKRandomForestClassifier(class_weight=weight)
-    D4P_model = D4PRandomForestClassifier(class_weight=weight)
+    alg_results = []
+    for _ in range(N_TRIES):
+        X, y = load_iris(return_X_y=True)
+        X_train, x_test, y_train, y_test = \
+            train_test_split(X, y, test_size=0.33, random_state=31)
 
-    SK_predict = SK_model.fit(X_train, y_train).predict(x_test)
-    D4P_predict = D4P_model.fit(X_train, y_train).predict(x_test)
+        SK_model = SKRandomForestClassifier(class_weight=weight)
+        D4P_model = D4PRandomForestClassifier(class_weight=weight)
 
-    SK_accuracy = accuracy_score(SK_predict, y_test)
-    D4P_accuracy = accuracy_score(D4P_predict, y_test)
-    ratio = D4P_accuracy / SK_accuracy
-    assert ratio >= check_ratio
+        SK_predict = SK_model.fit(X_train, y_train).predict(x_test)
+        D4P_predict = D4P_model.fit(X_train, y_train).predict(x_test)
+
+        SK_accuracy = accuracy_score(SK_predict, y_test)
+        D4P_accuracy = accuracy_score(D4P_predict, y_test)
+        ratio = D4P_accuracy / SK_accuracy
+        alg_results.append(ratio)
+
+    assert np.mean(alg_results) >= check_ratio
 
 SAMPLE_WEIGHTS_IRIS = [
     (np.full_like(range(100), 0), 'Only 0'),
@@ -75,42 +111,63 @@ SAMPLE_WEIGHTS_IRIS = [
     (np.random.rayleigh(scale=1, size=100), 'Rayleigh distribution'),
 ]
 
+
 @pytest.mark.parametrize('weight', SAMPLE_WEIGHTS_IRIS)
 def test_classifier_sample_weight_iris(weight):
     if daal_check_version((2020, 3)):
         check_classifier_sample_weight(weight)
 
+
 def check_classifier_sample_weight(weight, check_ratio=0.9):
-    X, y = load_iris(return_X_y=True)
-    X_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=31)
-        
-    SK_model = SKRandomForestClassifier()
-    D4P_model = D4PRandomForestClassifier()
+    alg_results = []
+    for _ in range(N_TRIES):
+        X, y = load_iris(return_X_y=True)
+        X_train, x_test, y_train, y_test = \
+            train_test_split(X, y, test_size=0.33, random_state=31)
 
-    SK_predict = SK_model.fit(X_train, y_train, sample_weight=weight[0]).predict(x_test)
-    D4P_predict = D4P_model.fit(X_train, y_train, sample_weight=weight[0]).predict(x_test)
+        SK_model = SKRandomForestClassifier()
+        D4P_model = D4PRandomForestClassifier()
 
-    SK_accuracy = accuracy_score(SK_predict, y_test)
-    D4P_accuracy = accuracy_score(D4P_predict, y_test)
-    ratio = D4P_accuracy / SK_accuracy
-    assert ratio >= check_ratio, 'Failed testing sample weights, sample_weight_type = ' + weight[1]
+        SK_predict = SK_model.fit(X_train, y_train,
+                                  sample_weight=weight[0]).predict(x_test)
+        D4P_predict = D4P_model.fit(X_train, y_train,
+                                    sample_weight=weight[0]).predict(x_test)
+
+        SK_accuracy = accuracy_score(SK_predict, y_test)
+        D4P_accuracy = accuracy_score(D4P_predict, y_test)
+        ratio = D4P_accuracy / SK_accuracy
+        alg_results.append(ratio)
+
+    assert np.mean(alg_results) >= check_ratio, \
+        'Failed testing sample weights, sample_weight_type = ' + weight[1]
+
 
 @pytest.mark.parametrize('weight', SAMPLE_WEIGHTS_IRIS)
 def test_regressor_sample_weight_iris(weight):
     if daal_check_version((2020, 3)) and weight[1] != 'Only 0':
         check_regressor_sample_weight(weight)
 
+
 def check_regressor_sample_weight(weight, check_ratio=1.4):
-    X, y = load_iris(return_X_y=True)
-    X_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=31)
-        
-    SK_model = SKRandomForestRegressor()
-    D4P_model = D4PRandomForestRegressor()
+    alg_results = []
+    for _ in range(N_TRIES):
+        X, y = load_iris(return_X_y=True)
+        X_train, x_test, y_train, y_test = \
+            train_test_split(X, y, test_size=0.33, random_state=31)
 
-    SK_predict = SK_model.fit(X_train, y_train, sample_weight=weight[0]).predict(x_test)
-    D4P_predict = D4P_model.fit(X_train, y_train, sample_weight=weight[0]).predict(x_test)
+        SK_model = SKRandomForestRegressor()
+        D4P_model = D4PRandomForestRegressor()
 
-    SK_accuracy = mean_squared_error(SK_predict, y_test)
-    D4P_accuracy = mean_squared_error(D4P_predict, y_test)
-    ratio = D4P_accuracy / SK_accuracy
-    assert ratio <= check_ratio, 'Failed while testing regression sample weights, sample_weight_type = ' + weight[1]
+        SK_predict = SK_model.fit(X_train, y_train,
+                                  sample_weight=weight[0]).predict(x_test)
+        D4P_predict = D4P_model.fit(X_train, y_train,
+                                    sample_weight=weight[0]).predict(x_test)
+
+        SK_accuracy = mean_squared_error(SK_predict, y_test)
+        D4P_accuracy = mean_squared_error(D4P_predict, y_test)
+        ratio = D4P_accuracy / SK_accuracy
+        alg_results.append(ratio)
+
+    assert np.mean(alg_results) <= check_ratio, \
+        'Failed while testing regression sample weights, \
+        sample_weight_type = ' + weight[1]
--- daal4py/sklearn/neighbors/tests/test_kneighbors.py
+++ daal4py/sklearn/neighbors/tests/test_kneighbors.py
@@ -1,5 +1,5 @@
 #
-#*******************************************************************************
+# ****************************************************************************
 # Copyright 2020 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#******************************************************************************/
+# ****************************************************************************
 
 import pytest
 import numpy as np
@@ -27,9 +27,14 @@ ALGORITHMS = ['brute', 'kd_tree', 'auto']
 WEIGHTS = ['uniform', 'distance']
 KS = [1, 3, 7, 25]
 
-def make_dataset(n_samples=256, n_features=5, n_classes=2, test_size=0.5, shuffle=True):
-    x, y = make_classification(n_samples=n_samples, n_features=n_features, n_classes=n_classes, random_state=777)
-    return train_test_split(x, y, random_state=777, test_size=test_size, shuffle=shuffle)
+
+def make_dataset(n_samples=256, n_features=5, n_classes=2,
+                 test_size=0.5, shuffle=True):
+    x, y = make_classification(n_samples=n_samples, n_features=n_features,
+                               n_classes=n_classes, random_state=777)
+    return train_test_split(x, y, random_state=777,
+                            test_size=test_size, shuffle=shuffle)
+
 
 @pytest.mark.parametrize('distance', DISTANCES)
 @pytest.mark.parametrize('algorithm', ALGORITHMS)
@@ -39,8 +44,9 @@ def test_check_determenistic(distance, algorithm, weight, k):
     if daal_check_version((2020, 3)):
         check_determenistic(distance, algorithm, weight, k)
 
+
 def check_determenistic(distance, algorithm, weight, k):
-    N_TRIES = 5
+    N_TRIES = 10
 
     x_train, x_test, y_train, y_test = make_dataset()
 
@@ -53,11 +59,13 @@ def check_determenistic(distance, algorithm, weight, k):
         distances, indices = alg.kneighbors(x_test)
         labels = alg.predict(x_test)
         alg_results.append((distances, indices, labels))
-        
+
     for i in range(1, N_TRIES):
         for j, res in enumerate(alg_results[i]):
-            assert (res == alg_results[0][j]).mean() == 1, 'Results are different between runs for {} {} {} k={}'.format(
-                algorithm, weight, distance, k)
+            assert (res == alg_results[0][j]).mean() == 1, \
+                'Results are different between runs for {} {} {} k={}'.format(
+                    algorithm, weight, distance, k)
+
 
 def convert_data(data, class_name=np.array, order='C', dtype=np.float64):
     if order == 'C':
@@ -66,6 +74,7 @@ def convert_data(data, class_name=np.array, order='C', dtype=np.float64):
         data = np.asfortranarray(data, dtype=dtype)
     return class_name(data)
 
+
 @pytest.mark.parametrize('distance', DISTANCES)
 @pytest.mark.parametrize('algorithm', ALGORITHMS)
 @pytest.mark.parametrize('weight', WEIGHTS)
@@ -73,6 +82,7 @@ def convert_data(data, class_name=np.array, order='C', dtype=np.float64):
 def test_data_formats_diff(distance, algorithm, weight, k):
     if daal_check_version((2020, 3)):
         check_data_formats_diff(distance, algorithm, weight, k)
+
 
 def check_data_formats_diff(distance, algorithm, weight, k):
     pd = pytest.importorskip('pandas')
@@ -89,9 +99,9 @@ def check_data_formats_diff(distance, algorithm, weight, k):
             y_train_copy = convert_data(y_train.copy(), data_format, order)
             y_test_copy = convert_data(y_test.copy(), data_format, order)
 
-            alg = KNeighborsClassifier(
-            n_neighbors=k, weights=weight, algorithm=algorithm,
-            leaf_size=30, p=2, metric=distance)
+            alg = KNeighborsClassifier(n_neighbors=k, weights=weight,
+                                       algorithm=algorithm, leaf_size=30,
+                                       p=2, metric=distance)
             alg.fit(x_train_copy, y_train_copy)
             distances, indices = alg.kneighbors(x_test_copy)
             labels = alg.predict(x_test_copy)
@@ -99,4 +109,5 @@ def check_data_formats_diff(distance, algorithm, weight, k):
 
     for i in range(1, len(alg_results)):
         for j, res in enumerate(alg_results[i]):
-            assert (res == alg_results[0][j]).mean() == 1, 'Results are different between formats'
+            assert (res == alg_results[0][j]).mean() == 1, \
+                'Results are different between formats'
