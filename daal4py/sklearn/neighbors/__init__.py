0531ac708c4e9db4a0375f7d70f072b7386839ed
Nikita Timakin <nikita.timakin@intel.com>
2020-10-14 07:18:36 +0300

    New Approach for kNN Patching (#312)
    * New approach for kNN patching
    
    * NearestNeighbors patching
    
    * Fix version check in kNN
    
    * Exception added
    
    * Removing NearestNeighbors
    
    * Fix for 23rd version
    
    * Fix for 22nd version
    
    * Fix for 21st version
    
    * KNeighborsClassifier.predict patched
    
    * Comments applied
    
    * kNN classification moved to other file

--- daal4py/sklearn/neighbors/__init__.py
+++ daal4py/sklearn/neighbors/__init__.py
@@ -3,7 +3,7 @@ The 'daal4py.sklearn.neighbors' module implements the k-nearest neighbors
 algorithm.
 """
 
-from .knn import KNeighborsClassifier
-from .knn import KNeighborsMixin
 
-__all__ = ['KNeighborsClassifier', 'KNeighborsMixin']
+from ._classification import KNeighborsClassifier
+
+__all__ = ['KNeighborsClassifier']
