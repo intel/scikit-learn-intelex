d6bd81f5e1bf5f17f57b042d014c717c0523c51a
Christopher Nguyen <61757292+cnguyen-uk@users.noreply.github.com>
2020-10-15 12:08:12 +0100

    Use enumerate() instead of range() and len()

--- generator/wrapper_gen.py
+++ generator/wrapper_gen.py
@@ -1252,7 +1252,7 @@ class wrapper_gen(object):
 
     def lp(self, t):
         tmp = t.split('\n')
-        for i in range(len(tmp)):
+        for i in enumerate(tmp):
             print(i, tmp[i])
 
     ##################################################################################
