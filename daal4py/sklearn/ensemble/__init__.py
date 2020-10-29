e39db2bfff304ee0672be36fc8a680ea187c605a
Kirill Petrov <kirill.petrov@intel.com>
2020-10-30 00:43:16 +0300

    fix conflict

--- daal4py/sklearn/ensemble/__init__.py
+++ daal4py/sklearn/ensemble/__init__.py
@@ -3,10 +3,7 @@ The 'daal4py.sklearn.ensemble' module implements daal4py-based
 RandomForestClassifier and RandomForestRegressor classes.
 """
 from daal4py.sklearn._utils import daal_check_version
-if daal_check_version((2020,'P', 3)):
-    from .forest import (RandomForestClassifier, RandomForestRegressor)
-else:
-    from .decision_forest import (RandomForestClassifier, RandomForestRegressor)
+from .forest import (RandomForestClassifier, RandomForestRegressor)
 from .GBTDAAL import (GBTDAALClassifier, GBTDAALRegressor)
 from .AdaBoostClassifier import AdaBoostClassifier
 
