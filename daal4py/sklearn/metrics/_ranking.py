ccb5e8b6abf60db870e1f8f2562a843fb6bf2456
OnlyDeniko <denis.kulandin@intel.com>
2020-11-24 14:51:23 +0300

    added __init__

--- daal4py/sklearn/metrics/_ranking.py
+++ daal4py/sklearn/metrics/_ranking.py
@@ -1,3 +1,20 @@
+#
+#*****************************************************************************
+# Copyright 2020 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#*****************************************************************************
+
 import daal4py as d4p
 import numpy as np
 from sklearn.utils import check_array
@@ -9,7 +26,7 @@ from .._utils import get_patch_message
 import logging
 from functools import partial
 
-def roc_auc_score(y_true, y_score, *, average="macro", sample_weight=None,
+def _daal_roc_auc_score(y_true, y_score, *, average="macro", sample_weight=None,
                   max_fpr=None, multi_class="raise", labels=None):
     y_type = type_of_target(y_true)
     y_true = check_array(y_true, ensure_2d=False, dtype=None)
--- daal4py/sklearn/monkeypatch/dispatcher.py
+++ daal4py/sklearn/monkeypatch/dispatcher.py
@@ -25,7 +25,7 @@ from ..utils.validation import _daal_assert_all_finite
 from ..svm.svm import SVC as SVC_daal4py
 from ..ensemble.forest import RandomForestClassifier as RandomForestClassifier_daal4py
 from ..ensemble.forest import RandomForestRegressor as RandomForestRegressor_daal4py
-from ..metrics import roc_auc_score as roc_auc_score_daal4py
+from ..metrics import _daal_roc_auc_score
 from ..cluster.k_means import KMeans as KMeans_daal4py
 from ..cluster.dbscan import DBSCAN as DBSCAN_daal4py
 from ..linear_model.coordinate_descent import Lasso as Lasso_daal4py
@@ -36,6 +36,7 @@ from ..decomposition.pca import PCA as PCA_daal4py
 from ..manifold import TSNE as TSNE_daal4py
 from ..linear_model.logistic_path import LogisticRegression as LogisticRegression_daal4py
 from sklearn import model_selection
+from sklearn import metrics
 from sklearn.utils import validation
 from sklearn.metrics import pairwise
 import sklearn.neighbors as neighbors_module
@@ -51,7 +52,6 @@ from functools import lru_cache
 import sklearn.cluster as cluster_module
 import sklearn.ensemble as ensemble_module
 import sklearn.svm as svm_module
-import sklearn.metrics as metrics_module
 
 if LooseVersion(sklearn_version) >= LooseVersion("0.22"):
     import sklearn.linear_model._logistic as logistic_module
@@ -93,7 +93,7 @@ def _get_map_of_algorithms():
         'knn_regressor':            [[(neighbors_module, 'KNeighborsRegressor', KNeighborsRegressor_daal4py), None]],
         'random_forest_classifier': [[(ensemble_module, 'RandomForestClassifier', RandomForestClassifier_daal4py), None]],
         'random_forest_regressor':  [[(ensemble_module, 'RandomForestRegressor', RandomForestRegressor_daal4py), None]],
-        'roc_auc_score':            [[(metrics_module, 'roc_auc_score', roc_auc_score_daal4py), None]],
+        'roc_auc_score':            [[(metrics, 'roc_auc_score', _daal_roc_auc_score), None]],
         'train_test_split':         [[(model_selection, 'train_test_split', _daal_train_test_split), None]],
         'fin_check':                [[(validation, '_assert_all_finite', _daal_assert_all_finite), None]],
         'tsne':                     [[(manifold_module, 'TSNE', TSNE_daal4py), None]],
