5c7c013c24be95ca33d586190ea653fa18f0b2d9
Kirill Petrov <kirill.petrov@intel.com>
2020-12-09 03:42:54 +0300

    fix

--- .circleci/setup_env.sh
+++ .circleci/setup_env.sh
@@ -8,8 +8,7 @@ conda update -q conda
 conda create -n bld python=3.7 conda-build
 source activate bld
 conda install -q --override-channels -c conda-forge numpy scipy pytest pandas pyyaml joblib numpydoc
-# need to remove dpcpp_cpp_rt & impi_rt after making dependencies optional
-conda install -q --override-channels -c intel dal dpcpp_cpp_rt impi_rt
+conda install -q --override-channels -c intel dal
 gcc -v
 g++ -v
 head /proc/cpuinfo
