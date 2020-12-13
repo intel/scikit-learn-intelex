46e9eb14c5c2506875fce15f5be1dfea19490caa
Kirill <kirill.petrov@intel.com>
2020-12-13 14:53:43 +0300

    Update setup_env.sh

--- .circleci/setup_env.sh
+++ .circleci/setup_env.sh
@@ -7,7 +7,7 @@ conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda create -n bld python=3.7 conda-build
 source activate bld
-conda install -q --override-channels -c defaults numpy scipy pytest pandas pyyaml joblib numpydoc
+conda install -q --override-channels -c conda-forge numpy scipy pytest pandas pyyaml joblib numpydoc
 conda install -q --override-channels -c intel dal
 gcc -v
 g++ -v
