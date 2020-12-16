6167d71d226990ebe7f5ef4bcb04fe741b823a1a
Pavel Yakovlev <p_yak_11@mail.ru>
2020-12-17 00:09:49 +0300

    checkout files with some changes

--- .circleci/setup_env.sh
+++ .circleci/setup_env.sh
@@ -1,13 +1,13 @@
+#!/bin/bash
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 bash miniconda.sh -b -p ~/miniconda
 export PATH=~/miniconda/bin:$PATH
 hash -r
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
-conda create -n bld python=3.7 conda-build
-source activate bld
-conda install -q scipy pytest pandas pyyaml joblib numpydoc cython jinja2 numpy clang-tools
-conda install -q --override-channels -c intel dal dal-include
+conda install -q conda-build
+conda create -n bld --override-channels -c intel daal daal-devel tbb python=3.7
+conda install -q -n bld --override-channels -c defaults numpy scipy pytest pandas pyyaml joblib numpydoc
 gcc -v
 g++ -v
 head /proc/cpuinfo
