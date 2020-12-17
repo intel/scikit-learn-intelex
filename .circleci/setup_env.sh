86cb456d6822d26a451826d823c442e32be66ed3
Pavel Yakovlev <35104773+Pahandrovich@users.noreply.github.com>
2020-12-17 22:37:29 +0300

    Fix nightly testing (#428)

--- .circleci/setup_env.sh
+++ .circleci/setup_env.sh
@@ -5,10 +5,10 @@ export PATH=~/miniconda/bin:$PATH
 hash -r
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
-conda install -q conda-build
-conda create -n bld --override-channels -c intel daal daal-devel tbb
-conda install -q -n bld --override-channels -c defaults python=3.7 numpy scipy pytest pandas pyyaml joblib numpydoc
-conda install -q -n bld -c conda-forge --override-channels mpi mpich
+conda create -n bld python=3.7 conda-build
+source activate bld
+conda install -q scipy pytest pandas pyyaml joblib numpydoc cython jinja2 numpy clang-tools
+conda install -q --override-channels -c intel dal dal-include
 gcc -v
 g++ -v
 head /proc/cpuinfo
