ca86dd0f3ec86788dd5bd7adbc51aec38c557614
ntimakin <nikita.timakin@intel.com>
2020-12-21 21:56:24 +0300

    Removing unncessary file for DF

--- daal4py/sklearn/ensemble/__init__.py
+++ daal4py/sklearn/ensemble/__init__.py
@@ -2,8 +2,7 @@
 The 'daal4py.sklearn.ensemble' module implements daal4py-based 
 RandomForestClassifier and RandomForestRegressor classes.
 """
-from daal4py.sklearn._utils import daal_check_version
-from .forest import (RandomForestClassifier, RandomForestRegressor)
+from ._forest import (RandomForestClassifier, RandomForestRegressor)
 from .GBTDAAL import (GBTDAALClassifier, GBTDAALRegressor)
 from .AdaBoostClassifier import AdaBoostClassifier
 
