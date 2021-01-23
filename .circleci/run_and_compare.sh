2564572e4958126504ec511e84b64905ac429f75
Kirill <kirill.petrov@intel.com>
2021-01-24 01:21:58 +0300

    Update run_and_compare.sh

--- .circleci/run_and_compare.sh
+++ .circleci/run_and_compare.sh
@@ -4,3 +4,7 @@ touch ~/d4p.out ~/skl.out
 export DESELECTED_TESTS=$(python deselect_tests.py ../deselected_tests.yaml --absolute)
 echo "-m daal4py -m pytest ${DESELECTED_TESTS} -q -ra --disable-warnings --pyargs sklearn"
 cd && ((python -m daal4py -m pytest ${DESELECTED_TESTS} -ra --disable-warnings --pyargs sklearn | tee ~/d4p.out) || true)
+# extract status strings
+export D4P=$(grep -E "=(\s\d*\w*,?)+ in .*\s=" ~/d4p.out)
+echo "Summary of patched run: " $D4P
+python compare_runs.py
