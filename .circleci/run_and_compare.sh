2a9471647baba9e0add662d1cc0fa256e91c731b
Kirill <kirill.petrov@intel.com>
2021-01-24 12:00:38 +0300

    ***NO_CI***

--- .circleci/run_and_compare.sh
+++ .circleci/run_and_compare.sh
@@ -3,7 +3,7 @@ DAAL4PY_ROOT=$1
 OUTPUT_ROOT=$2
 cd $DAAL4PY_ROOT/.circleci
 touch ~/d4p.out ~/skl.out
-export DESELECTED_TESTS=$(python deselect_tests.py ../deselected_tests.yaml --absolute --reduced)
+export DESELECTED_TESTS=$(python deselect_tests.py ../deselected_tests.yaml --reduced)
 echo "-m daal4py -m pytest ${DESELECTED_TESTS} -q -ra --disable-warnings --pyargs sklearn"
 cd && ((python -m daal4py -m pytest ${DESELECTED_TESTS} -ra --disable-warnings --pyargs sklearn | tee ~/d4p.out) || true)
 # extract status strings
