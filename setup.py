c2dd889552fbe95d9cccf61b816e7e3198f6dc5e
Denis Kulandin <kulandin08@gmail.com>
2021-01-24 22:36:29 +0300

    setup.py 3

--- setup.py
+++ setup.py
@@ -101,9 +101,9 @@ def get_lib_suffix():
             ld_lib_path_suffix == 'onedal',
             lib_dir_suffix == 'onedal']):
         return 'onedal'
-    if any([find_library('daal_core') is not None,
-            ld_lib_path_suffix == 'daal',
-            lib_dir_suffix == 'daal']):
+    elif any([find_library('daal_core') is not None,
+              ld_lib_path_suffix == 'daal',
+              lib_dir_suffix == 'daal']):
         return 'daal'
 
     raise ImportError('Unable to import oneDAL or oneDAL lib')
