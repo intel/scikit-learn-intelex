b376b0fad513dd5fa549a99fd85c0e1ecf83999b
ivan-samsonov <ivan.samsonov@intel.com>
2021-02-10 17:30:29 +0300

    Enable DPC++ compiler support on Windows platform (#510)
    * Enable DPCPP support for Windows build
    
    * Correct distutils MSVC base class search
    
    * Fix PEP8 errors
    
    * Update dpcppcompiler.py
    
    Co-authored-by: Nikolay Petrov <nikolay.a.petrov@intel.com>

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -7,14 +7,19 @@ IF DEFINED DAALROOT (set DALROOT=%DAALROOT%)
 
 IF NOT DEFINED DALROOT (set DALROOT=%PREFIX%) 
 
+set "BUILD_ARGS="
+
 IF DEFINED DPCPPROOT (
     echo "Sourcing DPCPPROOT"
     call "%DPCPPROOT%\env\vars.bat"
     set "CC=dpcpp"
     set "CXX=dpcpp"
     dpcpp --version
+    SET "BUILD_ARGS=--compiler dpcpp"
 )
 
 set PATH=%PATH%;%PREFIX%\Library\bin\libfabric
 
+%PYTHON% setup.py build %BUILD_ARGS%
+IF %ERRORLEVEL% neq 0 EXIT /b %ERRORLEVEL%
 %PYTHON% setup.py install --single-version-externally-managed --record record.txt
