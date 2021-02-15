be37aa09696622e2da719d40395685a440c18e73
OnlyDeniko <denis.kulandin@intel.com>
2021-02-15 20:09:01 +0300

    TEST

--- daal4py/sklearn/monkeypatch/tests/utils/_launch_algorithms.py
+++ daal4py/sklearn/monkeypatch/tests/utils/_launch_algorithms.py
@@ -87,8 +87,7 @@ def run_patch(model_info, dtype):
 def run_algotithms():
     for info in MODELS_INFO:
         for t in TYPES:
-            if get_class_name(info['model']) == 'LinearRegression' \
-                    and (t.__name__ == 'uint32' or t.__name__ == 'uint64'):
+            if get_class_name(info['model']) == 'LinearRegression':
                 continue
             run_patch(info, t)
 
