5ff77ff1880e2b2819552e0264275e86676666aa
Pavel Yakovlev <p_yak_11@mail.ru>
2021-02-18 18:35:52 +0300

    remove compiler flags from build.sh

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -26,11 +26,6 @@ fi
 if [ "$(uname)" == "Darwin" ]; then
     export CC=gcc
     export CXX=g++
-    # dead_strip_dylibs does not work with DAAL, which is underlinked by design
-    export LDFLAGS="${LDFLAGS//-Wl,-dead_strip_dylibs}"
-    export LDFLAGS_LD="${LDFLAGS_LD//-dead_strip_dylibs}"
-    # some dead_strip_dylibs come from Python's sysconfig. Setting LDSHARED overrides that
-    export LDSHARED="-bundle -undefined dynamic_lookup -flto -Wl,-export_dynamic -Wl,-pie -Wl,-headerpad_max_install_names"
 fi
 
 export DAAL4PY_VERSION=$PKG_VERSION
