177232dd775624fb73d19c07290e6564ccfb4edb
OnlyDeniko <denis.kulandin@intel.com>
2021-03-18 15:04:12 +0300

    -

--- daal4py/sklearn/monkeypatch/dispatcher.py
+++ daal4py/sklearn/monkeypatch/dispatcher.py
@@ -164,7 +164,7 @@ def enable(name=None, verbose=True, deprecation=True):
         sys.stderr.write(
             "Intel(R) oneAPI Data Analytics Library solvers for sklearn enabled: "
             "https://intelpython.github.io/daal4py/sklearn.html\n")
-        logging.warning('Please, do re import of all imported scikit-learn modules after patch_sklearn()')
+        logging.warning('Please, do re-import of all imported scikit-learn modules after patch_sklearn()')
 
 
 def disable(name=None):
@@ -177,7 +177,7 @@ def disable(name=None):
         sys.stderr.write(
             "Intel(R) oneAPI Data Analytics Library solvers for sklearn disabled: "
             "https://intelpython.github.io/daal4py/sklearn.html\n")
-        logging.warning('Please, do re import of all imported scikit-learn modules after unpatch_sklearn()')
+        logging.warning('Please, do re-import of all imported scikit-learn modules after unpatch_sklearn()')
 
 
 def _patch_names():
