4c2f332d651dadaf45fa613b3f3534f88234a550
Pavel Yakovlev <p_yak_11@mail.ru>
2021-03-24 16:51:22 +0300

    Fix sklearnex import (#582)
    (cherry picked from commit c484acc1f915501eb00685d60510a79ebcdf5c43)

--- sklearnex/dispatcher.py
+++ sklearnex/dispatcher.py
@@ -15,12 +15,9 @@
 # limitations under the License.
 #===============================================================================
 
-from daal4py.sklearn import patch_sklearn as patch_sklearn_orig
-from daal4py.sklearn import unpatch_sklearn as unpatch_sklearn_orig
-from daal4py.sklearn import sklearn_patch_names as get_patch_names_orig
-
 
 def patch_sklearn(name=None, verbose=True):
+    from daal4py.sklearn import patch_sklearn as patch_sklearn_orig
     try:
         patch_sklearn_orig(name, verbose, deprecation=False)
     except Exception:
@@ -28,8 +25,10 @@ def patch_sklearn(name=None, verbose=True):
 
 
 def unpatch_sklearn(name=None):
+    from daal4py.sklearn import unpatch_sklearn as unpatch_sklearn_orig
     unpatch_sklearn_orig(name)
 
 
 def get_patch_names():
+    from daal4py.sklearn import sklearn_patch_names as get_patch_names_orig
     return get_patch_names_orig()
