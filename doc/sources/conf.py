a15ed477c932ae278258a7d1c2d5737662341399
Pavel Yakovlev <p_yak_11@mail.ru>
2021-03-29 12:37:08 +0300

    fix pep8

--- doc/sources/conf.py
+++ doc/sources/conf.py
@@ -150,7 +150,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'scikit-learn-intelex.tex', 'Intel(R) Extension for Scikit-learn* Documentation',
+    (master_doc, 'scikit-learn-intelex.tex',
+     'Intel(R) Extension for Scikit-learn* Documentation',
      'Intel', 'manual'),
 ]
 
@@ -160,7 +161,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'scikit-learn-intelex', 'Intel(R) Extension for Scikit-learn* Documentation',
+    (master_doc, 'scikit-learn-intelex',
+     'Intel(R) Extension for Scikit-learn* Documentation',
      [author], 1)
 ]
 
@@ -171,7 +173,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'scikit-learn-intelex', 'Intel(R) Extension for Scikit-learn* Documentation',
+    (master_doc, 'scikit-learn-intelex',
+     'Intel(R) Extension for Scikit-learn* Documentation',
      author, 'scikit-learn-intelex', 'One line description of project.',
      'Miscellaneous'),
 ]
