24d094d14c31de75998f76147b165884280b56e9
Kirill Petrov <kirill.petrov@intel.com>
2021-04-09 12:44:11 +0300

    apply rst

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-#*******************************************************************************
-# Copyright 2014-2021 Intel Corporation
-# All Rights Reserved.
+#===============================================================================
+# Copyright 2018-2021 Intel Corporation
 #
-# This software is licensed under the Apache License, Version 2.0 (the
-# "License"), the following terms apply:
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# You may not use this file except in compliance with the License.  You may
-# obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#*******************************************************************************
+#===============================================================================
 
 daal4py_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 count=3
