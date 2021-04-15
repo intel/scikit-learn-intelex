da0ecd231ac61a1099162e16808897f4e9cfd006
Michael Smirnov <michael.smirnov@intel.com>
2021-04-15 15:57:49 +0300

    PEP8 fix

--- .circleci/run_xpu_tests.py
+++ .circleci/run_xpu_tests.py
@@ -2,16 +2,22 @@ import argparse
 import pytest
 import os
 
+
 def get_context(device):
     from daal4py.oneapi import sycl_context
     return sycl_context(device, host_offload_on_fail=True)
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Script to run scikit-learn tests with device context manager')
-    parser.add_argument('-d', '--device', type=str, help='device name', choices=['host', 'cpu', 'gpu'])
+    parser = argparse.ArgumentParser(
+        description='Script to run scikit-learn tests with device context manager')
+    parser.add_argument(
+        '-d', '--device',
+        type=str,
+        help='device name',
+        choices=['host', 'cpu', 'gpu'])
     args = parser.parse_args()
 
     with get_context(args.device):
-        pytest.main(os.environ['DESELECTED_TESTS'].split() +
-                    ["-ra", "--disable-warnings",
-                    "--pyargs", "sklearn"])
+        pytest.main(os.environ['DESELECTED_TESTS'].split() + [
+            "-ra", "--disable-warnings", "--pyargs", "sklearn"])
