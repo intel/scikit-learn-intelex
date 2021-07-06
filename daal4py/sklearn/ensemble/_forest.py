20597b89e965c25a1f3bd3cc6ef5aa97efe3feaf
agorshk <andrey.gorshkov@intel.com>
2021-07-07 01:35:39 +0300

    RF: fix versioning

--- daal4py/sklearn/ensemble/_forest.py
+++ daal4py/sklearn/ensemble/_forest.py
@@ -704,11 +704,9 @@ class RandomForestClassifier(RandomForestClassifier_original):
             The class probabilities of the input samples. The order of the
             classes corresponds to that in the attribute :term:`classes_`.
         """
-        X = check_array(X, accept_sparse=['csr', 'csc', 'coo'],
-                        dtype=[np.float64, np.float32])
         if not hasattr(self, 'daal_model_') or \
                 sp.issparse(X) or self.n_outputs_ != 1 or \
-                not daal_check_version((2021, 'P', 200)):
+                not daal_check_version((2021, 'P', 400)):
             logging.info(
                 "sklearn.ensemble.RandomForestClassifier."
                 "predict_proba: " + get_patch_message("sklearn"))
@@ -716,6 +714,8 @@ class RandomForestClassifier(RandomForestClassifier_original):
         logging.info(
             "sklearn.ensemble.RandomForestClassifier."
             "predict_proba: " + get_patch_message("daal"))
+        X = check_array(X, dtype=[np.float64, np.float32])
+        self._check_n_features(X, reset=False)
         return _daal_predict_proba(self, X)
 
     if sklearn_check_version('1.0'):
