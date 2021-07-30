6bef5c45a1152caeac0bcf9599703df7b4b25b61
Michael Smirnov <michael.smirnov@intel.com>
2021-07-30 12:47:51 +0300

    add rpaths to onedal libraries in cmake (#749)

--- onedal/__init__.py
+++ onedal/__init__.py
@@ -14,6 +14,17 @@
 # limitations under the License.
 #===============================================================================
 
+import platform
+if "Windows" in platform.system():
+    import os
+    import sys
+    import site
+    path_to_env = site.getsitepackages()[0]
+    path_to_libs = os.path.join(path_to_env, "Library", "bin")
+    if sys.version_info.minor >= 8:
+        os.add_dll_directory(path_to_libs)
+    os.environ['PATH'] += os.pathsep + path_to_libs
+
 try:
     import onedal._onedal_py_dpc as _backend
 except ImportError:
