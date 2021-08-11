e3c588c183be73860b6a9d57b4eab38aee460d6d
Alexander Andreev <alexander.andreev@intel.com>
2021-08-11 23:21:27 +0300

    Fix train_test_split and add test

---
+++ daal4py/sklearn/model_selection/tests/test_split.py
@@ -0,0 +1,42 @@
+#===============================================================================
+# Copyright 2021 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#===============================================================================
+
+import numpy as np
+import pytest
+from sklearn.model_selection import train_test_split as skl_train_test_split
+from daal4py.sklearn.model_selection import _daal_train_test_split as d4p_train_test_split
+from sklearn.datasets import make_classification
+
+
+N_SAMPLES = [2 ** i + 1 for i in range(2, 17)]
+RANDOM_STATE = 42
+
+
+@pytest.mark.parametrize('n_samples', N_SAMPLES)
+def test_results_similarity(n_samples):
+    x, y = make_classification(
+        n_samples=n_samples, n_features=4, random_state=RANDOM_STATE)
+    d4p_res = d4p_train_test_split(
+        x, y, test_size=n_samples // 2 - 1, train_size=n_samples // 2 - 1, random_state=RANDOM_STATE)
+    skl_res = skl_train_test_split(
+        x, y, test_size=n_samples // 2 - 1, train_size=n_samples // 2 - 1, random_state=RANDOM_STATE)
+
+    assert len(d4p_res) == len(
+        skl_res), 'train_test_splits have different output size'
+
+    for i in range(len(d4p_res)):
+        assert np.all(d4p_res[i] == skl_res[i]
+                      ), 'train_test_splits have different output'
