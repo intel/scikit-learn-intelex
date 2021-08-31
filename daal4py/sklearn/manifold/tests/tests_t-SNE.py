766301da2168e882e192126b7be5a55d20f4bc0d
Alexey Kolobyanin <alexey.kolobyanin@intel.com>
2021-08-31 12:07:57 +0300

    t-SNE: add tests for t-SNE

--- daal4py/sklearn/manifold/tests/tests_t-SNE.py
+++ daal4py/sklearn/manifold/tests/tests_t-SNE.py
@@ -11,47 +11,52 @@ DIGITS = load_digits()
 N_ITER = 1
 DIVERG = 1.04
 
+
 def compare_with_sklearn_tsne(early_exaggeration=12.0, learning_rate=200,
                               min_grad_norm=1e-7, angle=0.5, describe=""):
-    
+
     x = DIGITS.data
-    
+
     # Models
-    sklearn_model = sklearn_TSNE(early_exaggeration=early_exaggeration, learning_rate=learning_rate,
+    sklearn_model = sklearn_TSNE(early_exaggeration=early_exaggeration,
+                                 learning_rate=learning_rate,
                                  min_grad_norm=min_grad_norm, angle=angle)
-    
-    daal_model = daal_TSNE(early_exaggeration=early_exaggeration, learning_rate=learning_rate,
-                                 min_grad_norm=min_grad_norm, angle=angle)
-    
+
+    daal_model = daal_TSNE(early_exaggeration=early_exaggeration,
+                           learning_rate=learning_rate,
+                           min_grad_norm=min_grad_norm, angle=angle)
+
     # Train
     sklearn_model.fit(x)
     daal_model.fit(x)
-    
+
     # Divergence
-    sklearn_model_divergence = sklearn_model.kl_divergence_
-    daal_model_divergence = daal_model.kl_divergence_
-    ratio = daal_model_divergence / sklearn_model_divergence
-    reason = describe +        f"daal_divergence = {daal_model_divergence}, sklearn_divergence = {sklearn_model_divergence}"
+    sklearn_divergence = sklearn_model.kl_divergence_
+    daal_divergence = daal_model.kl_divergence_
+    ratio = daal_divergence / sklearn_divergence
+    reason = describe + \
+        f"daal_divergence = {daal_divergence}, sklearn_divergence = {sklearn_divergence}"
     assert ratio <= DIVERG, reason
-    
+
     # N_iter
     sklearn_model_n_iter = sklearn_model.n_iter_
     daal_model_n_iter = daal_model.n_iter_
     ratio = daal_model_n_iter / sklearn_model_n_iter
-    reason = describe +        f"daal_n_iter = {daal_model_n_iter}, sklearn_n_iter = {sklearn_model_n_iter}"
+    reason = describe + \
+        f"daal_n_iter = {daal_model_n_iter}, sklearn_n_iter = {sklearn_model_n_iter}"
     assert ratio == N_ITER, reason
-    
 
-    
 
 EARLY_EXAGGERATION = [5.0, 7.0, 9.0, 12.0, 14.0, 16.0]
+
 
 @pytest.mark.parametrize('early_exaggeration', EARLY_EXAGGERATION)
 def test_early_exaggeration(early_exaggeration):
     compare_with_sklearn_tsne(
         early_exaggeration=early_exaggeration,
-        describe = "early_exaggeration:"
+        describe="early_exaggeration:"
     )
+
 
 LEARNING_RATE = [100.0, 200.0, 300.0, 400.0, 500.0, 600.0]
 
@@ -60,27 +65,27 @@ LEARNING_RATE = [100.0, 200.0, 300.0, 400.0, 500.0, 600.0]
 def test_learning_rate(learning_rate):
     compare_with_sklearn_tsne(
         learning_rate=learning_rate,
-        describe = "learning_rate:"
+        describe="learning_rate:"
     )
+
 
 ANGLE = [0.2, 0.35, 0.5, 0.65, 0.8]
 
+
 @pytest.mark.parametrize('angle', ANGLE)
-def test_learning_rate(angle):
+def test_angle(angle):
     compare_with_sklearn_tsne(
         angle=angle,
-        describe = "angle:"
+        describe="angle:"
     )
+
 
 MIN_GRAD_NORM = [1e-5, 1e-6, 1e-7, 1e-8]
 
+
 @pytest.mark.parametrize('min_grad_norm', MIN_GRAD_NORM)
-def test_learning_rate(min_grad_norm):
+def test_min_grad_norm(min_grad_norm):
     compare_with_sklearn_tsne(
         min_grad_norm=min_grad_norm,
-        describe = "min_grad_norm:"
+        describe="min_grad_norm:"
     )
-
-
-
-
