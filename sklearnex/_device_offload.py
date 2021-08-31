1244d6b0315443428ec0caa4c433248b20efc11c
Michael Smirnov <michael.smirnov@intel.com>
2021-08-31 14:45:01 +0300

    pep fix

--- sklearnex/_device_offload.py
+++ sklearnex/_device_offload.py
@@ -41,8 +41,8 @@ def _get_global_queue():
 
     if target != 'auto':
         if d4p_target is not None and \
-            (d4p_target != target and
-             d4p_target not in target.sycl_device.get_filter_string()):
+           d4p_target != target and \
+           d4p_target not in target.sycl_device.get_filter_string():
             raise RuntimeError("Cannot use target offload option "
                                "inside daal4py.oneapi.sycl_context")
         if isinstance(target, SyclQueue):
