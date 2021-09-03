cdb4341ab85fe6bf9304bd3b60b2391501f0fa31
Michael Smirnov <michael.smirnov@intel.com>
2021-09-03 17:19:40 +0300

    fix import of host binary

--- onedal/common/_policy.py
+++ onedal/common/_policy.py
@@ -14,18 +14,20 @@
 # limitations under the License.
 #===============================================================================
 
-from onedal import _backend
+from onedal import _backend, _is_dpc_backend
 import sys
 
 
 def _get_policy(queue, *data):
     data_queue = _get_queue(*data)
-
-    if queue is None:
-        if data_queue is None:
-            return _HostInteropPolicy()
-        return _DataParallelInteropPolicy(data_queue)
-    return _DataParallelInteropPolicy(queue)
+    if _is_dpc_backend:
+        if queue is None:
+            if data_queue is None:
+                return _HostInteropPolicy()
+            return _DataParallelInteropPolicy(data_queue)
+        return _DataParallelInteropPolicy(queue)
+    assert data_queue is None and queue is None
+    return _HostInteropPolicy()
 
 
 def _get_queue(*data):
@@ -52,19 +54,19 @@ class _Daal4PyContextReset:
             self._d4p_context.apply()
 
 
-class _DataParallelInteropPolicy(_backend.data_parallel_policy):
-    def __init__(self, queue):
-        self._queue = queue
-        self._d4p_interop = _Daal4PyContextReset()
-        if 'sklearnex' in sys.modules:
-            from sklearnex._device_offload import DummySyclQueue
-            if isinstance(queue, DummySyclQueue):
-                super().__init__(self._queue.sycl_device.get_filter_string())
-                return
-        super().__init__(self._queue.addressof_ref())
-
-
 class _HostInteropPolicy(_backend.host_policy):
     def __init__(self):
         super().__init__()
         self._d4p_interop = _Daal4PyContextReset()
+
+if _is_dpc_backend:
+    class _DataParallelInteropPolicy(_backend.data_parallel_policy):
+        def __init__(self, queue):
+            self._queue = queue
+            self._d4p_interop = _Daal4PyContextReset()
+            if 'sklearnex' in sys.modules:
+                from sklearnex._device_offload import DummySyclQueue
+                if isinstance(queue, DummySyclQueue):
+                    super().__init__(self._queue.sycl_device.get_filter_string())
+                    return
+            super().__init__(self._queue.addressof_ref())
