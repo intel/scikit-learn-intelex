2a22636035b2d9e99964e56419ebc9676f9a972d
Dmitry Tronin <tronindmitr@gmail.com>
2021-10-12 19:31:11 +0300

    Add usage example of Catboost converter (#773)
    Co-authored-by: Dmitriy Kalyanov <dmitriy.kalyanov@intel.com>
    Co-authored-by: Nikolay Petrov <nikolay.a.petrov@intel.com>

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -197,6 +197,8 @@ gen_examples = [
      ((2020, 'P', 2), (2021, 'B', 109)), ['lightgbm']),
     ('gbt_cls_model_create_from_xgboost_batch', None, None,
      ((2020, 'P', 2), (2021, 'B', 109)), ['xgboost']),
+    ('gbt_cls_model_create_from_catboost_batch', None, None,
+     (2021, 'P', 4), ['catboost']),
     ('gradient_boosted_classification_batch',),
     ('gradient_boosted_regression_batch',),
     ('implicit_als_batch', 'implicit_als_batch.csv', 'prediction'),
