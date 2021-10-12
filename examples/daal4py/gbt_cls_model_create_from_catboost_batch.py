f46e85f0709eff8ea931e3a168c73cd7a83091e1
Dmitriy Kalyanov <dmitriy.kalyanov@intel.com>
2021-10-12 11:55:34 +0300

    fix copyright

--- examples/daal4py/gbt_cls_model_create_from_catboost_batch.py
+++ examples/daal4py/gbt_cls_model_create_from_catboost_batch.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#=============================================================================
+#===============================================================================
 
 # daal4py Gradient Bossting Classification model creation from Catboost example
 
