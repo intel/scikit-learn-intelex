b74c501320a793db76708d9d34d624602e023549
Ekaterina Mekhnetsova <ekaterina.mekhnetsova@intel.com>
2021-10-20 12:59:23 +0300

    Update sphinx theme

--- doc/sources/conf.py
+++ doc/sources/conf.py
@@ -96,17 +96,25 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
-html_theme_options = {
-    "sidebarwidth": 30,
-    "nosidebar": False,
-}
+html_theme = 'sphinx_book_theme'
+html_logo = '_static/oneAPI-rgb-rev-100.png'
+html_favicon = '_static/favicons.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 # html_theme_options = {}
+
+html_theme_options = {
+    'repository_url': 'https://github.com/intel/scikit-learn-intelex',
+    'path_to_docs': 'doc/sources',
+    'use_issues_button': True,
+    'use_edit_page_button': True,
+    'repository_branch': 'master',
+    'extra_footer': '<p align="right"><a href="https://www.intel.com/content/www/us/en/privacy/intel-cookie-notice.html">Cookies</a></p>'
+}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
