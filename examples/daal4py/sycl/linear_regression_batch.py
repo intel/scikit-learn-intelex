fb526ef6644e66b32e58b3550736722611ab6e0a
vlad-nazarov <vladislav.nazarov@intel.com>
2021-10-21 16:24:29 +0300

    Update atol

--- examples/daal4py/sycl/linear_regression_batch.py
+++ examples/daal4py/sycl/linear_regression_batch.py
@@ -119,7 +119,7 @@ def main(readcsv=read_csv, method='defaultDense'):
             sycl_test_indep_data = sycl_buffer(test_indep_data)
             result_gpu, _ = compute(sycl_train_indep_data, sycl_train_dep_data,
                                     sycl_test_indep_data)
-        assert np.allclose(result_classic.prediction, result_gpu.prediction, atol=1e-3)
+        assert np.allclose(result_classic.prediction, result_gpu.prediction, atol=1e-1)
 
     # It is possible to specify to make the computations on CPU
     with cpu_context():
