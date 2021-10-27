613a9e677866632506f10b54a157495019d13e9b
pavel.yakovlev <pyakovle@nnlmkl330.inn.intel.com>
2021-10-27 19:08:54 +0300

    fix pep8

--- onedal/svm/svm.py
+++ onedal/svm/svm.py
@@ -247,13 +247,11 @@ class BaseSVM(BaseEstimator, metaclass=ABCMeta):
             raise ValueError("break_ties must be False when "
                              "decision_function_shape is 'ovo'")
 
-        if (module in [_backend.svm.classification,_backend.svm.nu_classification]):
+        if (module in [_backend.svm.classification, _backend.svm.nu_classification]):
             sv = self.support_vectors_
             if not self._sparse and sv.size > 0 and self._n_support.sum() != sv.shape[0]:
-                print(f"The internal representation of {self.__class__.__name__} was altered")
-                raise ValueError(
-                    f"The internal representation of {self.__class__.__name__} was altered"
-                )
+                raise ValueError("The internal representation "
+                                f"of {self.__class__.__name__} was altered")
 
         if self.break_ties and self.decision_function_shape == 'ovr' and \
                 len(self.classes_) > 2:
@@ -318,13 +316,11 @@ class BaseSVM(BaseEstimator, metaclass=ABCMeta):
                 "cannot use sparse input in %r trained on dense data"
                 % type(self).__name__)
 
-        if (module in [_backend.svm.classification,_backend.svm.nu_classification]):
+        if (module in [_backend.svm.classification, _backend.svm.nu_classification]):
             sv = self.support_vectors_
             if not self._sparse and sv.size > 0 and self._n_support.sum() != sv.shape[0]:
-                print(f"The internal representation of {self.__class__.__name__} was altered")
-                raise ValueError(
-                    f"The internal representation of {self.__class__.__name__} was altered"
-                )
+                raise ValueError("The internal representation "
+                                f"of {self.__class__.__name__} was altered")
 
         policy = _get_policy(queue, X)
         params = self._get_onedal_params(X)
