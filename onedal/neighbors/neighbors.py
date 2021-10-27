f9e5558b202a040ec3ab27629d778985bb6592fd
Dmitriy Kalyanov <dmitriy.kalyanov@intel.com>
2021-10-27 11:33:19 +0300

    apply comment

--- onedal/neighbors/neighbors.py
+++ onedal/neighbors/neighbors.py
@@ -135,16 +135,14 @@ class NeighborsCommonBase(metaclass=ABCMeta):
 
 class NeighborsBase(NeighborsCommonBase, metaclass=ABCMeta):
     def __init__(self, n_neighbors=None, radius=None,
-                 algorithm='auto', leaf_size=30, metric='minkowski',
-                 p=2, metric_params=None, n_jobs=None):
+                 algorithm='auto', metric='minkowski',
+                 p=2, metric_params=None):
         self.n_neighbors = n_neighbors
         self.radius=radius
         self.algorithm=algorithm
-        self.leaf_size=leaf_size
         self.metric=metric
         self.p=p
         self.metric_params=metric_params
-        self.n_jobs=n_jobs
 
     def _validate_targets(self, y, dtype):
         arr = _column_or_1d(y, warn=True)
