ff1f39178395fffe81aa6750a130936cdd2f22f9
Dmitriy Kalyanov <dmitriy.kalyanov@intel.com>
2021-11-01 11:13:14 +0300

    apply @vlad-nazarov's comment

--- onedal/neighbors/neighbors.cpp
+++ onedal/neighbors/neighbors.cpp
@@ -42,19 +42,43 @@ struct method2t {
 
 template <typename Ops>
 struct metric2t {
-    metric2t(const Ops& ops)
-        : ops(ops) {}
+    metric2t(const Ops& ops) : ops(ops) {}
 
     template <typename Float, typename Method>
     auto operator()(const py::dict& params) {
         using namespace knn;
 
         auto metric = params["metric"].cast<std::string>();
-        ONEDAL_PARAM_DISPATCH_VALUE(metric, "manhattan", ops, Float, Method, minkowski_distance::descriptor<Float>);
-        ONEDAL_PARAM_DISPATCH_VALUE(metric, "minkowski", ops, Float, Method, minkowski_distance::descriptor<Float>);
-        ONEDAL_PARAM_DISPATCH_VALUE(metric, "euclidean", ops, Float, Method, minkowski_distance::descriptor<Float>);
-        ONEDAL_PARAM_DISPATCH_VALUE(metric, "chebyshev", ops, Float, Method, chebyshev_distance::descriptor<Float>);
-        ONEDAL_PARAM_DISPATCH_VALUE(metric, "cosine", ops, Float, Method, cosine_distance::descriptor<Float>);
+        ONEDAL_PARAM_DISPATCH_VALUE(metric,
+                                    "manhattan",
+                                    ops,
+                                    Float,
+                                    Method,
+                                    minkowski_distance::descriptor<Float>);
+        ONEDAL_PARAM_DISPATCH_VALUE(metric,
+                                    "minkowski",
+                                    ops,
+                                    Float,
+                                    Method,
+                                    minkowski_distance::descriptor<Float>);
+        ONEDAL_PARAM_DISPATCH_VALUE(metric,
+                                    "euclidean",
+                                    ops,
+                                    Float,
+                                    Method,
+                                    minkowski_distance::descriptor<Float>);
+        ONEDAL_PARAM_DISPATCH_VALUE(metric,
+                                    "chebyshev",
+                                    ops,
+                                    Float,
+                                    Method,
+                                    chebyshev_distance::descriptor<Float>);
+        ONEDAL_PARAM_DISPATCH_VALUE(metric,
+                                    "cosine",
+                                    ops,
+                                    Float,
+                                    Method,
+                                    cosine_distance::descriptor<Float>);
         ONEDAL_PARAM_DISPATCH_THROW_INVALID_VALUE(metric);
     }
 
@@ -98,76 +122,78 @@ auto get_onedal_result_options(const py::dict& params) {
         ONEDAL_PARAM_DISPATCH_THROW_INVALID_VALUE(result_option);
 }
 
+template <typename Float, typename Method, typename Task, typename Distance>
+struct descriptor_creator;
+
+template <typename Float, typename Method, typename Distance>
+struct descriptor_creator<Float, Method, knn::task::classification, Distance> {
+    static auto get(const std::int64_t class_count, const std::int64_t neighbor_count) {
+        return knn::descriptor<Float, Method, knn::task::classification, Distance>(class_count,
+                                                                         neighbor_count);
+    }
+};
+
+template <typename Float, typename Method, typename Distance>
+struct descriptor_creator<Float, Method, knn::task::search, Distance> {
+    static auto get(const std::int64_t, const std::int64_t neighbor_count) {
+        return knn::descriptor<Float, Method, knn::task::search, Distance>(neighbor_count);
+    }
+};
+
 struct params2desc {
     template <typename Float, typename Method, typename Task, typename Distance>
     auto operator()(const pybind11::dict& params) {
         using namespace knn;
 
-        constexpr bool is_cls = std::is_same_v<Task, task::classification>;
-        constexpr bool is_srch = std::is_same_v<Task, task::search>;
-
         constexpr bool is_bf = std::is_same_v<Method, method::brute_force>;
-
         const auto class_count = params["class_count"].cast<std::int64_t>();
         const auto neighbor_count = params["neighbor_count"].cast<std::int64_t>();
 
-        if constexpr (is_cls) {
-            auto desc = descriptor<Float, Method, Task, Distance>(class_count, neighbor_count)
-                        .set_voting_mode(get_onedal_voting_mode(params))
-                        .set_result_options(get_onedal_result_options(params));
-            if constexpr (is_bf) {
-                desc.set_distance(get_distance_descriptor<Distance>(params));
-            }
-            return desc;
+        auto desc =
+            descriptor_creator<Float, Method, Task, Distance>::get(class_count, neighbor_count)
+                .set_voting_mode(get_onedal_voting_mode(params))
+                .set_result_options(get_onedal_result_options(params));
+        if constexpr (is_bf) {
+            desc.set_distance(get_distance_descriptor<Distance>(params));
         }
-        
-        else if constexpr (is_srch) {
-            auto desc = descriptor<Float, Method, Task, Distance>(neighbor_count)
-                        .set_voting_mode(get_onedal_voting_mode(params))
-                        .set_result_options(get_onedal_result_options(params));
-            if constexpr (is_bf) {
-                desc.set_distance(get_distance_descriptor<Distance>(params));
-            }
-            return desc;
-        }
+        return desc;
     }
 };
 
-template<typename Policy, typename Task>
+template <typename Policy, typename Task>
 struct init_train_ops_dispatcher {};
 
 template <typename Policy>
 struct init_train_ops_dispatcher<Policy, knn::task::classification> {
-void operator()(py::module_& m) {
-    using Task = knn::task::classification;
-    m.def("train",
-          [](const Policy& policy,
-             const py::dict& params,
-             const table& data,
-             const table& responses) {
-              using namespace knn;
-              using input_t = train_input<Task>;
+    void operator()(py::module_& m) {
+        using Task = knn::task::classification;
+        m.def("train",
+              [](const Policy& policy,
+                 const py::dict& params,
+                 const table& data,
+                 const table& responses) {
+                  using namespace knn;
+                  using input_t = train_input<Task>;
 
-              train_ops ops(policy, input_t{ data, responses }, params2desc{} );
-              return fptype2t { method2t { Task{}, metric2t{ ops } } }(params);
-          });
-}};
+                  train_ops ops(policy, input_t{ data, responses }, params2desc{});
+                  return fptype2t{ method2t{ Task{}, metric2t{ ops } } }(params);
+              });
+    }
+};
 
 template <typename Policy>
 struct init_train_ops_dispatcher<Policy, knn::task::search> {
-void operator()(py::module_& m) {
-    using Task = knn::task::search;
-    m.def("train",
-          [](const Policy& policy,
-             const py::dict& params,
-             const table& data) {
-              using namespace knn;
-              using input_t = train_input<Task>;
+    void operator()(py::module_& m) {
+        using Task = knn::task::search;
+        m.def("train", [](const Policy& policy, const py::dict& params, const table& data) {
+            using namespace knn;
+            using input_t = train_input<Task>;
 
-              train_ops ops(policy, input_t{ data }, params2desc{} );
-              return fptype2t { method2t { Task{}, metric2t{ ops } } }(params);
-          });
-}};
+            train_ops ops(policy, input_t{ data }, params2desc{});
+            return fptype2t{ method2t{ Task{}, metric2t{ ops } } }(params);
+        });
+    }
+};
 
 template <typename Policy, typename Task>
 void init_train_ops(py::module& m) {
@@ -184,8 +210,8 @@ void init_infer_ops(py::module_& m) {
               using namespace knn;
               using input_t = infer_input<Task>;
 
-              infer_ops ops(policy, input_t{ data, model }, params2desc{} );
-              return fptype2t { method2t { Task{}, metric2t{ ops } } }(params);
+              infer_ops ops(policy, input_t{ data, model }, params2desc{});
+              return fptype2t{ method2t{ Task{}, metric2t{ ops } } }(params);
           });
 }
 
@@ -194,16 +220,15 @@ void init_model(py::module_& m) {
     using namespace knn;
     using model_t = model<Task>;
 
-    auto cls =
-        py::class_<model_t>(m, "model")
-            .def(py::init())
-            .def(py::pickle(
-                [](const model_t& m) {
-                    return serialize(m);
-                },
-                [](const py::bytes& bytes) {
-                    return deserialize<model_t>(bytes);
-                }));
+    auto cls = py::class_<model_t>(m, "model")
+                   .def(py::init())
+                   .def(py::pickle(
+                       [](const model_t& m) {
+                           return serialize(m);
+                       },
+                       [](const py::bytes& bytes) {
+                           return deserialize<model_t>(bytes);
+                       }));
 }
 
 template <typename Task>
@@ -211,9 +236,7 @@ void init_train_result(py::module_& m) {
     using namespace knn;
     using result_t = train_result<Task>;
 
-    py::class_<result_t>(m, "train_result")
-        .def(py::init())
-        .DEF_ONEDAL_PY_PROPERTY(model, result_t);
+    py::class_<result_t>(m, "train_result").def(py::init()).DEF_ONEDAL_PY_PROPERTY(model, result_t);
 }
 
 template <typename Task>
@@ -249,8 +272,7 @@ ONEDAL_PY_INIT_MODULE(neighbors) {
     using namespace knn;
     using namespace dal::detail;
 
-    using task_list =
-        types<task::classification, task::search>;
+    using task_list = types<task::classification, task::search>;
     auto sub = m.def_submodule("neighbors");
 
     ONEDAL_PY_INSTANTIATE(init_train_ops, sub, policy_list, task_list);
