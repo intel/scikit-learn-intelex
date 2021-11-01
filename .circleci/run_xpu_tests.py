e830a2f5936d5fc03aa986471063c2ba4ffba5f0
pavel.yakovlev <pyakovle@nnlmkl330.inn.intel.com>
2021-11-01 22:59:30 +0300

    turn on verbose for gpu skelarn testing

--- .circleci/run_xpu_tests.py
+++ .circleci/run_xpu_tests.py
@@ -85,5 +85,5 @@ if __name__ == '__main__':
             )
     else:
         pytest.main(
-            pytest_params + ["--pyargs", "sklearn"] + yml_deselected_tests
+            pytest_params + [ "-vv" ,"--pyargs", "sklearn"] + deselected_tests
         )
