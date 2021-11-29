7ff4ba3244960b9f99da6f3766228934f56bbff6
Vladislav Nazarov <vladislav.nazarov@intel.com>
2021-11-29 09:11:10 -0800

    Update xpu tests

--- .circleci/deselect_tests.py
+++ .circleci/deselect_tests.py
@@ -59,6 +59,40 @@ def filter_by_version(entry, sk_ver):
         return test_name
     return None
 
+def create_pytest_switches(filename, absolute, reduced, public, gpu):
+    pytest_switches = []
+    if os.path.exists(filename):
+        with open(filename, 'r') as fh:
+            dt = yaml_load(fh, Loader=FullLoader)
+
+        if absolute:
+            base_dir = os.path.relpath(
+                os.path.dirname(sklearn.__file__),
+                os.path.expanduser('~')) + '/'
+        else:
+            base_dir = ""
+
+        filtered_deselection = [
+            filter_by_version(test_name, sklearn_version)
+            for test_name in dt.get('deselected_tests', [])]
+        if reduced:
+            filtered_deselection.extend(
+                [filter_by_version(test_name, sklearn_version)
+                 for test_name in dt.get('reduced_tests', [])])
+        if public:
+            filtered_deselection.extend(
+                [filter_by_version(test_name, sklearn_version)
+                 for test_name in dt.get('public', [])])
+        if gpu:
+            filtered_deselection.extend(
+                [filter_by_version(test_name, sklearn_version)
+                 for test_name in dt.get('gpu', [])])
+        pytest_switches = []
+        for test_name in filtered_deselection:
+            if test_name:
+                pytest_switches.extend(["--deselect", base_dir + test_name])
+    return pytest_switches
+
 
 if __name__ == '__main__':
     argParser = argparse.ArgumentParser(
@@ -75,31 +109,4 @@ if __name__ == '__main__':
 
     fn = args.conf_file[0]
     if os.path.exists(fn):
-        with open(fn, 'r') as fh:
-            dt = yaml_load(fh, Loader=FullLoader)
-
-        if args.absolute:
-            base_dir = os.path.relpath(
-                os.path.dirname(sklearn.__file__),
-                os.path.expanduser('~')) + '/'
-        else:
-            base_dir = ""
-
-        filtered_deselection = [
-            filter_by_version(test_name, sklearn_version)
-            for test_name in dt.get('deselected_tests', [])]
-        if args.reduced:
-            filtered_deselection.extend(
-                [filter_by_version(test_name, sklearn_version)
-                 for test_name in dt.get('reduced_tests', [])])
-        if args.public:
-            filtered_deselection.extend(
-                [filter_by_version(test_name, sklearn_version)
-                 for test_name in dt.get('public', [])])
-        if args.gpu:
-            filtered_deselection.extend(
-                [filter_by_version(test_name, sklearn_version)
-                 for test_name in dt.get('gpu', [])])
-        pytest_switches = ["--deselect " + base_dir + test_name
-                           for test_name in filtered_deselection if test_name]
-        print(" ".join(pytest_switches))
+        print(" ".join(create_pytest_switches(fn, args.absolute, args.reduced, args.public, args.gpu)))
