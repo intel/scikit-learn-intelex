ca02f9c82ec806b2620fbc25b3c72d76376cedaf
Vladislav Nazarov <vladislav.nazarov@intel.com>
2021-11-29 23:42:17 -0800

    Update syntax

--- .circleci/run_xpu_tests.py
+++ .circleci/run_xpu_tests.py
@@ -80,7 +80,7 @@ if __name__ == '__main__':
 
     if args.device == "gpu":
         from daal4py.oneapi import sycl_context
-        with sycl_context(args.device, host_offload_on_fail=True)
+        with sycl_context(args.device, host_offload_on_fail=True):
             pytest.main(
                 pytest_params + ["--pyargs", "sklearn"] + deselected_tests + yml_deselected_tests
             )
