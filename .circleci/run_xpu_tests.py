842c2b325b41a8b9e6e2a993178191eaee97ad85
vlad-nazarov <vladislav.nazarov@intel.com>
2021-12-08 01:06:51 +0300

    Add more verbose

--- .circleci/run_xpu_tests.py
+++ .circleci/run_xpu_tests.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
     yml_deselected_tests = yml_deselected_tests + deselected_tests
 
     pytest_params = [
-        "-ra", "--disable-warnings"
+        "-ra", "--disable-warnings", "-v", "-s"
     ]
 
     if not args.quiet:
