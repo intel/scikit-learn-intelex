5c637d4e0b2488a53adfaafac1a6b9d51e85a8ab
Aleksei Khomenko <aleksei.khomenko@intel.com>
2022-01-11 13:27:03 +0300

    Update copyrights to 2022 year: fix

--- .ci/scripts/install_windows.bat
+++ .ci/scripts/install_windows.bat
@@ -1,18 +1,6 @@
-rem ============================================================================
-rem Copyright 2020-2022 Intel Corporation
-rem
-rem Licensed under the Apache License, Version 2.0 (the "License");
-rem you may not use this file except in compliance with the License.
-rem You may obtain a copy of the License at
-rem
-rem     http://www.apache.org/licenses/LICENSE-2.0
-rem
-rem Unless required by applicable law or agreed to in writing, software
-rem distributed under the License is distributed on an "AS IS" BASIS,
-rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-rem See the License for the specific language governing permissions and
-rem limitations under the License.
-rem ============================================================================
+REM SPDX-FileCopyrightText: 2020 Intel Corporation
+REM
+REM SPDX-License-Identifier: MIT
 
 set URL=%1
 set COMPONENTS=%2
