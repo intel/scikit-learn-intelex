1183fdd813a1b2d0a09b3d62a9bde58e029fd00c
md-shafiul-alam <md.shafiul.alam@intel.com>
2022-10-20 15:27:19 -0400

    pep8 checks

--- daal4py/sklearn/cluster/_dbscan.py
+++ daal4py/sklearn/cluster/_dbscan.py
@@ -30,7 +30,6 @@ from .._device_offload import support_usm_ndarray
 from .._utils import sklearn_check_version
 import numbers
 
-
 def _daal_dbscan(X, eps=0.5, min_samples=5, sample_weight=None):
     ww = make2d(sample_weight) if sample_weight is not None else None
     XX = make2d(X)
