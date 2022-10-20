59d8d85e8816361f325d2696f460e854ca929d4e
md-shafiul-alam <md.shafiul.alam@intel.com>
2022-10-20 12:09:17 -0400

    pep8

--- daal4py/sklearn/linear_model/_ridge.py
+++ daal4py/sklearn/linear_model/_ridge.py
@@ -33,20 +33,17 @@ def _daal4py_check(self, X, y):
     
     # does not check for array of alphas in case of multi-target
     if isinstance(self.alpha, numbers.Number) and self.alpha < 0.0:
-        raise ValueError(f"alpha == {self.alpha}, "
-                        "must be >= 0.0")
+        raise ValueError(f"alpha == {self.alpha}, must be >= 0.0")
     elif isinstance(self.alpha, str):
         raise TypeError("alpha must be an instance of float, not str")
 
     if isinstance(self.max_iter, int) and self.max_iter < 1:
-        raise ValueError(f"max_iter == {self.max_iter}, "
-                        "must be >= 1.")
+        raise ValueError(f"max_iter == {self.max_iter}, must be >= 1.")
     elif isinstance(self.max_iter, str):
         raise TypeError("max_iter must be an instance of int, not str")
     
     if isinstance(self.tol, numbers.Number) and self.tol < 0.0:
-        raise ValueError(f"tol == {self.tol}, "
-                        "must be >= 0.0")
+        raise ValueError(f"tol == {self.tol}, must be >= 0.0")
     elif isinstance(self.tol, str):
         raise TypeError("tol must be an instance of float, not str")
 
