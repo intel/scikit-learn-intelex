8646fda0f9e1ef3b62fa46014b144b3f5e41ad53
Alexander Andreev <alexander.andreev@intel.com>
2022-11-22 16:37:11 +0000

    Apply pep8 n2

--- scripts/build_backend.py
+++ scripts/build_backend.py
@@ -143,8 +143,7 @@ def custom_build_cmake_clib(iface, cxx=None):
 
     if iface == 'dpc':
         cmake_args += ["-DCMAKE_C_COMPILER_WORKS=1",
-                       "-DCMAKE_CXX_COMPILER_WORKS=1"
-        ]
+                       "-DCMAKE_CXX_COMPILER_WORKS=1"]
 
     import multiprocessing
     cpu_count = multiprocessing.cpu_count()
