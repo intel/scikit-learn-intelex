dd7915be102c93475eca588adacf55d0edab0e2b
Ethan Glaser <ethan.glaser@intel.com>
2022-12-08 09:10:52 -0500

    undoing last commit

--- setup.py
+++ setup.py
@@ -188,7 +188,7 @@ def get_build_options():
         if IS_WIN:
             include_dir_plat.append(
                 jp(os.environ.get('ICPP_COMPILER16', ''), 'compiler', 'include'))
-            eca += ['-std=c++17', '/MT']
+            eca += ['-std=c++17', '/MD']
     elif not using_intel and IS_WIN:
         eca += ['-wd4267', '-wd4244', '-wd4101', '-wd4996', '/std:c++17']
     else:
@@ -323,7 +323,7 @@ def build_oneapi_backend():
         targetname='oneapi_backend',
         targetprefix='' if IS_WIN else 'lib',
         targetsuffix='.dll' if IS_WIN else '.so',
-        libs=get_libs('daal') + ['OpenCL', 'onedal_sycl'],
+        libs=get_libs('daal') + ['OpenCL', 'onedal_sycl', 'sycl'],
         libdirs=ONEDAL_LIBDIRS,
         includes=includes,
         eca=eca,
