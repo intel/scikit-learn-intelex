b19c6de3b033976ac14fb0bf8a43379ace70850d
Alexander Andreev <alexander.andreev@intel.com>
2022-12-12 12:01:29 +0000

    Change matrix; update deps; add sklearnex/* tests

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -56,4 +56,8 @@ pytest --verbose --pyargs ${daal4py_dir}/daal4py/sklearn \
     --deselect="daal4py/sklearn/ensemble/tests/test_decision_forest.py::test_mse_regressor_big_estimators_iris[8000]"
 return_code=$(($return_code + $?))
 
+echo "Pytest of sklearnex running ..."
+pytest --verbose --pyargs ${daal4py_dir}/sklearnex
+return_code=$(($return_code + $?))
+
 exit $return_code
