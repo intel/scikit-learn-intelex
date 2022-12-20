fb48e0e670452660adc75bfa62b9e117c743d3b1
Md Shafiul Alam <md.shafiul.alam@intel.com>
2022-12-19 21:04:27 -0800

    remove redundancy

--- setup_sklearnex.py
+++ setup_sklearnex.py
@@ -89,7 +89,6 @@ setup(name="scikit-learn-intelex",
           'sklearnex.model_selection',
           'sklearnex.neighbors',
           'sklearnex.svm',
-          'sklearnex.decomposition',
           'sklearnex.utils'
       ]),
       )
