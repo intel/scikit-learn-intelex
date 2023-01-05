b48509a8846df62b0f78e8640222b62a1a516e54
Alexander Andreev <alexander.andreev@intel.com>
2023-01-05 13:32:55 +0000

    Fix license

--- sklearnex/neighbors/lof.py
+++ sklearnex/neighbors/lof.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #===============================================================================
-# Copyright 2022 Intel Corporation
+# Copyright 2023 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
