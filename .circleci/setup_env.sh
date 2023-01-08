e360058b53facaecf41a8b5375b7c1e3a4038e60
Nikolay Petrov <nikolay.a.petrov@intel.com>
2023-01-08 15:51:34 +0000

    Update setup_env.sh

--- .circleci/setup_env.sh
+++ .circleci/setup_env.sh
@@ -21,7 +21,7 @@ export PATH=~/miniconda/bin:$PATH
 hash -r
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
-conda create -n bld python=3.7 conda-build
+conda create -n bld python=3.10 conda-build
 source activate bld
 conda install -q scipy pytest pandas pyyaml joblib numpydoc cython jinja2 numpy clang-tools pybind11 cmake
 conda install -q --override-channels -c intel dal dal-include
