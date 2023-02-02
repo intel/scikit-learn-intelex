cdac26344b65bf8e1ab95f7e179665f216a8b716
y <md.shafiul.alam@intel.com>
2023-02-02 11:13:31 -0800

    remove test for components match with sklearn

--- sklearnex/decomposition/tests/test_pca.py
+++ sklearnex/decomposition/tests/test_pca.py
@@ -25,7 +25,3 @@ def test_sklearnex_import():
     pca = PCA(n_components=2, svd_solver='full').fit(X)
     assert 'sklearnex' in pca.__module__
     assert_allclose(pca.singular_values_, [[6.30061236, 0.54980354]])
-    assert_allclose(
-        pca.components_,
-        [[-0.83849223, -0.54491356], [0.54491356, -0.83849223]]
-    )
