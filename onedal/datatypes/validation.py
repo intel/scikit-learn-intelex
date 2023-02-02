7ac2c93f722a1d372e9e3779714405a385acb5fb
Alexander Andreev <alexander.andreev@intel.com>
2023-02-02 19:43:29 +0000

    Move import and pass input_name in onedal/datatypes/validation.py

--- onedal/datatypes/validation.py
+++ onedal/datatypes/validation.py
@@ -19,6 +19,7 @@ import warnings
 from scipy import sparse as sp
 from scipy.sparse import issparse, dok_matrix, lil_matrix
 from sklearn.preprocessing import LabelEncoder
+from sklearn.utils.validation import check_array
 from collections.abc import Sequence
 from numbers import Integral
 
@@ -99,12 +100,11 @@ def _validate_targets(y, class_weight, dtype):
 
 def _check_array(array, dtype="numeric", accept_sparse=False, order=None,
                  copy=False, force_all_finite=True,
-                 ensure_2d=True, accept_large_sparse=True):
-    # TODO
-    from sklearn.utils.validation import check_array
+                 ensure_2d=True, accept_large_sparse=True, input_name=''):
     array = check_array(array=array, dtype=dtype, accept_sparse=accept_sparse,
                         order=order, copy=copy, force_all_finite=force_all_finite,
-                        ensure_2d=ensure_2d, accept_large_sparse=accept_large_sparse)
+                        ensure_2d=ensure_2d, accept_large_sparse=accept_large_sparse,
+                        input_name='X')
 
     if sp.isspmatrix(array):
         return array
@@ -130,7 +130,8 @@ def _check_X_y(X, y, dtype="numeric", accept_sparse=False, order=None, copy=Fals
                      dtype=dtype, order=order, copy=copy,
                      force_all_finite=force_all_finite,
                      ensure_2d=ensure_2d,
-                     accept_large_sparse=accept_large_sparse)
+                     accept_large_sparse=accept_large_sparse,
+                     input_name='X')
 
     y = _column_or_1d(y, warn=True)
     if y_numeric and y.dtype.kind == 'O':
