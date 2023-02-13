77ebf64216cb6a8f34f2796027bca7070f334428
Nikita Kulikov <nikita.kulikov@intel.com>
2023-02-13 02:28:06 -0800

    Fixes for PEP8

--- onedal/datatypes/validation.py
+++ onedal/datatypes/validation.py
@@ -67,8 +67,8 @@ def _compute_class_weight(class_weight, classes, y):
         if not all(np.in1d(classes, le.classes_)):
             raise ValueError("classes should have valid labels that are in y")
 
-        weight = len(y_) / (len(le.classes_) * \
-                            np.bincount(y_ind).astype(np.float64))
+        y_bin = np.bincount(y_ind).astype(np.float64)
+        weight = len(y_) / (len(le.classes_) * y_bin)
     else:
         # user-defined dictionary
         weight = np.ones(classes.shape[0], dtype=np.float64, order='C')
