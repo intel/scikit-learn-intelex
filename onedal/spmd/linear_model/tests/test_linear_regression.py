d9893774a1e3fe10f5e452ed2a80cdf2f7dc9b99
Nikita Kulikov <nikita.kulikov@intel.com>
2023-02-14 07:30:42 -0800

    Minor

--- onedal/spmd/linear_model/tests/test_linear_regression.py
+++ onedal/spmd/linear_model/tests/test_linear_regression.py
@@ -24,8 +24,9 @@ if daal_check_version((2023, 'P', 100)):
 
     from numpy.testing import assert_allclose
 
-    from onedal.spmd.linear_model import LinearRegression
     from onedal.tests.utils._device_selection import get_queues
+    from sklearn.linear_model import LinearRegression as LinearRegressionGtr
+    from onedal.spmd.linear_model import LinearRegression as LinearRegressionSpmd
 
     from sklearn.datasets import load_diabetes
     from sklearn.metrics import mean_squared_error
@@ -42,42 +43,89 @@ if daal_check_version((2023, 'P', 100)):
         results = ([],) * n_arrays
 
         for b in range(n_ranks):
+            is_last = b + 1 == n_ranks
             for a in range(n_arrays):
                 first = block * b
-                last = min(first + block, n_samples)
-                shard = arrays[a][:, first : last]
+
+                if is_last:
+                    last = n_samples
+                else:
+                    last = first + block
+
+                shard = arrays[a][:, first:last]
                 results[a].append(shard)
 
         return results
 
-    def run_spmd_training(queue, params, X, y):
-        comm = MPI.COMM_WORLD
-        rank = comm.Get_rank()
-        size = comm.get_size()
+    def run_spmd_training(comm, queue, params, X, y):
+        rank, size = comm.Get_rank(), comm.Get_size()
+        Xs, ys = split_datasets(size, X, y)
 
-        Xs, ys = split_datasets(X, y)
-
-        model = LinearRegression(**params)
+        model = LinearRegressionSpmd(**params)
         model.fit(Xs[rank], ys[rank], queue)
 
-        coef = np.array(model.coef_)
+        coef = model.coef_
 
-        test = (size + rank) % size 
+        test = (rank + 1) % size 
         Xt, yt = Xs[test], yt[test]
 
+        n_samples = Xt.shape[0]
         yp = model.predict(Xt, queue)
-        yp = np.array(yp)
+        error = mean_squared_error(yt, yp)
 
-        return mean_squared_error(yt, yp)
+        return (n_samples, coef, error)
+
+    def run_groundtruth(params, X, y):
+        model = LinearRegressionGtr(**params)
+        model.fit(X, y)
+
+        coef = model.coef_
+
+        yp = model.predict(X)
+        error = mean_squared_error(y, yp)
+
+        return (coef, error)
+
+    def run_on_dataset(comm, queue, params, X, y):
+        gtr_coef, gtr_err = run_groundtruth(params, X, y)
+        spmd_coef, spmd_err, n_samples = 
+            run_spmd_training(comm, queue, params, X, y)
+
+        assert_allclose(gtr_coef, spmd_coef)
+
+        mse = np.array(spmd_err, dtype = np.float64)
+        global_mse = np.zeros(comm.Get_size(), dtype = np.float64)
+        comm.Allgather([mse, MPI.MPI_DOUBLE], [global_mse, MPI.MPI_DOUBLE])
+
+        nss = np.array(n_samples, dtype = np.float64)
+        global_nss = np.zeros(comm.Get_size(), dtype = np.float64)
+        comm.Allgather([nss, MPI.MPI_DOUBLE], [global_nss, MPI.MPI_DOUBLE])
+
+        mse = np.sum(global_mse * global_nss) / np.sum(global_nss)
+
+        assert_close(mse, gtr_err)
+
+
+
+
                 
     @pytest.mark.parametrize('queue', get_queues())
     def test_diabetes(queue):
+        comm = MPI.COMM_WORLD
+        params = { 'fit_intercept' : True }
         X, y = load_diabetes(return_X_y=True)
+        run_on_dataset(comm, queue, params, X, y)
 
-        model = LinearRegression(fit_intercept=True)
-        model.fit(X_train, y_train, queue=queue)
-        y_pred = model.predict(X_test, queue=queue)
-        assert mean_squared_error(y_test, y_pred) < 2396
+    @pytest.mark.parametrize('queue', get_queues())
+    def test_generated(queue):
+        comm = MPI.COMM_WORLD
+        rank, size = comm.Get_rank(), comm.Get_size()
+        n_samples, n_features = size * 10, size * 11
+
+        params = { 'fit_intercept' : True }
+        X, y = make_regression(n_samples = n_samples, 
+                               n_features = n_features)
+        run_on_dataset(comm, queue, params, X, y)
 
 
 
