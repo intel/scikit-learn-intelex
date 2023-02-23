e993378c4ddf662ffd5288e051f81c2cabe9212d
Nikita Kulikov <nikita.kulikov@intel.com>
2023-02-23 10:24:50 -0800

    Minor

--- onedal/common/_spmd_policy.py
+++ onedal/common/_spmd_policy.py
@@ -17,14 +17,14 @@
 from onedal import _backend, _is_dpc_backend
 import sys
 
+if _is_dpc_backend:
+    class _SPMDDataParallelInteropPolicy(_backend.spmd_data_parallel_policy):
+        def __init__(self, queue):
+            self._queue = queue
+            super().__init__(self._queue)
 
-class _SPMDDataParallelInteropPolicy(_backend.spmd_data_parallel_policy):
-    def __init__(self, queue):
-        self._queue = queue
-        super().__init__(self._queue)
 
-
-def _get_spmd_policy(queue):
-    # TODO:
-    # cases when queue is None
-    return _SPMDDataParallelInteropPolicy(queue)
+    def _get_spmd_policy(queue):
+        # TODO:
+        # cases when queue is None
+        return _SPMDDataParallelInteropPolicy(queue)
