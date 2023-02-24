0ae810becd434521d78b6021ebc7a378d813d26b
Nikita Kulikov <nikita.kulikov@intel.com>
2023-02-24 02:39:37 -0800

    Minor

--- onedal/__init__.py
+++ onedal/__init__.py
@@ -43,7 +43,9 @@ __all__ = ['decomposition', 'neighbors', 'primitives', 'svm']
 
 if _is_dpc_backend:
     __all__.append('spmd')
-    __all__.append('spmd.linear_model')
 
 if daal_check_version((2023, 'P', 100)):
     __all__.append('linear_model')
+
+    if _is_dpc_backend:
+        __all__.append('spmd.linear_model')
