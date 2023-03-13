91307a92fc53cbe2b8013301f42356722d7543bf
ethanglaser <ethan.glaser@intel.com>
2023-03-13 06:40:11 -0700

    removing commented lines

--- onedal/neighbors/neighbors.cpp
+++ onedal/neighbors/neighbors.cpp
@@ -315,10 +315,8 @@ ONEDAL_PY_INIT_MODULE(neighbors) {
 
     ONEDAL_PY_INSTANTIATE(init_train_ops, sub, policy_list, task_list);
 #ifdef ONEDAL_DATA_PARALLEL_SPMD
-    //ONEDAL_PY_INSTANTIATE(init_train_ops, sub, policy_list_spmd, task_list);
     ONEDAL_PY_INSTANTIATE(init_infer_ops, sub, policy_list_spmd, task_list);
 #else // ONEDAL_DATA_PARALLEL_SPMD
-    //ONEDAL_PY_INSTANTIATE(init_train_ops, sub, policy_list, task_list);
     ONEDAL_PY_INSTANTIATE(init_infer_ops, sub, policy_list, task_list);
 #endif // ONEDAL_DATA_PARALLEL_SPMD
 
