3e6b7e37eeb6a70848baf720f6ee68c7a6d9956f
jui.mhatre <jmhatre@jflmkl116.jf.intel.com>
2023-03-14 08:11:46 -0700

    flake8 changes in examples

--- examples/sklearnex/spmd_pca.py
+++ examples/sklearnex/spmd_pca.py
@@ -1,5 +1,4 @@
-
-#===============================================================================
+# ===============================================================================
 # Copyright 2023 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#===============================================================================
+# ===============================================================================
 
 import numpy as np
 from mpi4py import MPI
@@ -23,9 +22,10 @@ from onedal.spmd.decomposition import PCA as PCASpmd
 
 def get_data(data_seed, params_spmd):
     drng = np.random.default_rng(data_seed)
-    X = drng.uniform(-7, 7, size=(params_spmd['ns'], params_spmd['nf']))
-    y = drng.uniform(-7, 7, size=(params_spmd['ns'], params_spmd['nr']))
+    X = drng.uniform(-7, 7, size=(params_spmd["ns"], params_spmd["nf"]))
+    y = drng.uniform(-7, 7, size=(params_spmd["ns"], params_spmd["nr"]))
     return X, y
+
 
 q = dpctl.SyclQueue("gpu")
 comm = MPI.COMM_WORLD
@@ -40,4 +40,3 @@ pcaspmd = PCASpmd(n_components=2).fit(X_spmd, y_spmd, q)
 
 print(f"Singular values on rank {rank}:\n", pcaspmd.singular_values_)
 print(f"Explained variance Ratio on rank {rank}:\n", pcaspmd.explained_variance_ratio_)
-
