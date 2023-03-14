410e2fd65883c541880977bea07a1605e73d0ee8
jui.mhatre <jmhatre@jflmkl116.jf.intel.com>
2023-03-14 07:57:59 -0700

    simplifying examples

--- examples/sklearnex/spmd_pca.py
+++ examples/sklearnex/spmd_pca.py
@@ -18,24 +18,26 @@
 import numpy as np
 from mpi4py import MPI
 import dpctl
-from numpy.testing import assert_allclose
 from onedal.spmd.decomposition import PCA as PCASpmd
-from sklearn.decomposition import PCA as PCAstock
 
-if __name__ == "__main__":
-    q = dpctl.SyclQueue("gpu")
 
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
+def get_data(data_seed, params_spmd):
+    drng = np.random.default_rng(data_seed)
+    X = drng.uniform(-7, 7, size=(params_spmd['ns'], params_spmd['nf']))
+    y = drng.uniform(-7, 7, size=(params_spmd['ns'], params_spmd['nr']))
+    return X, y
 
-    params_spmd = {"ns": 15, "nf": 21, "nr": 23}
+q = dpctl.SyclQueue("gpu")
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
 
-    drng = np.random.default_rng(0)
-    Xsp = drng.uniform(-7, 7, size=(15, 12))
-    ysp = drng.uniform(-7, 7, size=(15, 23))
 
-    pcaspmd = PCASpmd(n_components=2).fit(Xsp, ysp, q)
+params_spmd = {"ns": 15, "nf": 21, "nr": 23}
+X_spmd, y_spmd = get_data(0, params_spmd)
 
-    pcastock = PCAstock(n_components=2).fit(Xsp)
-    assert_allclose(pcaspmd.singular_values_, pcastock.singular_values_)
+pcaspmd = PCASpmd(n_components=2).fit(X_spmd, y_spmd, q)
+
+print(f"Singular values on rank {rank}:\n", pcaspmd.singular_values_)
+print(f"Explained variance Ratio on rank {rank}:\n", pcaspmd.explained_variance_ratio_)
+
