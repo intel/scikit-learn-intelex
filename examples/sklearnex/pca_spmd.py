2405630020da1563646aef768f12951c6f0d2dc3
jmhatre-work <122313877+jmhatre-work@users.noreply.github.com>
2023-03-22 14:07:46 -0400

    PCA SPMD python interfaces (#1211)
    * Initial pca spmd changes
    
    * pca spmd example
    
    * flake8 fixes
    
    * simplifying examples
    
    * flake8 fixes
    
    * flake8 changes in examples
    
    * fixing pca call from sklearnex than onedal
    
    * removing y from fit params in pca
    
    * y remove from examples
    
    * flake8 changes to examples
    
    * flake8 changes after pull
    
    * Fixes for PR comments on examples and class names
    
    * Update runexamples with pca_spmd
    
    * Update onedal/__init__.py
    
    Co-authored-by: Samir Nasibli <samir.nasibli@intel.com>
    
    * Update setup.py
    
    Co-authored-by: Samir Nasibli <samir.nasibli@intel.com>
    
    * Add featues and samples in example
    
    ---------
    
    Co-authored-by: jui.mhatre <jmhatre@jflmkl116.jf.intel.com>
    Co-authored-by: Alexander Andreev <alexander.andreev@intel.com>
    Co-authored-by: Samir Nasibli <samir.nasibli@intel.com>

---
+++ examples/sklearnex/pca_spmd.py
@@ -0,0 +1,40 @@
+# ===============================================================================
+# Copyright 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+
+import numpy as np
+from mpi4py import MPI
+import dpctl
+from sklearnex.spmd.decomposition import PCA
+
+
+def get_data(data_seed):
+    ns, nf = 300, 30
+    drng = np.random.default_rng(data_seed)
+    X = drng.random(size=(ns, nf))
+    return X
+
+
+q = dpctl.SyclQueue("gpu")
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+X = get_data(rank)
+
+pca = PCA(n_components=2).fit(X, q)
+
+print(f"Singular values on rank {rank}:\n", pca.singular_values_)
+print(f"Explained variance Ratio on rank {rank}:\n", pca.explained_variance_ratio_)
