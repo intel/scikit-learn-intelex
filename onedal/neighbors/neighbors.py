b7ce115fc9a5e0fda77c8de415d9e4a6096df47f
Nikita Kulikov <nikita.kulikov@intel.com>
2023-03-23 11:02:33 -0700

    Fix

--- onedal/neighbors/neighbors.py
+++ onedal/neighbors/neighbors.py
@@ -557,6 +557,7 @@ class KNeighborsRegressor(NeighborsBase, RegressorMixin):
         params = self._get_onedal_params(X, y)
         train_alg_regr = _backend.neighbors.regression.train
         train_alg_srch = _backend.neighbors.search.train
+        
         if gpu_device:
             return train_alg_regr(policy, params, *to_table(X, y)).model
         return train_alg_srch(policy, params, to_table(X)).model
@@ -581,6 +582,7 @@ class KNeighborsRegressor(NeighborsBase, RegressorMixin):
             model = self._onedal_model
         else:
             model = self._create_model(backend)
+        params['fptype'] = 'float' if X.dtype == np.float32 else 'double'
         result = backend.infer(policy, params, model, to_table(X))
 
         return result
