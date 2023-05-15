63ca5f5cc2dec51f0ef47610ece4709d04525b1a
Dmitry Razdoburdin <>
2023-05-15 02:11:44 -0700

    compatibility with older versions of oneDAL

--- src/gbt_model_builder.pyx
+++ src/gbt_model_builder.pyx
@@ -18,9 +18,6 @@
 # The model builder object is retrieved through calling model_builder.
 # We will extend this once we know how other model builders will work in DAAL
 
-from daal4py.sklearn._utils import daal_check_version
-missing_values_are_supported = daal_check_version((2023, 'P', 200))
-
 cdef extern from "gbt_model_builder.h":
     ctypedef size_t c_gbt_clf_node_id
     ctypedef size_t c_gbt_clf_tree_id
@@ -30,23 +27,21 @@ cdef extern from "gbt_model_builder.h":
     cdef size_t c_gbt_clf_no_parent
     cdef size_t c_gbt_reg_no_parent
 
+    cdef gbt_classification_ModelPtr * get_gbt_classification_model_builder_model(c_gbt_classification_model_builder *)
+    cdef gbt_regression_ModelPtr * get_gbt_regression_model_builder_model(c_gbt_regression_model_builder *)
+
     cdef cppclass c_gbt_classification_model_builder:
         c_gbt_classification_model_builder(size_t nFeatures, size_t nIterations, size_t nClasses) except +
         c_gbt_clf_tree_id createTree(size_t nNodes, size_t classLabel)
         c_gbt_clf_node_id addLeafNode(c_gbt_clf_tree_id treeId, c_gbt_clf_node_id parentId, size_t position, double response)
-        c_gbt_clf_node_id addSplitNode(c_gbt_clf_tree_id treeId, c_gbt_clf_node_id parentId, size_t position, size_t featureIndex, double featureValue)
-        c_gbt_clf_node_id addSplitNode(c_gbt_clf_tree_id treeId, c_gbt_clf_node_id parentId, size_t position, size_t featureIndex, double featureValue, int default_left)
 
     cdef cppclass c_gbt_regression_model_builder:
         c_gbt_regression_model_builder(size_t nFeatures, size_t nIterations) except +
         c_gbt_reg_tree_id createTree(size_t nNodes)
         c_gbt_reg_node_id addLeafNode(c_gbt_reg_tree_id treeId, c_gbt_reg_node_id parentId, size_t position, double response)
-        c_gbt_reg_node_id addSplitNode(c_gbt_reg_tree_id treeId, c_gbt_reg_node_id parentId, size_t position, size_t featureIndex, double featureValue)
-        c_gbt_reg_node_id addSplitNode(c_gbt_reg_tree_id treeId, c_gbt_reg_node_id parentId, size_t position, size_t featureIndex, double featureValue, int default_left)
 
-    cdef gbt_classification_ModelPtr * get_gbt_classification_model_builder_model(c_gbt_classification_model_builder *)
-    cdef gbt_regression_ModelPtr * get_gbt_regression_model_builder_model(c_gbt_regression_model_builder *)
-
+    cdef c_gbt_clf_node_id clfAddSplitNodeWrapper(c_gbt_classification_model_builder * c_ptr, c_gbt_clf_tree_id treeId, c_gbt_clf_node_id parentId, size_t position, size_t featureIndex, double featureValue, int defaultLeft)
+    cdef c_gbt_reg_node_id regAddSplitNodeWrapper(c_gbt_regression_model_builder     * c_ptr, c_gbt_reg_tree_id treeId, c_gbt_reg_node_id parentId, size_t position, size_t featureIndex, double featureValue, int defaultLeft)
 
 cdef class gbt_classification_model_builder:
     '''
@@ -91,12 +86,10 @@ cdef class gbt_classification_model_builder:
         :param size_t position: position in parent (e.g. 0 for left and 1 for right child in a binary tree)
         :param size_t feature_index: feature index for spliting
         :param double feature_value: feature value for spliting
+        :param int default_left: default behaviour in case of missing value
         :rtype: node identifier
         '''
-        if missing_values_are_supported:
-            return self.c_ptr.addSplitNode(tree_id, parent_id, position, feature_index, feature_value, default_left)
-        else:
-            return self.c_ptr.addSplitNode(tree_id, parent_id, position, feature_index, feature_value)
+        return clfAddSplitNodeWrapper(self.c_ptr, tree_id, parent_id, position, feature_index, feature_value, default_left)
 
     def model(self):
         '''
@@ -151,12 +144,10 @@ cdef class gbt_regression_model_builder:
         :param size_t position: position in parent (e.g. 0 for left and 1 for right child in a binary tree)
         :param size_t feature_index: feature index for spliting
         :param double feature_value: feature value for spliting
+        :param int default_left: default behaviour in case of missing value
         :rtype: node identifier
         '''
-        if missing_values_are_supported:
-            return self.c_ptr.addSplitNode(tree_id, parent_id, position, feature_index, feature_value, default_left)
-        else:
-            return self.c_ptr.addSplitNode(tree_id, parent_id, position, feature_index, feature_value)
+        return regAddSplitNodeWrapper(self.c_ptr, tree_id, parent_id, position, feature_index, feature_value, default_left)
 
     def model(self):
         '''
