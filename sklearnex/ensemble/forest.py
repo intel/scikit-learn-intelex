1141df10a62e6c1d1e35f12548ad32390f651c37
icfaust <icfaust@gmail.com>
2023-05-19 13:18:42 -0700

    PEP8 test

--- sklearnex/ensemble/forest.py
+++ sklearnex/ensemble/forest.py
@@ -538,8 +538,8 @@ class ExtraTreesClassifier(sklearn_ExtraTreesClassifier, BaseTree):
             ready, X, y, sample_weight = self._onedal_fit_ready(_patching_status, *data)
 
             dal_ready = ready and _patching_status.and_conditions([
-                (self.splitter_mode == 'random' and \
-                    not daal_check_version((2023, 'P', 200)),
+                (self.splitter_mode == 'random'
+                    and not daal_check_version((2023, 'P', 200)),
                     "ExtraTrees only supported starting from oneDAL version 2023.2"),
                 (not sp.issparse(sample_weight), "sample_weight is sparse. "
                                                  "Sparse input is not supported."),
