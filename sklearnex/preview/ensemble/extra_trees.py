3179c80b09ecac8b8daa94f27b75b1fe71b61bd9
Ian Faust <icfaust@gmail.com>
2023-05-31 23:33:01 +0200

    Remove random_state warning from extra_trees.py

--- sklearnex/preview/ensemble/extra_trees.py
+++ sklearnex/preview/ensemble/extra_trees.py
@@ -569,11 +569,6 @@ class ExtraTreesClassifier(sklearn_ExtraTreesClassifier, BaseTree):
 
             dal_ready = dal_ready and not hasattr(self, 'estimators_')
 
-            if dal_ready and (self.random_state is not None):
-                warnings.warn("Setting 'random_state' value is not supported. "
-                              "State set by oneDAL to default value (777).",
-                              RuntimeWarning)
-
         elif method_name in ['predict',
                              'predict_proba']:
 
@@ -616,11 +611,6 @@ class ExtraTreesClassifier(sklearn_ExtraTreesClassifier, BaseTree):
             ])
 
             dal_ready &= not hasattr(self, 'estimators_')
-
-            if dal_ready and (self.random_state is not None):
-                warnings.warn("Setting 'random_state' value is not supported. "
-                              "State set by oneDAL to default value (777).",
-                              RuntimeWarning)
 
         elif method_name in ['predict',
                              'predict_proba']:
@@ -1042,11 +1032,6 @@ class ExtraTreesRegressor(sklearn_ExtraTreesRegressor, BaseTree):
 
             dal_ready &= not hasattr(self, 'estimators_')
 
-            if dal_ready and (self.random_state is not None):
-                warnings.warn("Setting 'random_state' value is not supported. "
-                              "State set by oneDAL to default value (777).",
-                              RuntimeWarning)
-
         elif method_name in ['predict',
                              'predict_proba']:
 
@@ -1089,11 +1074,6 @@ class ExtraTreesRegressor(sklearn_ExtraTreesRegressor, BaseTree):
             ])
 
             dal_ready &= not hasattr(self, 'estimators_')
-
-            if dal_ready and (self.random_state is not None):
-                warnings.warn("Setting 'random_state' value is not supported. "
-                              "State set by oneDAL to default value (777).",
-                              RuntimeWarning)
 
         elif method_name in ['predict',
                              'predict_proba']:
