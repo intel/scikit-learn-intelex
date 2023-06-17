c6a9b57738de3a770a56b571042cd160c5569746
Nikita Kulikov <nikita.kulikov@intel.com>
2023-06-17 06:50:37 -0700

    Minor

--- .circleci/run_xpu_tests.py
+++ .circleci/run_xpu_tests.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
     yml_deselected_tests = yml_deselected_tests + deselected_tests
 
     pytest_params = [
-        "-ra", "--disable-warnings", "-s"
+        "-ra", "--disable-warnings"
     ]
 
     if not args.quiet:
