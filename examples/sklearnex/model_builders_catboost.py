bc303967810708d27bd57341934a5fbac823058b
Dmitry Razdoburdin <>
2023-07-05 02:56:11 -0700

    examples rename

--- examples/sklearnex/model_builders_catboost.py
+++ examples/sklearnex/model_builders_catboost.py
@@ -1,5 +1,5 @@
 #===============================================================================
-# Copyright 2014 Intel Corporation
+# Copyright 2021 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,10 +14,10 @@
 # limitations under the License.
 #===============================================================================
 
-# daal4py Gradient Bossting Classification model creation from XGBoost example
+# daal4py Gradient Bossting Classification model creation from Catboost example
 
 from daal4py.sklearn.ensemble import GBTDAALClassifier
-import xgboost as xgb
+import catboost as cb
 import numpy as np
 import pandas as pd
 
@@ -28,8 +28,8 @@ def pd_read_csv(f, c=None, t=np.float64):
 
 def main(readcsv=pd_read_csv, method='defaultDense'):
     # Path to data
-    train_file = "./examples/daal4py/data/batch/df_classification_train.csv"
-    test_file = "./examples/daal4py/data/batch/df_classification_test.csv"
+    train_file = "../daal4py/data/batch/df_classification_train.csv"
+    test_file = "../daal4py/data/batch/df_classification_test.csv"
 
     # Data reading
     X_train = readcsv(train_file, range(3), t=np.float32)
@@ -37,26 +37,26 @@ def main(readcsv=pd_read_csv, method='defaultDense'):
     X_test = readcsv(test_file, range(3), t=np.float32)
     y_test = readcsv(test_file, range(3, 4), t=np.float32)
 
-    # training parameters setting
-    clf = xgb.XGBClassifier(
-        max_bin=256,
-        scale_pos_weight=2,
-        lambda_l2=1,
-        alpha=0.9,
+    # Datasets creation
+    cb_train = cb.Pool(X_train, label=np.array(y_train))
+    cb_test = cb.Pool(X_test, label=np.array(y_test))
+
+    clf = cb.CatBoostClassifier(
+        reg_lambda=1,
         max_depth=6,
         num_leaves=2**6,
-        verbosity=0,
-        objective='multi:softmax',
+        verbose=0,
+        objective='MultiClass',
         learning_rate=0.3,
-        num_class=5,
-        n_estimators=25)
+        n_estimators=25,
+        classes_count=5)
 
     # Training
-    clf.fit(X_train, y_train)
+    clf.fit(cb_train)
 
-    # XGBoost prediction
-    xgb_prediction = clf.predict(X_test)
-    xgb_errors_count = np.count_nonzero(xgb_prediction - np.ravel(y_test))
+    # Catboost prediction
+    cb_prediction = clf.predict(cb_test, prediction_type='Class').T[0]
+    cb_errors_count = np.count_nonzero(cb_prediction - np.ravel(y_test))
 
     # Conversion to daal4py
     daal_model = GBTDAALClassifier.convert_model(clf)
@@ -64,22 +64,26 @@ def main(readcsv=pd_read_csv, method='defaultDense'):
     # daal4py prediction
     daal_prediction = daal_model.predict(X_test)
     daal_errors_count = np.count_nonzero(daal_prediction - np.ravel(y_test))
-    assert np.absolute(xgb_errors_count - daal_errors_count) == 0
+    assert np.absolute(cb_errors_count - daal_errors_count) == 0
 
-    return (xgb_prediction, xgb_errors_count, daal_prediction,
+    return (cb_prediction, cb_errors_count, daal_prediction,
             daal_errors_count, np.ravel(y_test))
 
 
 if __name__ == "__main__":
-    (xgb_prediction, xgb_errors_count,
+    (cb_prediction, cb_errors_count,
      daal_prediction, daal_errors_count, y_test) = main()
-    print("\nXGBoost prediction results (first 10 rows):\n", xgb_prediction[0:10])
-    print("\ndaal4py prediction results (first 10 rows):\n", daal_prediction[0:10])
+    print("\nCatboost prediction results (first 10 rows):\n",
+          cb_prediction[0:10])
+    print("\ndaal4py prediction results (first 10 rows):\n",
+          daal_prediction[0:10])
     print("\nGround truth (first 10 rows):\n", y_test[0:10])
 
-    print("XGBoost errors count:", xgb_errors_count)
-    print("XGBoost accuracy score:", 1 - xgb_errors_count / xgb_prediction.shape[0])
+    print("Catboost errors count:", cb_errors_count)
+    print("Catboost accuracy score:",
+          1 - cb_errors_count / cb_prediction.shape[0])
 
     print("\ndaal4py errors count:", daal_errors_count)
-    print("daal4py accuracy score:", 1 - daal_errors_count / daal_prediction.shape[0])
+    print("daal4py accuracy score:",
+          1 - daal_errors_count / daal_prediction.shape[0])
     print("\nAll looks good!")
--- examples/sklearnex/model_builders_lightgbm.py
+++ examples/sklearnex/model_builders_lightgbm.py
@@ -1,5 +1,5 @@
 #===============================================================================
-# Copyright 2021 Intel Corporation
+# Copyright 2020 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,10 +14,10 @@
 # limitations under the License.
 #===============================================================================
 
-# daal4py Gradient Bossting Classification model creation from Catboost example
+# daal4py Gradient Bossting Classification model creation from LightGBM example
 
 from daal4py.sklearn.ensemble import GBTDAALClassifier
-import catboost as cb
+import lightgbm as lgb
 import numpy as np
 import pandas as pd
 
@@ -28,8 +28,8 @@ def pd_read_csv(f, c=None, t=np.float64):
 
 def main(readcsv=pd_read_csv, method='defaultDense'):
     # Path to data
-    train_file = "./examples/daal4py/data/batch/df_classification_train.csv"
-    test_file = "./examples/daal4py/data/batch/df_classification_test.csv"
+    train_file = "../daal4py/data/batch/df_classification_train.csv"
+    test_file = "../daal4py/data/batch/df_classification_test.csv"
 
     # Data reading
     X_train = readcsv(train_file, range(3), t=np.float32)
@@ -37,26 +37,25 @@ def main(readcsv=pd_read_csv, method='defaultDense'):
     X_test = readcsv(test_file, range(3), t=np.float32)
     y_test = readcsv(test_file, range(3, 4), t=np.float32)
 
-    # Datasets creation
-    cb_train = cb.Pool(X_train, label=np.array(y_train))
-    cb_test = cb.Pool(X_test, label=np.array(y_test))
-
-    clf = cb.CatBoostClassifier(
-        reg_lambda=1,
+    clf = lgb.LGBMClassifier(
+        max_bin=256,
+        scale_pos_weight=2,
+        lambda_l2=1,
+        alpha=0.9,
         max_depth=6,
         num_leaves=2**6,
-        verbose=0,
-        objective='MultiClass',
+        verbose=-1,
+        objective='multiclass',
         learning_rate=0.3,
-        n_estimators=25,
-        classes_count=5)
+        num_class=5,
+        n_estimators=25)
 
     # Training
-    clf.fit(cb_train)
+    clf.fit(X_train, np.array(y_train).reshape(X_train.shape[0]))
 
-    # Catboost prediction
-    cb_prediction = clf.predict(cb_test, prediction_type='Class').T[0]
-    cb_errors_count = np.count_nonzero(cb_prediction - np.ravel(y_test))
+    # LightGBM prediction
+    lgb_prediction = clf.predict(X_test)
+    lgb_errors_count = np.count_nonzero(lgb_prediction - np.ravel(y_test))
 
     # Conversion to daal4py
     daal_model = GBTDAALClassifier.convert_model(clf)
@@ -64,26 +63,22 @@ def main(readcsv=pd_read_csv, method='defaultDense'):
     # daal4py prediction
     daal_prediction = daal_model.predict(X_test)
     daal_errors_count = np.count_nonzero(daal_prediction - np.ravel(y_test))
-    assert np.absolute(cb_errors_count - daal_errors_count) == 0
+    assert np.absolute(lgb_errors_count - daal_errors_count) == 0
 
-    return (cb_prediction, cb_errors_count, daal_prediction,
+    return (lgb_prediction, lgb_errors_count, daal_prediction,
             daal_errors_count, np.ravel(y_test))
 
 
 if __name__ == "__main__":
-    (cb_prediction, cb_errors_count,
-     daal_prediction, daal_errors_count, y_test) = main()
-    print("\nCatboost prediction results (first 10 rows):\n",
-          cb_prediction[0:10])
-    print("\ndaal4py prediction results (first 10 rows):\n",
-          daal_prediction[0:10])
+    (lgb_prediction, lgb_errors_count, daal_prediction,
+     daal_errors_count, y_test) = main()
+    print("\nLightGBM prediction results (first 10 rows):\n", lgb_prediction[0:10])
+    print("\ndaal4py prediction results (first 10 rows):\n", daal_prediction[0:10])
     print("\nGround truth (first 10 rows):\n", y_test[0:10])
 
-    print("Catboost errors count:", cb_errors_count)
-    print("Catboost accuracy score:",
-          1 - cb_errors_count / cb_prediction.shape[0])
+    print("LightGBM errors count:", lgb_errors_count)
+    print("LightGBM accuracy score:", 1 - lgb_errors_count / lgb_prediction.shape[0])
 
     print("\ndaal4py errors count:", daal_errors_count)
-    print("daal4py accuracy score:",
-          1 - daal_errors_count / daal_prediction.shape[0])
+    print("daal4py accuracy score:", 1 - daal_errors_count / daal_prediction.shape[0])
     print("\nAll looks good!")
