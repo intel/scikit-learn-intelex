5aaac7d574957e83e42afe902b0a2081891595ed
Andreas Huber <andreas.huber@intel.com>
2023-08-08 16:36:47 +0200

    fix copyright string length

--- generator/format.py
+++ generator/format.py
@@ -1,4 +1,4 @@
-# ===============================================================================
+# ==============================================================================
 # Copyright 2014 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ===============================================================================
+# ==============================================================================
 
 # Provides helper class to format variables (input args, template args and parameters)
 # Different syntax for C++, cython and python are precomputed.
