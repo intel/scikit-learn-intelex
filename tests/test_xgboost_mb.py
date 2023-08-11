caab07555a0f68eb4b6bba19a1e8401302847885
Andreas Huber <andreas.huber@intel.com>
2023-08-11 08:48:14 -0700

    re-enable mb tests

--- tests/test_xgboost_mb.py
+++ tests/test_xgboost_mb.py
@@ -34,67 +34,67 @@ reason += str(daal_version)
 
 
 class XgboostModelBuilder(unittest.TestCase):
-    # @unittest.skipUnless(
-    #     all(
-    #         [
-    #             hasattr(d4p, "get_gbt_model_from_xgboost"),
-    #             hasattr(d4p, "gbt_classification_prediction"),
-    #             daal_check_version(((2021, "P", 1))),
-    #         ]
-    #     ),
-    #     reason,
-    # )
-    # @unittest.skipUnless(
-    #     importlib.util.find_spec("xgboost") is not None,
-    #     "xgboost library is not installed",
-    # )
-    # def test_earlystop(self):
-    #     import xgboost as xgb
+    @unittest.skipUnless(
+        all(
+            [
+                hasattr(d4p, "get_gbt_model_from_xgboost"),
+                hasattr(d4p, "gbt_classification_prediction"),
+                daal_check_version(((2021, "P", 1))),
+            ]
+        ),
+        reason,
+    )
+    @unittest.skipUnless(
+        importlib.util.find_spec("xgboost") is not None,
+        "xgboost library is not installed",
+    )
+    def test_earlystop(self):
+        import xgboost as xgb
 
-    #     num_classes = 3
-    #     X, y = make_classification(
-    #         n_samples=1000,
-    #         n_features=10,
-    #         n_informative=3,
-    #         n_classes=num_classes,
-    #         random_state=42,
-    #     )
-    #     X_train, X_test, y_train, y_test = train_test_split(
-    #         X, y, test_size=0.3, random_state=42
-    #     )
+        num_classes = 3
+        X, y = make_classification(
+            n_samples=1000,
+            n_features=10,
+            n_informative=3,
+            n_classes=num_classes,
+            random_state=42,
+        )
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.3, random_state=42
+        )
 
-    #     # training parameters setting
-    #     params = {
-    #         "n_estimators": 100,
-    #         "max_bin": 256,
-    #         "scale_pos_weight": 2,
-    #         "lambda_l2": 1,
-    #         "alpha": 0.9,
-    #         "max_depth": 8,
-    #         "num_leaves": 2**8,
-    #         "verbosity": 0,
-    #         "objective": "multi:softproba",
-    #         "learning_rate": 0.3,
-    #         "num_class": num_classes,
-    #         "early_stopping_rounds": 5,
-    #     }
+        # training parameters setting
+        params = {
+            "n_estimators": 100,
+            "max_bin": 256,
+            "scale_pos_weight": 2,
+            "lambda_l2": 1,
+            "alpha": 0.9,
+            "max_depth": 8,
+            "num_leaves": 2**8,
+            "verbosity": 0,
+            "objective": "multi:softproba",
+            "learning_rate": 0.3,
+            "num_class": num_classes,
+            "early_stopping_rounds": 5,
+        }
 
-    #     xgb_clf = xgb.XGBClassifier(**params)
-    #     xgb_clf.fit(X_train, y_train, eval_set=[(X_test, y_test)])
-    #     booster = xgb_clf.get_booster()
+        xgb_clf = xgb.XGBClassifier(**params)
+        xgb_clf.fit(X_train, y_train, eval_set=[(X_test, y_test)])
+        booster = xgb_clf.get_booster()
 
-    #     xgb_prediction = xgb_clf.predict(X_test)
-    #     xgb_proba = xgb_clf.predict_proba(X_test)
-    #     xgb_errors_count = np.count_nonzero(xgb_prediction - np.ravel(y_test))
+        xgb_prediction = xgb_clf.predict(X_test)
+        xgb_proba = xgb_clf.predict_proba(X_test)
+        xgb_errors_count = np.count_nonzero(xgb_prediction - np.ravel(y_test))
 
-    #     daal_model = d4p.mb.convert_model(booster)
+        daal_model = d4p.mb.convert_model(booster)
 
-    #     daal_prediction = daal_model.predict(X_test)
-    #     daal_proba = daal_model.predict_proba(X_test)
-    #     daal_errors_count = np.count_nonzero(daal_prediction - np.ravel(y_test))
+        daal_prediction = daal_model.predict(X_test)
+        daal_proba = daal_model.predict_proba(X_test)
+        daal_errors_count = np.count_nonzero(daal_prediction - np.ravel(y_test))
 
-    #     self.assertTrue(np.absolute(xgb_errors_count - daal_errors_count) == 0)
-    #     self.assertTrue(np.allclose(xgb_proba, daal_proba))
+        self.assertTrue(np.absolute(xgb_errors_count - daal_errors_count) == 0)
+        self.assertTrue(np.allclose(xgb_proba, daal_proba))
 
     @unittest.skipUnless(
         all(
