9a5afbd47b51a8f68dd27ccf5e680f8de56f2234
Alexander Andreev <alexander.andreev@intel.com>
2023-08-16 02:30:09 -0700

    Fix os name getting

--- .ci/scripts/run_sklearn_tests.sh
+++ .ci/scripts/run_sklearn_tests.sh
@@ -28,7 +28,7 @@ export DESELECTED_TESTS=$(python ../.circleci/deselect_tests.py ../deselected_te
 
 # manual setting of OCL_ICD_FILENAMES is required in
 # specific MSYS environment with conda packages downloaded from intel channel
-if [[ "${os}" =~ "MSYS" ]] && [ -z "${OCL_ICD_FILENAMES}" ] && [ -n "${CONDA_PREFIX}" ]; then
+if [[ "$(uname)" =~ "MSYS" ]] && [ -z "${OCL_ICD_FILENAMES}" ] && [ -n "${CONDA_PREFIX}" ]; then
     export OCL_ICD_FILENAMES=$(find $CONDA_PREFIX -name intelocl64.dll | sed 's/\\/\//g')
     echo "OCL_ICD_FILENAMES is set to ${OCL_ICD_FILENAMES}"
 fi
