7460d36a880c9376bc83f6100e6c28f400361885
Alexander Andreev <alexander.andreev@intel.com>
2023-08-16 04:16:27 -0700

    Change echo command

--- .ci/scripts/run_sklearn_tests.sh
+++ .ci/scripts/run_sklearn_tests.sh
@@ -30,6 +30,8 @@ export DESELECTED_TESTS=$(python ../.circleci/deselect_tests.py ../deselected_te
 # specific MSYS environment with conda packages downloaded from intel channel
 if [[ "$(uname)" =~ "MSYS" ]] && [ -z "${OCL_ICD_FILENAMES}" ] && [ -n "${CONDA_PREFIX}" ]; then
     export OCL_ICD_FILENAMES=$(find $CONDA_PREFIX -name intelocl64.dll | sed 's/\\/\//g')
+fi
+if [ -n "${OCL_ICD_FILENAMES}" ]; then
     echo "OCL_ICD_FILENAMES is set to ${OCL_ICD_FILENAMES}"
 fi
 
