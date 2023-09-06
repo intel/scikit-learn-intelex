e6748d942b3248cda1d471001c6dc0b43c3b72d1
ethanglaser <ethan.glaser@intel.com>
2023-09-06 05:08:41 -0700

    replacing todo with internal ticket

--- examples/sklearnex/random_forest_classifier_spmd.py
+++ examples/sklearnex/random_forest_classifier_spmd.py
@@ -36,7 +36,6 @@ def generate_X_y(par, seed):
     return data, resp
 
 
-# TODO: Investigate out of resources error with 256 classes, accuracy issues
 params_train = {"ns": 1000000, "nf": 3}
 params_test = {"ns": 100, "nf": 3}
 
