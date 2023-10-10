697622ef11b15fc64e8c3727cdf13ff1da6f8c9e
Andreas Huber <andreas.huber@intel.com>
2023-10-10 03:08:40 -0700

    Add GBT model builder API version descriptions

--- src/gbt_model_builder.h
+++ src/gbt_model_builder.h
@@ -23,8 +23,11 @@
 #include "onedal/version.hpp"
 
 #if (((MAJOR_VERSION == 2024) && (MINOR_VERSION == 0) && (UPDATE_VERSION >= 1)) || ((MAJOR_VERSION > 2024) && (MINOR_VERSION >= 1)))
+    // added missing value support to GBT regression
+    // added SHAP value support
     #define _gbt_inference_api_version 2
 #elif (((MAJOR_VERSION == 2023) && (MINOR_VERSION >= 2)) || (MAJOR_VERSION > 2023))
+    // added missing value support to GBT classification
     #define _gbt_inference_api_version 1
 #else
     #define _gbt_inference_api_version 0
