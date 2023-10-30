3626d0222a438af13fe72a3a5fb44b996494a8e3
ethanglaser <ethan.glaser@intel.com>
2023-10-30 12:25:25 -0700

    minor paths update

--- sklearnex/linear_model/linear.py
+++ sklearnex/linear_model/linear.py
@@ -67,10 +67,9 @@ if daal_check_version((2023, "P", 100)):
 
     from daal4py.sklearn._utils import get_dtype, make2d, sklearn_check_version
 
-    from ..._device_offload import dispatch, wrap_output_data
-    from ..._utils import PatchingConditionsChain, get_patch_message
-    from ...utils.validation import _assert_all_finite
-    from ._common import BaseLinearRegression
+    from .._device_offload import dispatch, wrap_output_data
+    from .._utils import PatchingConditionsChain, get_patch_message
+    from ..utils.validation import _assert_all_finite
 
     if sklearn_check_version("1.0") and not sklearn_check_version("1.2"):
         from sklearn.linear_model._base import _deprecate_normalize
