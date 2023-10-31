930d6c4e32aac2f52206c5352fdf332fec929eec
ethanglaser <42726565+ethanglaser@users.noreply.github.com>
2023-10-31 08:19:30 -0500

    Update sklearnex/tests/test_memory_usage.py
    Co-authored-by: Alexander Andreev <alexander.andreev@intel.com>

--- sklearnex/tests/test_memory_usage.py
+++ sklearnex/tests/test_memory_usage.py
@@ -28,7 +28,6 @@ from sklearn.datasets import make_classification
 from sklearn.model_selection import KFold
 
 from sklearnex import get_patch_map
-from sklearnex.linear_model import LinearRegression
 from sklearnex.metrics import pairwise_distances, roc_auc_score
 from sklearnex.model_selection import train_test_split
 from sklearnex.preview.decomposition import PCA as PreviewPCA
