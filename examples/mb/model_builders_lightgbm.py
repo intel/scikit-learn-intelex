1727624c21e1a49a216adcd0db3736e9604ed0b6
Andreas Huber <andreas.huber@intel.com>
2023-11-02 04:13:44 -0700

    test: remove valid_sets

--- examples/mb/model_builders_lightgbm.py
+++ examples/mb/model_builders_lightgbm.py
@@ -75,9 +75,7 @@ def main(readcsv=pd_read_csv):
     }
 
     # Training
-    lgb_model = timeit(lgb.train)(
-        params, lgb_train, valid_sets=lgb_train, callbacks=[lgb.log_evaluation(0)]
-    )
+    lgb_model = timeit(lgb.train)(params, lgb_train, callbacks=[lgb.log_evaluation(0)])
 
     # LightGBM prediction
     lgb_prediction = np.argmax(lgb_model.predict(X_test), axis=1)
