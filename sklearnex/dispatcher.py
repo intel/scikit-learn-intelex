99a0dbd0e6c392098f39419bc851fc9dedccfa03
Anatoly Volkov <anatoly.volkov@intel.com>
2023-11-28 03:49:24 -0800

    Add module to dispatcher

--- sklearnex/dispatcher.py
+++ sklearnex/dispatcher.py
@@ -77,6 +77,9 @@ def get_patch_map():
         # Preview classes for patching
         from .preview.cluster import KMeans as KMeans_sklearnex
         from .preview.decomposition import PCA as PCA_sklearnex
+        from .preview.linear_model import (
+            LogisticRegression as LogisticRegression_sklearnex,
+        )
         from .svm import SVC as SVC_sklearnex
         from .svm import SVR as SVR_sklearnex
         from .svm import NuSVC as NuSVC_sklearnex
@@ -96,6 +99,20 @@ def get_patch_map():
                         cluster_module,
                         "KMeans",
                         KMeans_sklearnex,
+                    ),
+                    None,
+                ]
+            ]
+
+            # LogisticRegression
+            mapping.pop("logistic")
+            mapping.pop("log_reg")
+            mapping["log_reg"] = [
+                [
+                    (
+                        linear_model_module,
+                        "LogisticRegression",
+                        LogisticRegression_sklearnex,
                     ),
                     None,
                 ]
