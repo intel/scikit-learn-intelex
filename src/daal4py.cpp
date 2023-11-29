d64b68db517697b276d6995bf4040a1d3190f752
Ian Faust <icfaust@gmail.com>
2023-11-29 22:18:38 +0100

    english

--- src/daal4py.cpp
+++ src/daal4py.cpp
@@ -555,16 +555,16 @@ daal::data_management::NumericTablePtr make_nt(PyObject * obj)
                     size_t c_nr = static_cast<size_t>(PyInt_AsSsize_t(nr));
                     py_err_check();
 #define MKCSR_(_T) ret = daal::data_management::CSRNumericTable::create(daal::services::SharedPtr<_T>(reinterpret_cast<_T *>(array_data(np_vals)), NumpyDeleter(reinterpret_cast<PyArrayObject *>(np_vals))), daal::services::SharedPtr<size_t>(c_indcs_one_based, daal::services::ServiceDeleter()), daal::services::SharedPtr<size_t>(c_roffs_one_based, daal::services::ServiceDeleter()), c_nc, c_nr)
-                    SET_NPY_FEATURE(array_type(np_vals), MKCSR_, throw std::invalid_argument("Found unsupported data type in csr_object"));
+                    SET_NPY_FEATURE(array_type(np_vals), MKCSR_, throw std::invalid_argument("Found unsupported data type in CSR object"));
 #undef MKCSR_
                 }
                 else
-                    throw std::invalid_argument("Failed accessing csr data when converting csr_object.\n");
+                    throw std::invalid_argument("Failed accessing csr data when converting CSR object.\n");
                 Py_DECREF(np_indcs);
                 Py_DECREF(np_roffs);
             }
             else
-                throw std::invalid_argument("Got invalid csr_matrix object.\n");
+                throw std::invalid_argument("Got invalid CSR object.\n");
             Py_DECREF(shape);
             Py_DECREF(roffs);
             Py_DECREF(indcs);
