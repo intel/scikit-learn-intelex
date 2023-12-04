7d866e0668ecd5ee5abe4c0f9f9e3d1972c197ad
Andreas Huber <andreas.huber@intel.com>
2023-12-04 09:30:13 -0800

    Use more realistic version values

--- daal4py/sklearn/utils/test/test_utils.py
+++ daal4py/sklearn/utils/test/test_utils.py
@@ -4,25 +4,26 @@ from daal4py.sklearn import _utils
 
 
 def injected_get_daal_version():
-    return (2020, "C", 100)
+    return (2020, "P", 100)
 
 
 @pytest.mark.parametrize(
     "required_version,expected",
     [
-        ((2020, "C", 100), True),
-        ((2019, "C", 100), True),
-        ((2020, "B", 100), True),
-        ((2020, "C", 99), True),
-        ((2021, "C", 100), False),
-        ((2020, "D", 100), False),
-        ((2020, "C", 101), False),
-        ((2019, "B", 99), True),
-        ((2021, "D", 101), False),
-        (((2020, "C", 100), (2021, "D", 101)), True),
-        (((2019, "C", 100), (2018, "C", 100)), True),
-        (((2019, "C", 100), (2018, "C", 100)), True),
-        (((2021, "C", 100), (2022, "C", 100)), False),
+        ((2020, "P", 100), True),
+        ((2019, "P", 100), True),
+        ((2020, "A", 100), True),
+        ((2020, "P", 1), True),
+        ((2021, "P", 100), False),
+        ((2020, "Z", 100), False),
+        ((2020, "P", 200), False),
+        ((2019, "A", 1), True),
+        ((2021, "Z", 200), False),
+        ((2019, "P", 200), True),
+        (((2020, "P", 100), (2021, "Z", 200)), True),
+        (((2019, "P", 100), (2018, "P", 100)), True),
+        (((2019, "P", 100), (2018, "P", 100)), True),
+        (((2021, "P", 100), (2022, "P", 100)), False),
     ],
 )
 def test_daal_check_version(required_version, expected):
