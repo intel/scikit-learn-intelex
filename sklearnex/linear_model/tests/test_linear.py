82be602e0b055a8ceb583391fcb8c88f969c333a
icfaust <icfaust@gmail.com>
2023-12-07 10:57:39 -0800

    change situation to match test name

--- sklearnex/linear_model/tests/test_linear.py
+++ sklearnex/linear_model/tests/test_linear.py
@@ -87,8 +87,7 @@ def test_bf16_blas_epsilon():
     from sklearnex.linear_model import LinearRegression
 
     X = np.array([[0.0, 1.0], [1.0, 1.0]], dtype=np.float32)
-    y = np.array([[1.0], [1.0]], dtype=np.float32)
-    y[1, 0] += np.finfo(np.float32).eps
+    y = np.array([[1.0], [1.0 + np.finfo(np.float32).eps]], dtype=np.float32)
 
     # Make changes in y not observable in bfloat16 which means casting
     # to bf16 for gemm should yield an incorrect value, everything
