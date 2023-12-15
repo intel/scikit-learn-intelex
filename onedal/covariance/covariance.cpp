0e4602d59e2d3141f84f6f262a8e1cafc506fad7
Kruglov, Oleg <oleg.kruglov@intel.com>
2023-12-15 07:05:10 -0800

    Add onedal incremental covariance

--- onedal/covariance/covariance.cpp
+++ onedal/covariance/covariance.cpp
@@ -57,6 +57,33 @@ struct params2desc {
 };
 
 template <typename Policy, typename Task>
+void init_finalize_compute_ops(pybind11::module_& m) {
+    using namespace dal::covariance;
+    using input_t = partial_compute_result<Task>;
+    m.def("finalize_compute", [](const Policy& policy, const pybind11::dict& params, const input_t& data) {
+        finalize_compute_ops ops(policy, data, params2desc{});
+        return fptype2t{ method2t{ Task{}, ops } }(params);
+    });
+}
+
+template <typename Policy, typename Task>
+void init_partial_compute_ops(pybind11::module_& m) {
+    using prev_result_t = dal::covariance::partial_compute_result<Task>;
+    m.def("partial_compute", [](
+        const Policy& policy,
+        const pybind11::dict& params,
+        const prev_result_t& prev,
+        const table& data) {
+            using namespace dal::covariance;
+            using input_t = partial_compute_input<Task>;
+            partial_compute_ops ops(policy, input_t{prev, data}, params2desc{});
+            return fptype2t{ method2t{ Task{}, ops } }(params);
+        }
+    );
+}
+
+
+template <typename Policy, typename Task>
 void init_compute_ops(py::module_& m) {
 #if defined(ONEDAL_VERSION) && ONEDAL_VERSION >= 20240000
     using compute_hyperparams_t = dal::covariance::detail::compute_parameters<Task>;
@@ -96,6 +123,17 @@ inline void init_compute_result(py::module_& m) {
         .def_property("means", &result_t::get_means, &result_t::set_means);
 }
 
+template <typename Task>
+inline void init_partial_compute_result(pybind11::module_& m) {
+    using namespace dal::covariance;
+    using result_t = partial_compute_result<Task>;
+    pybind11::class_<result_t>(m, "partial_compute_result")
+        .def(pybind11::init())
+        .def_property("partial_n_rows", &result_t::get_partial_n_rows, &result_t::set_partial_n_rows)
+        .def_property("partial_crossproduct", &result_t::get_partial_crossproduct, &result_t::set_partial_crossproduct)
+        .def_property("partial_sums", &result_t::get_partial_sum, &result_t::set_partial_sum);
+}
+
 #if defined(ONEDAL_VERSION) && ONEDAL_VERSION >= 20240000
 
 template <typename Task>
@@ -116,7 +154,10 @@ void init_compute_hyperparameters(py::module_& m) {
 #endif // defined(ONEDAL_VERSION) && ONEDAL_VERSION >= 20240000
 
 ONEDAL_PY_DECLARE_INSTANTIATOR(init_compute_result);
+ONEDAL_PY_DECLARE_INSTANTIATOR(init_partial_compute_result);
 ONEDAL_PY_DECLARE_INSTANTIATOR(init_compute_ops);
+ONEDAL_PY_DECLARE_INSTANTIATOR(init_finalize_compute_ops);
+ONEDAL_PY_DECLARE_INSTANTIATOR(init_partial_compute_ops);
 #if defined(ONEDAL_VERSION) && ONEDAL_VERSION >= 20240000
     ONEDAL_PY_DECLARE_INSTANTIATOR(init_compute_hyperparameters);
 #endif // defined(ONEDAL_VERSION) && ONEDAL_VERSION >= 20240000
@@ -130,10 +171,15 @@ ONEDAL_PY_INIT_MODULE(covariance) {
     auto sub = m.def_submodule("covariance");
     #ifdef ONEDAL_DATA_PARALLEL_SPMD
         ONEDAL_PY_INSTANTIATE(init_compute_ops, sub, policy_list_spmd, task::compute);
+        ONEDAL_PY_INSTANTIATE(init_partial_compute_ops, sub, policy_list_spmd, task::compute);
+        ONEDAL_PY_INSTANTIATE(init_finalize_compute_ops, sub, policy_list_spmd, task::compute);
     #else    
         ONEDAL_PY_INSTANTIATE(init_compute_ops, sub, policy_list, task::compute);
+        ONEDAL_PY_INSTANTIATE(init_partial_compute_ops, sub, policy_list, task::compute);
+        ONEDAL_PY_INSTANTIATE(init_finalize_compute_ops, sub, policy_list, task::compute);
     #endif
     ONEDAL_PY_INSTANTIATE(init_compute_result, sub, task::compute);
+    ONEDAL_PY_INSTANTIATE(init_partial_compute_result, sub, task::compute);
     #if defined(ONEDAL_VERSION) && ONEDAL_VERSION >= 20240000
         ONEDAL_PY_INSTANTIATE(init_compute_hyperparameters, sub, task::compute);
     #endif // defined(ONEDAL_VERSION) && ONEDAL_VERSION >= 20240000
---
+++ onedal/covariance/incremental_covariance.py
@@ -0,0 +1,72 @@
+# ===============================================================================
+# Copyright 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+from daal4py.sklearn._utils import daal_check_version, get_dtype, make2d
+from onedal import _backend
+from ..common._policy import _get_policy
+from ..datatypes import _convert_to_supported, from_table, to_table
+from sklearn.utils import check_array
+import numpy as np
+
+
+class IncrementalEmpiricalCovariance():
+    def __init__(self, method="dense", bias=False):
+        self.method = method
+        self.bias = bias
+        self._partial_result = _backend.covariance.partial_compute_result()
+
+    def _get_policy(self, queue, *data):
+        return _get_policy(queue, *data)
+
+    def _get_onedal_params(self, dtype=np.float32):
+        params = {
+            "fptype": "float" if dtype == np.float32 else "double",
+            "method": self.method,
+        }
+        if daal_check_version((2024, "P", 1)):
+            params["bias"] = self.bias
+        return params
+
+
+    def partial_compute(self, X, queue=None):
+        if not hasattr(self, "_policy"):
+            self._policy = self._get_policy(queue, X)
+        if not hasattr(self, "_dtype"):
+            self._dtype = get_dtype(X)
+        X = check_array(X, dtype=[np.float64, np.float32])
+        X = make2d(X)
+        types = [np.float32, np.float64]
+        if get_dtype(X) not in types:
+            X = X.astype(np.float64)
+        X = _convert_to_supported(self._policy, X)
+        params = self._get_onedal_params(self._dtype)
+        module = _backend.covariance
+        table_X = to_table(X)
+        self._partial_result = module.partial_compute(self._policy, params, self._partial_result, table_X)
+
+    def finalize_compute(self, queue=None):
+        params = self._get_onedal_params(self._dtype)
+        module = _backend.covariance
+        result = module.finalize_compute(self._policy, params, self._partial_result)
+        if daal_check_version((2024, "P", 1)) or (not self.bias):
+            self.covariance_ = from_table(result.cov_matrix)
+        else:
+            n_rows = self._partial_result.partial_n_rows
+            self.covariance_ = from_table(result.cov_matrix) * (n_rows - 1) / n_rows
+            
+
+        self.location_ = from_table(result.means).ravel()
+
+        return self
