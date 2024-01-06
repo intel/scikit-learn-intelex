8fbc3f0253ea4490dd5f5680b7b9cb67013b365a
Md Shafiul Alam <md.shafiul.alam@intel.com>
2024-01-06 10:15:30 -0800

    revert scipy version bump for sklearn 1.0.2

--- .ci/scripts/get_compatible_scipy_version.py
+++ .ci/scripts/get_compatible_scipy_version.py
@@ -24,8 +24,10 @@ if sklearn_check_version("1.3") or python_version[1] > 11:
     print("scipy")
 elif sklearn_check_version("1.2") or python_version[1] > 10:
     print("scipy==1.9.*")
-elif sklearn_check_version("1.0"):
+elif sklearn_check_version("1.1"):
     print("scipy==1.8.*")
+elif sklearn_check_version("1.0"):
+    print("scipy==1.7.*")
 elif sklearn_check_version("0.24"):
     # scipy 1.6 is compatible with pandas versions lower than 1.4
     print("scipy==1.6.* pandas==1.3.*")
