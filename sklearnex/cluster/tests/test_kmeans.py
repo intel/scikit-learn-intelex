f27f78ce3b0a5f3abb55df20992163066feea4bc
Md Shafiul Alam <md.shafiul.alam@intel.com>
2024-01-19 07:23:10 -0800

    minor fix

--- sklearnex/cluster/tests/test_kmeans.py
+++ sklearnex/cluster/tests/test_kmeans.py
@@ -26,7 +26,7 @@ from onedal.tests.utils._dataframes_support import (
 
 
 @pytest.mark.parametrize("dataframe,queue", get_dataframes_and_queues())
-def test_sklearnex_import():
+def test_sklearnex_import(dataframe, queue):
     from sklearnex.cluster import KMeans
 
     X = np.array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
