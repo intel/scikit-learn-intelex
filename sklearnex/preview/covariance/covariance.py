dd575756da10043aedd6efb234472df2f21e97ba
Ian Faust <icfaust@gmail.com>
2024-01-27 09:12:49 +0100

    Update covariance.py

--- sklearnex/preview/covariance/covariance.py
+++ sklearnex/preview/covariance/covariance.py
@@ -106,9 +106,9 @@ class EmpiricalCovariance(sklearn_EmpiricalCovariance):
     @wrap_output_data
     def mahalanobis(self, X, queue=None):
         if sklearn_check_version("1.2"):
-            X = self._validate_data(X, reset=False, copy=self.copy)
+            X = self._validate_data(X, reset=False)
         else:
-            X = check_array(X, reset=False, copy=self.copy)
+            X = check_array(X, reset=False)
 
         precision = self.get_precision()
         with config_context(assume_finite=True):
