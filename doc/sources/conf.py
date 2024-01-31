58986926a8b39ab279aa2a08b2c83771d6b1b64d
Alexandra <alexandra.epanchinzeva@intel.com>
2024-01-31 18:38:16 +0100

    Update conf.py

--- doc/sources/conf.py
+++ doc/sources/conf.py
@@ -66,6 +66,7 @@ extensions = [
     "sphinx_tabs.tabs",
     "notfound.extension",
     "sphinx_design",
+    "sphinx_copybutton",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
