0a8a1cacb319bd7fc4892a45817fe815c78c46d2
Kruglov, Oleg <oleg.kruglov@intel.com>
2024-02-22 05:40:59 -0800

    Fix examples

--- examples/sklearnex/incremental_basic_statistics_dpctl.py
+++ examples/sklearnex/incremental_basic_statistics_dpctl.py
@@ -22,8 +22,27 @@ from sklearnex.basic_statistics import IncrementalBasicStatistics
 # We create GPU SyclQueue and then put data to dpctl tensor using
 # the queue. It allows us to do computation on GPU.
 
-incbs = IncrementalBasicStatistics(result_options=["mean", "max", "sum"], batch_size=3)
 queue = dpctl.SyclQueue("gpu")
+
+incbs = IncrementalBasicStatistics(result_options=["mean", "max", "sum"])
+
+# We do partial_fit for each batch and then print final result.
+X_1 = dpt.asarray([[0, 1], [0, 1]], sycl_queue=queue)
+result = incbs.partial_fit(X_1)
+
+X_2 = dpt.asarray([[1, 2]], sycl_queue=queue)
+result = incbs.partial_fit(X_2)
+
+X_3 = dpt.asarray([[1, 1], [1, 2], [2, 3]], sycl_queue=queue)
+result = incbs.partial_fit(X_3)
+
+print(f"Mean:\n{result.mean}")
+print(f"Max:\n{result.max}")
+print(f"Sum:\n{result.sum}")
+
+# We put the whole data to fit method, it is split automatically and then
+# partial_fit is called for each batch.
+incbs = IncrementalBasicStatistics(result_options=["mean", "max", "sum"], batch_size=3)
 X = dpt.asarray([[0, 1], [0, 1], [1, 2], [1, 1], [1, 2], [2, 3]], sycl_queue=queue)
 result = incbs.fit(X)
 
