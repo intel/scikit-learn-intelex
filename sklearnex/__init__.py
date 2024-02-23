1bf3fb5096b3a40f77e2725d5b438b94f84233d6
Ajay Kumar Patel <ajaykumar.patel@fujitsu.com>
2024-02-23 11:48:26 +0530

    Added copyright

--- sklearnex/__init__.py
+++ sklearnex/__init__.py
@@ -1,5 +1,6 @@
 # ==============================================================================
 # Copyright 2021 Intel Corporation
+# Copyright contributors to the oneDAL project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
