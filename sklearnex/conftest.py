cfae3b8e3466bd4d22850760985a16d85823edc7
Ian Faust <icfaust@gmail.com>
2024-02-27 21:09:40 +0100

    [enhancement] Add guard for sklearn fallback in sklearnex testing (#1686)
    * added conftest
    
    * formatting
    
    * change to CI
    
    * add mark
    
    * improved text
    
    * thats a wrap for the night
    
    * remove stderr from sklearnex logger during tests
    
    * readd monkeypatch to central test list
    
    * Update run_test.sh
    
    * Update test_memory_usage.py
    
    * Update test_memory_usage.py
    
    * Update test_memory_usage.py
    
    * Update test_parallel.py
    
    * Update run_test.sh
    
    * remove mark due to update in EmpiricalCovariance
    
    * Update _forest.py
    
    * Update conftest.py
    
    * Update test_parallel.py
    
    * Update conftest.py
    
    * isort issues

--- sklearnex/conftest.py
+++ sklearnex/conftest.py
@@ -14,9 +14,47 @@
 # limitations under the License.
 # ==============================================================================
 
+import io
+import logging
+
 import pytest
 
 from sklearnex import patch_sklearn, unpatch_sklearn
+
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers", "allow_sklearn_fallback: mark test to not check for sklearnex usage"
+    )
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_call(item):
+    # setup logger to check for sklearn fallback
+    if not item.get_closest_marker("allow_sklearn_fallback"):
+
+        log_stream = io.StringIO()
+        log_handler = logging.StreamHandler(log_stream)
+        sklearnex_logger = logging.getLogger("sklearnex")
+        level = sklearnex_logger.level
+        sklearnex_stderr_handler = sklearnex_logger.handlers
+        sklearnex_logger.handlers = []
+        sklearnex_logger.addHandler(log_handler)
+        sklearnex_logger.setLevel(logging.INFO)
+        log_handler.setLevel(logging.INFO)
+
+        yield
+
+        sklearnex_logger.handlers = sklearnex_stderr_handler
+        sklearnex_logger.setLevel(level)
+        sklearnex_logger.removeHandler(log_handler)
+        text = log_stream.getvalue()
+        if "fallback to original Scikit-learn" in text:
+            raise TypeError(
+                f"test did not properly evaluate sklearnex functionality and fell back to sklearn:\n{text}"
+            )
+    else:
+        yield
 
 
 @pytest.fixture
