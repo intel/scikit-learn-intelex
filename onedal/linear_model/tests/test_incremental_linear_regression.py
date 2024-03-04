6f1bc9d27e8a50e14721704b59ecc5fff1690bbd
Kruglov, Oleg <oleg.kruglov@intel.com>
2024-03-04 06:14:48 -0800

    Update deselected tests, add TODO

--- onedal/linear_model/tests/test_incremental_linear_regression.py
+++ onedal/linear_model/tests/test_incremental_linear_regression.py
@@ -112,6 +112,7 @@ def test_no_intercept_results(queue, num_blocks, dtype):
         model.partial_fit(X_split[i], y_split[i], queue=queue)
     model.finalize_fit()
 
+    # TODO Find out is it necessary to have accuracy so different for float32 and float64
     if queue.sycl_device.is_gpu:
         tol = 3e-3 if model.coef_.dtype == np.float32 else 1e-7
     else:
