dde187ca5732808daa3f415e3b6a3bb4f3a284a6
Ian Faust <icfaust@gmail.com>
2024-03-12 12:12:45 +0100

    Update knn_regression.py

--- sklearnex/neighbors/knn_regression.py
+++ sklearnex/neighbors/knn_regression.py
@@ -228,7 +228,7 @@ class KNeighborsRegressor(KNeighborsRegressor_, KNeighborsDispatchingBase):
                 "onedal": self.__class__._onedal_kneighbors,
                 "sklearn": sklearn_KNeighborsRegressor.kneighbors,
             },
-            X=X,
+            X,
             n_neighbors=n_neighbors,
             return_distance=return_distance,
         )
@@ -285,7 +285,7 @@ class KNeighborsRegressor(KNeighborsRegressor_, KNeighborsDispatchingBase):
         return self._onedal_estimator.predict(X, queue=queue)
 
     def _onedal_kneighbors(
-        self, X=None, n_neighbors=None, return_distance=True, queue=None
+        self, X, n_neighbors=None, return_distance=True, queue=None
     ):
         return self._onedal_estimator.kneighbors(
             X, n_neighbors, return_distance, queue=queue
