d726146031678ea2a38ccaf163cf73106a19e9c0
Ian Faust <icfaust@gmail.com>
2024-03-13 17:12:19 +0100

    Update _dataframes_support.py

--- onedal/tests/utils/_dataframes_support.py
+++ onedal/tests/utils/_dataframes_support.py
@@ -79,7 +79,7 @@ def _convert_to_dataframe(obj, sycl_queue=None, target_df=None, *args, **kwargs)
         return np.asarray(obj, *args, **kwargs)
     # Pandas Dataframe
     if target_df == "pandas":
-        return pd.DataFrame(obj, *args, **kwargs).squeeze()
+        return pd.DataFrame(obj, *args, **kwargs)
     # DPNP ndarray.
     if target_df == "dpnp":
         return dpnp.asarray(
