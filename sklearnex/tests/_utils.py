590a63d078361571fd0a0fd56ac8e4b27e21348f
Ian Faust <icfaust@gmail.com>
2024-03-14 08:34:31 +0100

    Update _utils.py

--- sklearnex/tests/_utils.py
+++ sklearnex/tests/_utils.py
@@ -134,13 +134,13 @@ def gen_dataset(estimator, queue=None, target_df=None, dtype=np.float64):
         X, y = load_diabetes(return_X_y=True)
     else:
         raise ValueError("Unknown dataset type")
-    if target_df != "pandas" or not np.issubdtype(dtype, np.integer):
-        X = _convert_to_dataframe(X, sycl_queue=queue, target_df=target_df, dtype=dtype)
-        y = _convert_to_dataframe(y, sycl_queue=queue, target_df=target_df, dtype=dtype)
-    else:
+    if target_df == "pandas" and np.issubdtype(dtype, np.integer):
         # Pandas float to int not allowed
         X = _convert_to_dataframe(X.astype(dtype), sycl_queue=queue, target_df=target_df)
         y = _convert_to_dataframe(y.astype(dtype), sycl_queue=queue, target_df=target_df)
+    else:            
+        X = _convert_to_dataframe(X, sycl_queue=queue, target_df=target_df, dtype=dtype)
+        y = _convert_to_dataframe(y, sycl_queue=queue, target_df=target_df, dtype=dtype)
         
     return X, y
 
