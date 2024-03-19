9c6b084b5369983384b86638ffa4b67c98a7cf66
Ajay Kumar Patel <ajaykumar.patel@fujitsu.com>
2024-03-19 10:11:52 +0530

    Update to os.environ.get(..) for uniformity

--- sklearnex/__init__.py
+++ sklearnex/__init__.py
@@ -51,7 +51,7 @@ __all__ = [
     "unpatch_sklearn",
     "utils",
 ]
-onedal_iface_flag = os.getenv("OFF_ONEDAL_IFACE", "0")
+onedal_iface_flag = os.environ.get("OFF_ONEDAL_IFACE", "0")
 if onedal_iface_flag == "0":
     from onedal import _is_spmd_backend
     from onedal.common.hyperparameters import get_hyperparameters
