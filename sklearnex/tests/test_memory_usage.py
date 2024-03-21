783c0160f375532f87a883315fc0b5f91d7597b6
Kruglov, Oleg <oleg.kruglov@intel.com>
2024-03-21 08:12:26 -0700

    Update comment for memory test

--- sklearnex/tests/test_memory_usage.py
+++ sklearnex/tests/test_memory_usage.py
@@ -97,7 +97,7 @@ def remove_duplicated_estimators(estimators_list):
 
 
 BANNED_ESTIMATORS = (
-    "IncrementalLinearRegression",  # TODO fix memory leak issue
+    "IncrementalLinearRegression",  # TODO fix memory leak issue in private CI for data_shape = (1000, 100), data_transform_function = dataframe_f
     "TSNE",  # too slow for using in testing on common data size
 )
 estimators = [
