bf8c75f4f02ad5ef30c7f4f311d7d07f7f06b186
Md Shafiul Alam <md.shafiul.alam@intel.com>
2024-03-22 08:24:56 -0700

    deselected tests

--- sklearnex/cluster/k_means.py
+++ sklearnex/cluster/k_means.py
@@ -312,7 +312,11 @@ if daal_check_version((2023, "P", 200)):
 
         def _onedal_predict(self, X, queue=None):
             X = self._validate_data(
-                X, accept_sparse="csr", reset=False, dtype=[np.float64, np.float32], accept_large_sparse=False,
+                X,
+                accept_sparse="csr",
+                reset=False,
+                dtype=[np.float64, np.float32],
+                accept_large_sparse=False,
             )
             if not hasattr(self, "_onedal_estimator"):
                 self._initialize_onedal_estimator()
