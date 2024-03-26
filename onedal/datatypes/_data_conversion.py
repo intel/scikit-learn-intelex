b16f232f7cbc71617e891393f8eeec4146bd8d3b
ethanglaser <ethan.glaser@intel.com>
2024-03-26 08:08:09 -0700

    debug

--- onedal/datatypes/_data_conversion.py
+++ onedal/datatypes/_data_conversion.py
@@ -67,6 +67,7 @@ if _is_dpc_backend:
         device = policy._queue.sycl_device
 
         def convert_or_pass(x):
+            raise ValueError(f"{x.dtype} {(x.dtype == np.float64)} {x.astype(np.float32).dtype}")
             if (x is not None) and (x.dtype == np.float64):
                 warnings.warn(
                     "Data will be converted into float32 from "
