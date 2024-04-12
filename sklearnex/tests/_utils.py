3478d169cdfcaa5683af6e7f8eea6dd3812e2144
Ian Faust <icfaust@gmail.com>
2024-04-12 22:04:05 +0200

    Update _utils.py

--- sklearnex/tests/_utils.py
+++ sklearnex/tests/_utils.py
@@ -30,6 +30,7 @@ from sklearn.neighbors._base import KNeighborsMixin
 
 from onedal.tests.utils._dataframes_support import _convert_to_dataframe
 from sklearnex import get_patch_map, patch_sklearn, sklearn_is_patched, unpatch_sklearn
+from sklearnex.basic_statistics import IncrementalBasicStatistics
 from sklearnex.linear_model import LogisticRegression
 from sklearnex.neighbors import (
     KNeighborsClassifier,
@@ -97,6 +98,7 @@ SPECIAL_INSTANCES = {
         KNeighborsRegressor(algorithm="brute"),
         NearestNeighbors(algorithm="brute"),
         LogisticRegression(solver="newton-cg"),
+        IncrementalBasicStatistics(),
     ]
 }
 
