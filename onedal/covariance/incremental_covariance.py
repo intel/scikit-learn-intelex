9b5491f7acdeea12648da9a752c251b32587fcaf
icfaust <icfaust@gmail.com>
2024-04-11 22:15:57 -0700

    change order to prevent fp64 issues

--- onedal/covariance/incremental_covariance.py
+++ onedal/covariance/incremental_covariance.py
@@ -18,9 +18,8 @@ import numpy as np
 from daal4py.sklearn._utils import daal_check_version, get_dtype, make2d
 from onedal import _backend
 from onedal.utils import _check_array
-
-from ..datatypes import _convert_to_supported, from_table, to_table
-from .covariance import BaseEmpiricalCovariance
+from onedal.datatypes import _convert_to_supported, from_table, to_table
+from onedal.covariance import BaseEmpiricalCovariance
 
 
 class IncrementalEmpiricalCovariance(BaseEmpiricalCovariance):
@@ -79,10 +78,12 @@ class IncrementalEmpiricalCovariance(BaseEmpiricalCovariance):
 
         if not hasattr(self, "_policy"):
             self._policy = self._get_policy(queue, X)
+
+        X = _convert_to_supported(self._policy, X)
+
         if not hasattr(self, "_dtype"):
             self._dtype = get_dtype(X)
 
-        X = _convert_to_supported(self._policy, X)
         params = self._get_onedal_params(self._dtype)
         table_X = to_table(X)
         self._partial_result = self._get_backend(
