38a05e0c55a35c20742985ed2113ef690893270a
Kruglov, Oleg <oleg.kruglov@intel.com>
2024-04-17 15:41:53 -0700

    Fix example

--- examples/sklearnex/incremental_linear_regression_dpctl.py
+++ examples/sklearnex/incremental_linear_regression_dpctl.py
@@ -27,13 +27,17 @@ queue = dpctl.SyclQueue("gpu")
 inclin = IncrementalLinearRegression()
 
 # We do partial_fit for each batch and then print final result.
-X_1, y_1 = dpt.asarray([[0, 1], [1, 2]]), dpt.asarray([2, 4])
+X_1, y_1 = dpt.asarray([[0, 1], [1, 2]], sycl_queue=queue), dpt.asarray(
+    [2, 4], sycl_queue=queue
+)
 result = inclin.partial_fit(X_1, y_1)
 
-X_2, y_2 = dpt.asarray([[2, 3]]), dpt.asarray([6])
+X_2, y_2 = dpt.asarray([[2, 3]], sycl_queue=queue), dpt.asarray([6], sycl_queue=queue)
 result = inclin.partial_fit(X_2, y_2)
 
-X_3, y_3 = dpt.asarray([[0, 2], [1, 3], [2, 4]]), dpt.asarray([3, 5, 7])
+X_3, y_3 = dpt.asarray([[0, 2], [1, 3], [2, 4]], sycl_queue=queue), dpt.asarray(
+    [3, 5, 7], sycl_queue=queue
+)
 result = inclin.partial_fit(X_3, y_3)
 
 print(f"Coefs:\n{result.coef_}")
@@ -42,9 +46,9 @@ print(f"Intercept:\n{result.intercept_}")
 # We put the whole data to fit method, it is split automatically and then
 # partial_fit is called for each batch.
 inclin = IncrementalLinearRegression(batch_size=3)
-X, y = dpt.asarray([[0, 1], [1, 2], [2, 3], [0, 2], [1, 3], [2, 4]]), dpt.asarray(
-    [2, 4, 6, 3, 5, 7]
-)
+X, y = dpt.asarray(
+    [[0, 1], [1, 2], [2, 3], [0, 2], [1, 3], [2, 4]], sycl_queue=queue
+), dpt.asarray([2, 4, 6, 3, 5, 7], sycl_queue=queue)
 result = inclin.fit(X, y)
 
 print(f"Coefs:\n{result.coef_}")
