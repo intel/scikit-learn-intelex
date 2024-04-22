c79be5d08ad793ce6504f5c27b253d10223f6486
Alexandra <alexandra.epanchinzeva@intel.com>
2024-04-22 14:27:46 +0200

    Update set_version.sh

--- doc/set_version.sh
+++ doc/set_version.sh
@@ -2,6 +2,6 @@ if [ -z "${DALROOT}" ]; then
     export DALROOT=${PREFIX}
 fi
 
-export DOC_VERSION=`python -c "import sys; import os; sys.path.append(os.path.join(os.getcwd(), '..', 'scripts')); from version import get_onedal_version; print(get_onedal_version('${DALROOT}'))"`
+export DOC_VERSION=$(python -c "import sys; import os; sys.path.append(os.path.join(os.getcwd(), '..', 'scripts')); from version import get_onedal_version; print(get_onedal_version('${DALROOT}'))")
 rm -f sources/doc_version.json
 echo "{\"version\":\"$DOC_VERSION\"}" >> sources/doc_version.json
