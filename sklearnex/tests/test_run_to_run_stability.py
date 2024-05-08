e7bb1e2f9cb75c208681efde673f6d66918e1328
icfaust <icfaust@gmail.com>
2024-05-08 00:56:13 -0700

    reformatting

--- sklearnex/tests/test_run_to_run_stability.py
+++ sklearnex/tests/test_run_to_run_stability.py
@@ -231,9 +231,7 @@ def test_other_estimator_stability(estimator, method, dataframe, queue):
         params["random_state"] = 0
         est.set_params(**params)
 
-    datasets = gen_dataset(
-        est, datasets=_dataset_dict, queue=queue, target_df=dataframe
-    )
+    datasets = gen_dataset(est, datasets=_dataset_dict, queue=queue, target_df=dataframe)
     _run_test(est, method, datasets)
 
 
