e0e022cbc29edbb5855d02268da87cfa6832fcc7
Ethan Glaser <ethan.glaser@intel.com>
2024-05-21 16:37:18 -0700

    conditional import

--- daal4py/sklearn/utils/validation.py
+++ daal4py/sklearn/utils/validation.py
@@ -19,7 +19,12 @@ from contextlib import suppress
 
 import numpy as np
 import scipy.sparse as sp
-from numpy.exceptions import ComplexWarning
+try:
+    # numpy_version >= 2.0
+    from numpy.exceptions import ComplexWarning
+except ModuleNotFoundError:
+    # numpy_version < 2.0
+    from numpy.core.numeric import ComplexWarning
 from sklearn import get_config as _get_config
 from sklearn.utils.extmath import _safe_accumulator_op
 from sklearn.utils.fixes import _object_dtype_isnan
