a30082391601e6b946aac5d73e9e8a2c3a3f7457
Ian Faust <icfaust@gmail.com>
2024-05-22 09:41:31 +0200

    Update logistic_regression.py

--- sklearnex/linear_model/logistic_regression.py
+++ sklearnex/linear_model/logistic_regression.py
@@ -303,8 +303,8 @@ if daal_check_version((2024, "P", 1)):
             patching_status.and_conditions(
                 [
                     (
-                        method_name == "fit" or method_name == "predict",
-                        "only fit and predict are supported by oneDAL on cpu",
+                        method_name in ["fit", "predict", "score"]:
+                        "only fit, predict, and score are supported by oneDAL on cpu",
                     )
                 ]
             )
