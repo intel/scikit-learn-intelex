43180645eefd5c76a28ecfa1ea1887fc3bb5c72f
Samir Nasibli <samir.nasibli@intel.com>
2024-06-11 13:44:31 -0700

    minor fix for daal4py/sklearn/linear_model/_coordinate_descent.py

--- daal4py/sklearn/linear_model/_coordinate_descent.py
+++ daal4py/sklearn/linear_model/_coordinate_descent.py
@@ -730,6 +730,8 @@ class ElasticNet(ElasticNet_original):
     def dual_gap_(self):
         self._gap = None
 
+    score = ElasticNet_original.score
+
     fit.__doc__ = ElasticNet_original.fit.__doc__
     predict.__doc__ = ElasticNet_original.predict.__doc__
     score.__doc__ = ElasticNet_original.score.__doc__
