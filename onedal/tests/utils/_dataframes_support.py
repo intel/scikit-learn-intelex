3d26928923f910d3e21b158d213dd2de9e645429
Ian Faust <icfaust@gmail.com>
2024-06-19 12:43:03 +0200

    Update _dataframes_support.py

--- onedal/tests/utils/_dataframes_support.py
+++ onedal/tests/utils/_dataframes_support.py
@@ -16,6 +16,7 @@
 
 import pytest
 from sklearn import get_config
+import scipy.sparse as sp
 
 try:
     import dpctl
@@ -90,6 +91,10 @@ def _as_numpy(obj, *args, **kwargs):
         return dpt.to_numpy(obj, *args, **kwargs)
     if isinstance(obj, pd.DataFrame) or isinstance(obj, pd.Series):
         return obj.to_array(*args, **kwargs)
+    if hasattr(obj, "__dlpack__"):
+        return np.from_dlpack(obj, *args, **kwargs)
+    if sp.issparse(obj):
+        return obj.toarray(*args, **kwargs)
     return np.asarray(obj, *args, **kwargs)
 
 
