d2fc6f37f049f150657accc95bd52127c5ed3d47
Samir Nasibli <samir.nasibli@intel.com>
2024-06-20 07:59:19 -0700

    removed TODO comment from ridge.py
    the comment addressed on #1843

--- sklearnex/linear_model/ridge.py
+++ sklearnex/linear_model/ridge.py
@@ -17,8 +17,6 @@
 from daal4py.sklearn.linear_model import Ridge
 from onedal._device_offload import support_usm_ndarray
 
-# TODO:
-# implement GPU offloading via onedal4py backend.
 Ridge.fit = support_usm_ndarray(queue_param=False)(Ridge.fit)
 Ridge.predict = support_usm_ndarray(queue_param=False)(Ridge.predict)
 Ridge.score = support_usm_ndarray(queue_param=False)(Ridge.score)
