8b000729d0229a19842583671904ca7da8751ad4
Ian Faust <icfaust@gmail.com>
2024-06-21 14:17:06 +0200

    Update test_patching.py

--- sklearnex/tests/test_patching.py
+++ sklearnex/tests/test_patching.py
@@ -165,7 +165,10 @@ def test_standard_estimator_patching(caplog, dataframe, queue, dtype, estimator,
         est.fit(X, y)
 
         if method:
-            if method not in ["score", "partial_fit", "path"]:
+            if method == "inverse_transform":
+                # PCA's inverse_transform takes (n_samples, n_components)
+                data = (X[:, : est.n_components_],)
+            elif method not in ["score", "partial_fit", "path"]:
                 data = (X,)
             else:
                 data = (X, y)
@@ -207,7 +210,10 @@ def test_special_estimator_patching(caplog, dataframe, queue, dtype, estimator, 
             pytest.skip(f"sklearn available_if prevents testing {estimator}.{method}")
 
         if method:
-            if method not in ["score", "partial_fit", "path"]:
+            if method == "inverse_transform":
+                # PCA's inverse_transform takes (n_samples, n_components)
+                data = (X[:, : est.n_components_],)
+            elif method not in ["score", "partial_fit", "path"]:
                 data = (X,)
             else:
                 data = (X, y)
