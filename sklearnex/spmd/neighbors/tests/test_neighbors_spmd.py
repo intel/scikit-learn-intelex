82f3368e1164a9dc3fdb501d914f95f6a3b074da
ethanglaser <ethan.glaser@intel.com>
2024-07-11 13:39:01 -0700

    black + minor fix

--- sklearnex/spmd/neighbors/tests/test_neighbors_spmd.py
+++ sklearnex/spmd/neighbors/tests/test_neighbors_spmd.py
@@ -38,7 +38,10 @@ from sklearnex.tests._utils_spmd import (
     not _mpi_libs_and_gpu_available,
     reason="GPU device and MPI libs required for test",
 )
-@pytest.mark.parametrize("dataframe,queue", get_dataframes_and_queues(dataframe_filter_="dpnp,dpctl", device_filter_="gpu"))
+@pytest.mark.parametrize(
+    "dataframe,queue",
+    get_dataframes_and_queues(dataframe_filter_="dpnp,dpctl", device_filter_="gpu"),
+)
 @pytest.mark.mpi
 def test_knncls_spmd_gold(dataframe, queue):
     # Import spmd and batch algo
@@ -70,9 +73,15 @@ def test_knncls_spmd_gold(dataframe, queue):
         ]
     )
 
-    local_dpt_X_train = _convert_to_dataframe(_get_local_tensor(X_train), sycl_queue=queue, target_df=dataframe)
-    local_dpt_y_train = _convert_to_dataframe(_get_local_tensor(y_train), sycl_queue=queue, target_df=dataframe)
-    local_dpt_X_test = _convert_to_dataframe(_get_local_tensor(X_test), sycl_queue=queue, target_df=dataframe)
+    local_dpt_X_train = _convert_to_dataframe(
+        _get_local_tensor(X_train), sycl_queue=queue, target_df=dataframe
+    )
+    local_dpt_y_train = _convert_to_dataframe(
+        _get_local_tensor(y_train), sycl_queue=queue, target_df=dataframe
+    )
+    local_dpt_X_test = _convert_to_dataframe(
+        _get_local_tensor(X_test), sycl_queue=queue, target_df=dataframe
+    )
 
     # ensure predictions of batch algo match spmd
     spmd_model = KNeighborsClassifier_SPMD(n_neighbors=1, algorithm="brute").fit(
@@ -99,10 +108,19 @@ def test_knncls_spmd_gold(dataframe, queue):
 @pytest.mark.parametrize("n_features_and_classes", [(5, 2), (25, 2), (25, 10)])
 @pytest.mark.parametrize("n_neighbors", [1, 5, 20])
 @pytest.mark.parametrize("weights", ["uniform", "distance"])
-@pytest.mark.parametrize("dataframe,queue", get_dataframes_and_queues(dataframe_filter_="dpnp,dpctl", device_filter_="gpu"))
+@pytest.mark.parametrize(
+    "dataframe,queue",
+    get_dataframes_and_queues(dataframe_filter_="dpnp,dpctl", device_filter_="gpu"),
+)
 @pytest.mark.mpi
 def test_knncls_spmd_synthetic(
-    n_samples, n_features_and_classes, n_neighbors, weights, metric="euclidean", dataframe, queue
+    n_samples,
+    n_features_and_classes,
+    n_neighbors,
+    weights,
+    dataframe,
+    queue,
+    metric="euclidean",
 ):
     n_features, n_classes = n_features_and_classes
     # Import spmd and batch algo
@@ -114,9 +132,15 @@ def test_knncls_spmd_synthetic(
         n_samples, n_features, n_classes
     )
 
-    local_dpt_X_train = _convert_to_dataframe(_get_local_tensor(X_train), sycl_queue=queue, target_df=dataframe)
-    local_dpt_y_train = _convert_to_dataframe(_get_local_tensor(y_train), sycl_queue=queue, target_df=dataframe)
-    local_dpt_X_test = _convert_to_dataframe(_get_local_tensor(X_test), sycl_queue=queue, target_df=dataframe)
+    local_dpt_X_train = _convert_to_dataframe(
+        _get_local_tensor(X_train), sycl_queue=queue, target_df=dataframe
+    )
+    local_dpt_y_train = _convert_to_dataframe(
+        _get_local_tensor(y_train), sycl_queue=queue, target_df=dataframe
+    )
+    local_dpt_X_test = _convert_to_dataframe(
+        _get_local_tensor(X_test), sycl_queue=queue, target_df=dataframe
+    )
 
     # ensure predictions of batch algo match spmd
     spmd_model = KNeighborsClassifier_SPMD(
@@ -139,7 +163,10 @@ def test_knncls_spmd_synthetic(
     not _mpi_libs_and_gpu_available,
     reason="GPU device and MPI libs required for test",
 )
-@pytest.mark.parametrize("dataframe,queue", get_dataframes_and_queues(dataframe_filter_="dpnp,dpctl", device_filter_="gpu"))
+@pytest.mark.parametrize(
+    "dataframe,queue",
+    get_dataframes_and_queues(dataframe_filter_="dpnp,dpctl", device_filter_="gpu"),
+)
 @pytest.mark.mpi
 def test_knnreg_spmd_gold(dataframe, queue):
     # Import spmd and batch algo
@@ -170,9 +197,15 @@ def test_knnreg_spmd_gold(dataframe, queue):
         ]
     )
 
-    local_dpt_X_train = _convert_to_dataframe(_get_local_tensor(X_train), sycl_queue=queue, target_df=dataframe)
-    local_dpt_y_train = _convert_to_dataframe(_get_local_tensor(y_train), sycl_queue=queue, target_df=dataframe)
-    local_dpt_X_test = _convert_to_dataframe(_get_local_tensor(X_test), sycl_queue=queue, target_df=dataframe)
+    local_dpt_X_train = _convert_to_dataframe(
+        _get_local_tensor(X_train), sycl_queue=queue, target_df=dataframe
+    )
+    local_dpt_y_train = _convert_to_dataframe(
+        _get_local_tensor(y_train), sycl_queue=queue, target_df=dataframe
+    )
+    local_dpt_X_test = _convert_to_dataframe(
+        _get_local_tensor(X_test), sycl_queue=queue, target_df=dataframe
+    )
 
     # ensure predictions of batch algo match spmd
     spmd_model = KNeighborsRegressor_SPMD(n_neighbors=1, algorithm="brute").fit(
@@ -202,9 +235,14 @@ def test_knnreg_spmd_gold(dataframe, queue):
 @pytest.mark.parametrize(
     "metric", ["euclidean", "manhattan", "minkowski", "chebyshev", "cosine"]
 )
-@pytest.mark.parametrize("dataframe,queue", get_dataframes_and_queues(dataframe_filter_="dpnp,dpctl", device_filter_="gpu"))
+@pytest.mark.parametrize(
+    "dataframe,queue",
+    get_dataframes_and_queues(dataframe_filter_="dpnp,dpctl", device_filter_="gpu"),
+)
 @pytest.mark.mpi
-def test_knnreg_spmd_synthetic(n_samples, n_features, n_neighbors, weights, metric, dataframe, queue):
+def test_knnreg_spmd_synthetic(
+    n_samples, n_features, n_neighbors, weights, metric, dataframe, queue
+):
     # Import spmd and batch algo
     from sklearnex.neighbors import KNeighborsRegressor as KNeighborsRegressor_Batch
     from sklearnex.spmd.neighbors import KNeighborsRegressor as KNeighborsRegressor_SPMD
@@ -212,9 +250,15 @@ def test_knnreg_spmd_synthetic(n_samples, n_features, n_neighbors, weights, metr
     # Generate data and process into dpt
     X_train, X_test, y_train, _ = _generate_regression_data(n_samples, n_features)
 
-    local_dpt_X_train = _convert_to_dataframe(_get_local_tensor(X_train), sycl_queue=queue, target_df=dataframe)
-    local_dpt_y_train = _convert_to_dataframe(_get_local_tensor(y_train), sycl_queue=queue, target_df=dataframe)
-    local_dpt_X_test = _convert_to_dataframe(_get_local_tensor(X_test), sycl_queue=queue, target_df=dataframe)
+    local_dpt_X_train = _convert_to_dataframe(
+        _get_local_tensor(X_train), sycl_queue=queue, target_df=dataframe
+    )
+    local_dpt_y_train = _convert_to_dataframe(
+        _get_local_tensor(y_train), sycl_queue=queue, target_df=dataframe
+    )
+    local_dpt_X_test = _convert_to_dataframe(
+        _get_local_tensor(X_test), sycl_queue=queue, target_df=dataframe
+    )
 
     # ensure predictions of batch algo match spmd
     spmd_model = KNeighborsRegressor_SPMD(
@@ -228,6 +272,6 @@ def test_knnreg_spmd_synthetic(n_samples, n_features, n_neighbors, weights, metr
     spmd_result = spmd_model.predict(local_dpt_X_test)
     batch_result = batch_model.predict(X_test)
 
-    _assert_unordered_allclose(_as_numpy(spmd_indc), batch_indcs, localize=True)
+    _assert_unordered_allclose(_as_numpy(spmd_indcs), batch_indcs, localize=True)
     _assert_unordered_allclose(_as_numpy(spmd_dists), batch_dists, localize=True)
     _spmd_assert_allclose(_as_numpy(spmd_result), batch_result, atol=1e-4)
