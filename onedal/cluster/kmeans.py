dd552ff5121b4f6b2e93760932f19f8487676c4d
Md Shafiul Alam <md.shafiul.alam@intel.com>
2024-07-15 14:10:28 -0700

    not a table

--- onedal/cluster/kmeans.py
+++ onedal/cluster/kmeans.py
@@ -380,7 +380,7 @@ class _BaseKMeans(onedal_BaseEstimator, TransformerMixin, ClusterMixin, ABC):
 
         if result_options:
             # Only set for score function
-            return from_table(result.objective_function_value)[0] * -1
+            return result.objective_function_value * -1
         else:
             return from_table(result.responses).ravel()
 
