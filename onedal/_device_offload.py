e83612df6df263abcba76193c03e12754d622a1a
olegkkruglov <102592747+olegkkruglov@users.noreply.github.com>
2024-07-16 23:38:11 -0700

    FIX: prevent `support_usm_ndarray` from changing queue if explicitly provided. (#1940)
    (cherry picked from commit 69d147fc986f14083741049d5ea02d7ac4d04eea)
    
    # Conflicts:
    #	onedal/_device_offload.py

--- onedal/_device_offload.py
+++ onedal/_device_offload.py
@@ -59,12 +59,48 @@ def _run_on_device(func, obj=None, *args, **kwargs):
     return func(*args, **kwargs)
 
 
+<<<<<<< HEAD
 def support_usm_ndarray(freefunc=False):
     def decorator(func):
         def wrapper_impl(obj, *args, **kwargs):
             if _sklearnex_available:
                 usm_iface = _extract_usm_iface(*args, **kwargs)
                 data_queue, hostargs, hostkwargs = _get_host_inputs(*args, **kwargs)
+=======
+if dpnp_available:
+
+    def _convert_to_dpnp(array):
+        if isinstance(array, usm_ndarray):
+            return dpnp.array(array, copy=False)
+        elif isinstance(array, Iterable):
+            for i in range(len(array)):
+                array[i] = _convert_to_dpnp(array[i])
+        return array
+
+
+def support_usm_ndarray(freefunc=False, queue_param=True):
+    """
+    Handles USMArray input. Puts SYCLQueue from data to decorated function arguments.
+    Converts output of decorated function to dpctl.tensor/dpnp.ndarray if input was of this type.
+
+    Parameters
+    ----------
+    freefunc (bool) : Set to True if decorates free function.
+    queue_param (bool) : Set to False if the decorated function has no `queue` parameter
+
+    Notes
+    -----
+    Queue will not be changed if provided explicitly.
+    """
+
+    def decorator(func):
+        def wrapper_impl(obj, *args, **kwargs):
+            usm_iface = _extract_usm_iface(*args, **kwargs)
+            data_queue, hostargs, hostkwargs = _get_host_inputs(*args, **kwargs)
+            if queue_param and not (
+                "queue" in hostkwargs and hostkwargs["queue"] is not None
+            ):
+>>>>>>> 69d147fc (FIX: prevent `support_usm_ndarray` from changing queue if explicitly provided. (#1940))
                 hostkwargs["queue"] = data_queue
                 result = _run_on_device(func, obj, *hostargs, **hostkwargs)
                 if usm_iface is not None and hasattr(result, "__array_interface__"):
