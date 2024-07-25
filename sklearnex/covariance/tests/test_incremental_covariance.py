f6cdaa2c12de6e3915209fa8c079e24f8c508898
Kruglov, Oleg <oleg.kruglov@intel.com>
2024-07-25 02:22:00 -0700

    Fix is_gpu check

--- sklearnex/covariance/tests/test_incremental_covariance.py
+++ sklearnex/covariance/tests/test_incremental_covariance.py
@@ -42,7 +42,7 @@ def test_sklearnex_partial_fit_on_gold_data(dataframe, queue, dtype, assume_cent
         from dpctl import device_type
 
         is_gpu = queue is not None and queue.sycl_device.device_type == device_type.gpu
-    finally:
+    except:
         is_gpu = False
     if assume_centered and is_gpu and not daal_check_version((2024, "P", 700)):
         pytest.skip("There is a bug on oneDAL side")
@@ -156,7 +156,7 @@ def test_sklearnex_fit_on_random_data(
         from dpctl import device_type
 
         is_gpu = queue is not None and queue.sycl_device.device_type == device_type.gpu
-    finally:
+    except:
         is_gpu = False
     if assume_centered and is_gpu and not daal_check_version((2024, "P", 700)):
         pytest.skip("There is a bug on oneDAL side")
