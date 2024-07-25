05934e0ca3b094e7d79dc226df882e863a9926b2
Ian Faust <icfaust@gmail.com>
2024-07-25 08:56:36 +0200

    Update ridge.py

--- sklearnex/preview/linear_model/ridge.py
+++ sklearnex/preview/linear_model/ridge.py
@@ -31,6 +31,9 @@ if daal_check_version((2024, "P", 600)):
 
     if sklearn_check_version("1.0") and not sklearn_check_version("1.2"):
         from sklearn.linear_model._base import _deprecate_normalize
+    if sklearn_check_version("1.1") and not sklearn_check_version("1.2"):
+        from sklearn.utils import check_scalar
+
 
     from onedal.linear_model import Ridge as onedal_Ridge
     from onedal.utils import _num_features, _num_samples
@@ -274,6 +277,23 @@ if daal_check_version((2024, "P", 600)):
             # `Sample weight` is not supported. Expected to be None value.
             assert sample_weight is None
 
+            if sklearn_check_version("1.2"):
+                self._validate_params()
+            elif sklearn_check_version("1.1"):
+                if self.max_iter is not None:
+                    self.max_iter = check_scalar(
+                        self.max_iter, "max_iter", target_type=numbers.Integral, min_val=1
+                    )
+                self.tol = check_scalar(self.tol, "tol", target_type=numbers.Real, min_val=0.0)
+                if self.alpha is not None and not isinstance(self.alpha, (np.ndarray, tuple)):
+                    self.alpha = check_scalar(
+                        self.alpha,
+                        "alpha",
+                        target_type=numbers.Real,
+                        min_val=0.0,
+                        include_boundaries="left",
+                    )
+
             check_params = {
                 "X": X,
                 "y": y,
@@ -284,7 +304,6 @@ if daal_check_version((2024, "P", 600)):
             }
             if sklearn_check_version("1.2"):
                 X, y = self._validate_data(**check_params)
-                self._validate_params()
             else:
                 X, y = check_X_y(**check_params)
 
