45c8957de2542686519691aed83f97f1a7806b76
Ian Faust <icfaust@gmail.com>
2024-07-26 23:14:15 +0200

    Update dispatcher.py

--- sklearnex/dispatcher.py
+++ sklearnex/dispatcher.py
@@ -414,7 +414,7 @@ def get_patch_map_core(preview=False):
         ]
 
         # Louvain
-         mapping["louvain"] = [
+        mapping["louvain"] = [
             [
                 (
                     cluster_module,
