1fe780910440055c8b07355fe6e0460f850e622a
Ian Faust <icfaust@gmail.com>
2024-07-26 22:59:52 +0200

    Update _louvain.py

--- sklearnex/cluster/_louvain.py
+++ sklearnex/cluster/_louvain.py
@@ -299,8 +299,8 @@ class Louvain(ClusterMixin, BaseEstimator):
                 n_jobs=self.n_jobs,
                 metric="precomputed" if "precomputed" in self.affinity else "minkowski",
             ).fit(X)
-            dist = estimator.kneighbors_graph(mode="distance")
-            self.affinity_matrix_ = 0.5 * (dist + dist.T)
+            connectivity = estimator.kneighbors_graph(mode="connectivity")
+            self.affinity_matrix_ = 0.5 * (connectivity + connectivity.T)
         elif self.affinity == "precomputed":
             self.affinity_matrix_ = X
         else:
