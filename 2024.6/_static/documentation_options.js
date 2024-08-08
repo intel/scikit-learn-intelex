90d2f90f73602928c9d0c916a0118d2661b3721f
bdmoore1 <benjamin.d.moore@intel.com>
2024-08-07 17:44:19 -0700

    Update Anaconda references

--- 2024.6/_static/documentation_options.js
+++ 2024.6/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2024.6',
+    VERSION: '2024.3.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
