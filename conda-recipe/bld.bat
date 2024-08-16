db5957d299b2165cea80c3f4ed5f1a0f85917821
Alexander Andreev <alexander.andreev@intel.com>
2024-08-16 14:33:43 +0100

    Add CC and CXX reset on Windows

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -20,6 +20,11 @@ IF DEFINED PKG_VERSION (set SKLEARNEX_VERSION=%PKG_VERSION%)
 IF NOT DEFINED DALROOT (set DALROOT=%PREFIX%)
 IF NOT DEFINED MPIROOT (set MPIROOT=%PREFIX%\Library)
 
+rem reset preferred compilers to avoid usage of icx/icpx by default in all cases
+set CC=cl.exe
+set CXX=cl.exe
+
+rem source compiler if DPCPPROOT is set outside of conda-build
 IF DEFINED DPCPPROOT (
     echo "Sourcing DPCPPROOT"
     call "%DPCPPROOT%\env\vars.bat"
