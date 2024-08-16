3d17efd66d30f135cd6a74f8147f84a9762b0a44
Alexander Andreev <alexander.andreev@intel.com>
2024-08-16 15:52:20 +0100

    Remove CC/CXX hints on Win

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -20,12 +20,6 @@ IF DEFINED PKG_VERSION (set SKLEARNEX_VERSION=%PKG_VERSION%)
 IF NOT DEFINED DALROOT (set DALROOT=%PREFIX%)
 IF NOT DEFINED MPIROOT (set MPIROOT=%PREFIX%\Library)
 
-rem reset preferred compilers to avoid usage of icx/icpx by default in all cases
-set CC=cl.exe
-set CXX=cl.exe
-set CMAKE_C_COMPILER=cl.exe
-set CMAKE_CXX_COMPILER=cl.exe
-
 rem source compiler if DPCPPROOT is set outside of conda-build
 IF DEFINED DPCPPROOT (
     echo "Sourcing DPCPPROOT"
