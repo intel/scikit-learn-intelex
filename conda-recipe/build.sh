7de77039420b53440e62de54e8d278b08a8bdf53
Alexander Andreev <alexander.andreev@intel.com>
2024-08-16 13:54:45 +0100

    Add dpcpp compiler in recipe

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -30,7 +30,12 @@ fi
 if [ -z "${MPIROOT}" ]; then
     export MPIROOT=${PREFIX}
 fi
-
+# reset preferred compilers to avoid usage of icx/icpx by default in all cases
+if [ -z "${CC_FOR_BUILD}" ] && [ -z "${CXX_FOR_BUILD}" ]; then
+    export CC=$CC_FOR_BUILD
+    export CXX=$CXX_FOR_BUILD
+fi
+# source compiler if DPCPPROOT is set outside of conda-build
 if [ ! -z "${DPCPPROOT}" ]; then
     source ${DPCPPROOT}/env/vars.sh
 fi
