37ba94a514a75c5df44f5a9adb1db1cf158af0a5
Alexander Andreev <alexander.andreev@intel.com>
2024-08-16 14:01:02 +0100

    Fix CC_FOR_BUILD condition

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -31,7 +31,7 @@ if [ -z "${MPIROOT}" ]; then
     export MPIROOT=${PREFIX}
 fi
 # reset preferred compilers to avoid usage of icx/icpx by default in all cases
-if [ -z "${CC_FOR_BUILD}" ] && [ -z "${CXX_FOR_BUILD}" ]; then
+if [ ! -z "${CC_FOR_BUILD}" ] && [ ! -z "${CXX_FOR_BUILD}" ]; then
     export CC=$CC_FOR_BUILD
     export CXX=$CXX_FOR_BUILD
 fi
