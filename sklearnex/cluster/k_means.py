8bdf8da737ebe16bccebc420a17d9c33a90db90a
Md Shafiul Alam <md.shafiul.alam@intel.com>
2024-09-11 07:26:49 -0700

    ci fix

--- sklearnex/cluster/k_means.py
+++ sklearnex/cluster/k_means.py
@@ -109,10 +109,11 @@ if daal_check_version((2023, "P", 200)):
                 _is_csr(X) and daal_check_version((2024, "P", 700))
             ) or not issparse(X)
 
-            _acceptable_sample_weights = sample_weight is None or isinstance(
-                sample_weight, numbers.Number
-            )
-            if isinstance(sample_weight, np.ndarray):
+            _acceptable_sample_weights = True
+            if sample_weight is not None or not isinstance(sample_weight, numbers.Number):
+                sample_weight = _check_sample_weight(
+                    sample_weight, X, dtype=X.dtype if hasattr(X, "dtype") else None
+                )
                 _acceptable_sample_weights = np.all(sample_weight == 1)
 
             patching_status.and_conditions(
@@ -124,7 +125,7 @@ if daal_check_version((2023, "P", 200)):
                     (correct_count, "n_clusters is smaller than number of samples"),
                     (
                         _acceptable_sample_weights,
-                        "oneDAL doesn't support sample_weight, None or equal weights are accepted.",
+                        "oneDAL doesn't support sample_weight. Accepted options are None, constant, or array of ones.",
                     ),
                     (
                         is_data_supported,
