f682b81d68617edaa810c911f045ee7006d6af77
icfaust <icfaust@gmail.com>
2024-09-17 15:12:35 -0700

    fix discrepancy in call_method

--- sklearnex/tests/_utils.py
+++ sklearnex/tests/_utils.py
@@ -232,7 +232,7 @@ def call_method(estimator, method, X, y, **kwargs):
 
     func = getattr(estimator, method)
     argdict = signature(func).parameters
-    argnum = len([i for i in argdict if argdict[i].default == Parameter.empty])
+    argnum = len([i for i in argdict if argdict[i].default == Parameter.empty or i in ["X", "y"]])
 
     if method == "inverse_transform":
         # PCA's inverse_transform takes (n_samples, n_components)
