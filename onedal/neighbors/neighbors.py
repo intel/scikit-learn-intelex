16107cbc62bcaaa84e9638a2d8f388bd855fde22
Ian Faust <icfaust@gmail.com>
2024-09-18 17:09:24 +0200

    Update neighbors.py

--- onedal/neighbors/neighbors.py
+++ onedal/neighbors/neighbors.py
@@ -600,7 +600,7 @@ class KNeighborsRegressor(NeighborsBase, RegressorMixin):
         X = _convert_to_supported(policy, X)
         backend = (
             self._get_backend("neighbors", "regression", None)
-            if gpu_device
+            if queue is not None and queue.sycl_device.is_gpu:
             else self._get_backend("neighbors", "search", None)
         )
 
