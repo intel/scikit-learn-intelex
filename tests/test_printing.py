496519fce4774d176c9bfb0d494ab90fbf4e0212
David Cortes <david.cortes@intel.com>
2024-09-25 16:18:32 +0200

    add repr to print in ipython

---
+++ tests/test_printing.py
@@ -0,0 +1,32 @@
+# ==============================================================================
+# Copyright 2024 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import re
+import unittest
+
+import numpy as np
+
+import daal4py
+
+
+class Test(unittest.TestCase):
+    def test_printing(self):
+        rng = np.random.default_rng(seed=123)
+        X = rng.standard_normal(size=(10, 5))
+        qr_algorithm = daal4py.qr()
+        qr_result = qr_algorithm.compute(X)
+        assert "matrixQ" in qr_result.__str__()
+        assert "matrixQ" in qr_result.__repr__()
