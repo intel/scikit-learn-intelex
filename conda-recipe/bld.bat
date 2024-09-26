326363b3b6e0f170c1ec00cfa33300fef9f88cb3
Alexander Andreev <alexander.andreev@intel.com>
2024-09-26 10:12:12 +0100

    Fix MPIROOT setting

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -18,7 +18,7 @@ rem ============================================================================
 IF NOT DEFINED PYTHON (set PYTHON="python")
 IF DEFINED PKG_VERSION (set SKLEARNEX_VERSION=%PKG_VERSION%)
 IF NOT DEFINED DALROOT (set DALROOT=%PREFIX%)
-IF NOT DEFINED MPIROOT IF "%NO_DIST%"=="" (set MPIROOT=%PREFIX%)
+IF NOT DEFINED MPIROOT IF "%NO_DIST%"=="" (set MPIROOT=%PREFIX%\Library)
 
 rem reset preferred compilers to avoid usage of icx/icpx by default in all cases
 set CC=cl.exe
