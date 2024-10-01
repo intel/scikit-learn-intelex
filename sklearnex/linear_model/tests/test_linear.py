577dce15f2f5219f212e49975900136bccffa64e
Ian Faust <icfaust@gmail.com>
2024-10-01 16:58:31 +0200

    Update test_linear.py

--- sklearnex/linear_model/tests/test_linear.py
+++ sklearnex/linear_model/tests/test_linear.py
@@ -56,7 +56,7 @@ def test_sklearnex_import_linear(dataframe, queue, dtype, macro_block):
     assert "sklearnex" in linreg.__module__
     assert linreg.n_features_in_ == 2
 
-    tol = 1e-5 if _as_numpy(linreg.coef_).dtype == np.float32 else 1e-7
+    tol = 2e-5 if _as_numpy(linreg.coef_).dtype == np.float32 else 1e-7
     assert_allclose(_as_numpy(linreg.intercept_), 3.0, rtol=tol)
     assert_allclose(_as_numpy(linreg.coef_), [1.0, 2.0], rtol=tol)
 
