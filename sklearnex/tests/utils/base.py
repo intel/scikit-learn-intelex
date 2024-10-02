4a025e9ba81d64e41bb0ea80335b15e9efd24341
icfaust <icfaust@gmail.com>
2024-10-02 06:15:44 -0700

    make explicit

--- sklearnex/tests/utils/base.py
+++ sklearnex/tests/utils/base.py
@@ -351,7 +351,7 @@ DTYPES = [
 def _get_processor_info():
     proc = ""
     if platform.system() == "Linux":
-        proc = subprocess.check_output(["cat", "/proc/cpuinfo"]).strip().decode("utf-8")
+        proc = subprocess.check_output(["/usr/bin/cat", "/proc/cpuinfo"]).strip().decode("utf-8")
     elif platform.system() == "Windows":
         proc = platform.processor()
     elif platform.system() == "Darwin":
