1ad82a1185fc16473f51cae9cace3475e58bbd85
Ian Faust <icfaust@gmail.com>
2024-10-11 17:01:10 +0200

    Update run_test.sh

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -31,6 +31,11 @@ if [[ count -eq 0 ]]; then
     exit 1
 fi
 
+COV_CMD = "--no-cov"
+if [[ "$COVERAGE" == "true" ]]; then
+COV_CMD = "--cov-config='$daal4py_dir/.coveragerc' --cov --cov-report="
+fi
+
 echo "Start testing ..."
 return_code=0
 
@@ -50,15 +55,15 @@ python -m unittest discover -v -s ${daal4py_dir}/tests -p test*.py
 return_code=$(($return_code + $?))
 
 echo "Pytest of daal4py running ..."
-pytest --verbose --pyargs ${daal4py_dir}/daal4py/sklearn
+pytest --verbose --${daal4py_dir}/daal4py/sklearn $COV_CMD
 return_code=$(($return_code + $?))
 
 echo "Pytest of sklearnex running ..."
-pytest --verbose --pyargs sklearnex
+pytest --verbose --pyargs sklearnex $COV_CMD
 return_code=$(($return_code + $?))
 
 echo "Pytest of onedal running ..."
-pytest --verbose --pyargs ${daal4py_dir}/onedal
+pytest --verbose --pyargs onedal $COV_CMD
 return_code=$(($return_code + $?))
 
 echo "Global patching test running ..."
