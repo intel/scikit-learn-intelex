64f807a37e17d2ad37890af66e99142b27b198a5
Faust, Ian <ian.faust@intel.com>
2024-10-11 10:42:32 +0200

    remove comment

--- sklearnex/tests/test_common.py
+++ sklearnex/tests/test_common.py
@@ -341,7 +341,6 @@ def n_jobs_check(text, estimator, method):
 
 def runtime_property_check(text, estimator, method):
     """use of Python's 'property' should not be used at runtime, only at class instantiation"""
-    # remove the _get_backend function from sklearnex from considered _get_backend
     assert (
         len(re.findall("property(", text[1])) == 0
     ), f"{estimator}.{method} should only use 'property' at instantiation"
