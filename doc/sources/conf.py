3d35d180647d388d8856ec0c3284b3a482050084
Kruglov, Oleg <oleg.kruglov@intel.com>
2024-10-14 09:51:29 -0700

    Add back accidentally removed empty line

--- doc/sources/conf.py
+++ doc/sources/conf.py
@@ -34,6 +34,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath("../"))
 
+
 # -- Project information -----------------------------------------------------
 
 project = "Intel(R) Extension for Scikit-learn*"
