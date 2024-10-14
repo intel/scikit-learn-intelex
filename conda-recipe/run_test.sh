34dec52d2899b02b62c8bcd4984bde46f7d978a5
Ian Faust <icfaust@gmail.com>
2024-10-14 09:13:33 +0200

    Update run_test.sh

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -33,7 +33,7 @@ fi
 
 COV_ARGS=(--no-cov)
 if [[ "$COVERAGE" == "true" ]]; then
-COV_ARGS=(--cov-config '$daal4py_dir/.coveragerc' --cov-report term)
+COV_ARGS=(--cov-config "$daal4py_dir/.coveragerc" --cov-report term)
 fi
 
 echo "Start testing ..."
