7e74fc3648d5f9b2eef37d3a5ed2e1c2dc17a7ef
Ian Faust <icfaust@gmail.com>
2024-10-14 23:19:46 +0200

    Update sklearnex/tests/test_common.py
    Co-authored-by: Samir Nasibli <samir.nasibli@intel.com>

--- sklearnex/tests/test_common.py
+++ sklearnex/tests/test_common.py
@@ -157,9 +157,9 @@ def test_class_trailing_underscore_ban(monkeypatch):
             # to error if queried and the estimator is not fitted
             assert all(
                 [
-                    isinstance(getattr(obj, i), property)
-                    or (i.startswith("_") or not i.endswith("_"))
-                    for i in dir(obj)
+                    isinstance(getattr(obj, attr), property)
+                    or (attr.startswith("_") or not attr.endswith("_"))
+                    for attr in dir(obj)
                 ]
             ), f"{name} contains class attributes which have a trailing underscore but no leading one"
 
