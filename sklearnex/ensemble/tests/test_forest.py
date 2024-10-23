432010f08a8c6e6bd1895a3f5c11c804fe7233c2
Andreas Huber <andreas.huber@intel.com>
2024-10-23 08:53:28 -0700

    fix test

--- sklearnex/ensemble/tests/test_forest.py
+++ sklearnex/ensemble/tests/test_forest.py
@@ -25,28 +25,6 @@ from onedal.tests.utils._dataframes_support import (
     get_dataframes_and_queues,
 )
 
-
-def test_rf_classifier(queue, block, trees, rows, scale):
-    from sklearnex.ensemble import RandomForestClassifier
-
-    X, y = make_classification(
-        n_samples=100,
-        n_features=4,
-        n_informative=2,
-        n_redundant=0,
-        random_state=0,
-        shuffle=False,
-    )
-    rf = RandomForestClassifier(max_depth=2, random_state=0).fit(X, y, queue=queue)
-    if daal_check_version((2024, "P", 300)) and block is not None:
-        hparams = rf.get_hyperparameters("infer")
-        hparams.block_size = block
-        hparams.min_trees_for_threading = trees
-        hparams.min_number_of_rows_for_vect_seq_compute = rows
-        hparams.scale_factor_for_vect_parallel_compute = scale
-    assert_allclose([1], rf.predict([[0, 0, 0, 0]], queue=queue))
-
-
 hparam_values = [
     (None, None, None, None),
     (8, 100, 32, 0.3),
@@ -73,6 +51,12 @@ def test_sklearnex_import_rf_classifier(dataframe, queue, block, trees, rows, sc
     X = _convert_to_dataframe(X, sycl_queue=queue, target_df=dataframe)
     y = _convert_to_dataframe(y, sycl_queue=queue, target_df=dataframe)
     rf = RandomForestClassifier(max_depth=2, random_state=0).fit(X, y)
+    hparams = rf.get_hyperparameters("infer")
+    if hparams and block is not None:
+        hparams.block_size = block
+        hparams.min_trees_for_threading = trees
+        hparams.min_number_of_rows_for_vect_seq_compute = rows
+        hparams.scale_factor_for_vect_parallel_compute = scale
     assert "sklearnex" in rf.__module__
     assert_allclose([1], _as_numpy(rf.predict([[0, 0, 0, 0]])))
 
