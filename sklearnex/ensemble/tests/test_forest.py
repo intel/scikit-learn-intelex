d9db8ccc93325ccbf18ae54ba89f3dadaca20462
Andreas Huber <9201869+ahuber21@users.noreply.github.com>
2024-10-24 13:40:07 +0200

    FEAT(forest): hyperparameter support for RF clf inference (#1751)
    * expand forest classes with hyperparams
    
    * fix build errors
    
    * add some version checks
    
    * black format
    
    * version check for hyperparameters backend
    
    * fixup
    
    * fixup
    
    * fixup
    
    * fixup
    
    * add version check for hyperparams
    
    * Add daal_require_version_wrapper
    
    * simplify obtaining hparams
    
    * fix version checks
    
    * Fix ONEDAL_VERSION in comment
    
    Co-authored-by: Victoriya Fedotova <viktoria.nn@gmail.com>
    
    * Add version comment to endif
    
    Co-authored-by: Victoriya Fedotova <viktoria.nn@gmail.com>
    
    * add block_size_multiplier hyperparam
    
    * fix argument name
    
    * add hyperparameter tests
    
    * review comments
    
    * fixup for registe_hyperparameters
    
    * simplify get_hyperparamters() definition
    
    * rename set_block_size
    
    * remove block size multiplier from testing
    
    * modify hyperparameters getattr/setattr for better debug output
    
    * instantiate hyperparams for entire task_list
    
    * rename df_infer_hp
    
    * instantiate hp only for classification
    
    * Retrieve hparams only for classification
    
    * change hyperparam values to %8 values
    
    * [workaround] use older artifacts to avoid pipeline error - revert before merge
    
    * Revert "[workaround] use older artifacts to avoid pipeline error - revert before merge"
    
    This reverts commit 294396cdc30334bf12c295352e06a9aaba6ae178.
    
    * improve comment on hyperparameters __getattr__
    
    * simplify hyperparameters retrieval
    
    * Revert adding of daal_require_version_wrapper
    
    * clean up imports
    
    * clean up unused type
    
    * fixup after renaming predict->infer
    
    * move get_hyperparameters API to sklearnex
    
    * move tests to sklearnex
    
    * fix test
    
    ---------
    
    Co-authored-by: Victoriya Fedotova <viktoria.nn@gmail.com>

--- sklearnex/ensemble/tests/test_forest.py
+++ sklearnex/ensemble/tests/test_forest.py
@@ -14,7 +14,6 @@
 # limitations under the License.
 # ===============================================================================
 
-import numpy as np
 import pytest
 from numpy.testing import assert_allclose
 from sklearn.datasets import make_classification, make_regression
@@ -26,9 +25,19 @@ from onedal.tests.utils._dataframes_support import (
     get_dataframes_and_queues,
 )
 
+hparam_values = [
+    (None, None, None, None),
+    (8, 100, 32, 0.3),
+    (16, 100, 32, 0.3),
+    (32, 100, 32, 0.3),
+    (64, 10, 32, 0.1),
+    (128, 100, 1000, 1.0),
+]
 
-@pytest.mark.parametrize("dataframe,queue", get_dataframes_and_queues())
-def test_sklearnex_import_rf_classifier(dataframe, queue):
+
+@pytest.mark.parametrize("dataframe, queue", get_dataframes_and_queues())
+@pytest.mark.parametrize("block, trees, rows, scale", hparam_values)
+def test_sklearnex_import_rf_classifier(dataframe, queue, block, trees, rows, scale):
     from sklearnex.ensemble import RandomForestClassifier
 
     X, y = make_classification(
@@ -42,6 +51,12 @@ def test_sklearnex_import_rf_classifier(dataframe, queue):
     X = _convert_to_dataframe(X, sycl_queue=queue, target_df=dataframe)
     y = _convert_to_dataframe(y, sycl_queue=queue, target_df=dataframe)
     rf = RandomForestClassifier(max_depth=2, random_state=0).fit(X, y)
+    hparams = rf.get_hyperparameters("infer")
+    if hparams and block is not None:
+        hparams.block_size = block
+        hparams.min_trees_for_threading = trees
+        hparams.min_number_of_rows_for_vect_seq_compute = rows
+        hparams.scale_factor_for_vect_parallel_compute = scale
     assert "sklearnex" in rf.__module__
     assert_allclose([1], _as_numpy(rf.predict([[0, 0, 0, 0]])))
 
