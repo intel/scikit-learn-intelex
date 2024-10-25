ebd6279d02704485f6a7f79f29858831fc21fd67
Ian Faust <icfaust@gmail.com>
2024-10-25 11:10:14 +0200

    Update run_test.bat

--- conda-recipe/run_test.bat
+++ conda-recipe/run_test.bat
@@ -21,6 +21,8 @@ set exitcode=0
 
 IF NOT DEFINED PYTHON (set "PYTHON=python")
 
+IF DEFINED COVERAGE_RCFILE (set COVARGS=--cov=onedal --cov=sklearnex --cov-config=%COVERAGE_RCFILE% --cov-append --cov-report=) else (set COVARGS=)
+
 %PYTHON% -c "from sklearnex import patch_sklearn; patch_sklearn()" || set exitcode=1
 
 rem Note: execute with argument --json-report as second argument
@@ -33,21 +35,21 @@ if "%~2"=="--json-report" (
 )
 
 if "%with_json_report%"=="1" (
-    %PYTHON% -m pytest --verbose -s %1tests --json-report --json-report-file=.pytest_reports\legacy_report.json || set exitcode=1
-    pytest --verbose --pyargs daal4py --json-report --json-report-file=.pytest_reports\daal4py_report.json || set exitcode=1
-    pytest --verbose --pyargs sklearnex --json-report --json-report-file=.pytest_reports\sklearnex_report.json || set exitcode=1
-    pytest --verbose --pyargs onedal --json-report --json-report-file=.pytest_reports\onedal_report.json || set exitcode=1
-    pytest --verbose %1.ci\scripts\test_global_patch.py --json-report --json-report-file=.pytest_reports\global_patching_report.json || set exitcode=1
+    %PYTHON% -m pytest --verbose -s %1tests --json-report --json-report-file=.pytest_reports\legacy_report.json %COVARGS% || set exitcode=1
+    pytest --verbose --pyargs daal4py --json-report --json-report-file=.pytest_reports\daal4py_report.json %COVARGS% || set exitcode=1
+    pytest --verbose --pyargs sklearnex --json-report --json-report-file=.pytest_reports\sklearnex_report.json %COVARGS% || set exitcode=1
+    pytest --verbose --pyargs onedal --json-report --json-report-file=.pytest_reports\onedal_report.json %COVARGS% || set exitcode=1
+    pytest --verbose %1.ci\scripts\test_global_patch.py --json-report --json-report-file=.pytest_reports\global_patching_report.json %COVARGS% || set exitcode=1
     if NOT EXIST .pytest_reports\legacy_report.json (
         echo "Error: JSON report files failed to be produced."
         set exitcode=1
     )
 ) else (
-    %PYTHON% -m pytest --verbose -s %1tests || set exitcode=1
-    pytest --verbose --pyargs daal4py || set exitcode=1
-    pytest --verbose --pyargs sklearnex || set exitcode=1
-    pytest --verbose --pyargs onedal || set exitcode=1
-    pytest --verbose %1.ci\scripts\test_global_patch.py || set exitcode=1
+    %PYTHON% -m pytest --verbose -s %1tests %COVARGS% || set exitcode=1
+    pytest --verbose --pyargs daal4py %COVARGS% || set exitcode=1
+    pytest --verbose --pyargs sklearnex %COVARGS% || set exitcode=1
+    pytest --verbose --pyargs onedal %COVARGS% || set exitcode=1
+    pytest --verbose %1.ci\scripts\test_global_patch.py %COVARGS% || set exitcode=1
 )
 
 EXIT /B %exitcode%
