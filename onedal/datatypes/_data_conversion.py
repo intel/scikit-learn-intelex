026aca1250edb5f212a32d12733f868938f89e86
Samir Nasibli <samir.nasibli@intel.com>
2024-10-26 00:11:53 -0700

    import time check for _is_dpc_backend

--- onedal/datatypes/_data_conversion.py
+++ onedal/datatypes/_data_conversion.py
@@ -34,62 +34,8 @@ def _apply_and_pass(func, *args, **kwargs):
     return tuple(map(lambda arg: func(arg, **kwargs), args))
 
 
-def convert_one_from_table(table, sycl_queue=None, sua_iface=None, xp=None):
-    # Currently only `__sycl_usm_array_interface__` protocol used to
-    # convert into dpnp/dpctl tensors.
-    if sua_iface:
-        if _is_dpc_backend:
-            if (
-                sycl_queue
-                and sycl_queue.sycl_device.is_cpu
-                and table.__sycl_usm_array_interface__["syclobj"] is None
-            ):
-                # oneDAL returns tables with None sycl queue for CPU sycl queue inputs.
-                # This workaround is necessary for the functional preservation
-                # of the compute-follows-data execution.
-                # Host tables first converted into numpy.narrays and then to array from xp
-                # namespace.
-                return xp.asarray(
-                    _backend.from_table(table), usm_type="device", sycl_queue=sycl_queue
-                )
-            else:
-                xp_name = xp.__name__
-                if dpnp_available and xp_name == "dpnp":
-                    # By default DPNP ndarray created with a copy.
-                    # TODO:
-                    # investigate why dpnp.array(table, copy=False) doesn't work.
-                    # Work around with using dpctl.tensor.asarray.
-                    return xp.array(dpt.asarray(table), copy=False)
-                else:
-                    return xp.asarray(table)
-        raise RuntimeError(
-            "SYCL usm array conversion from table requires the DPC backend"
-        )
-    return _backend.from_table(table)
-
-
-def convert_one_to_table(arg, sua_iface=None):
-    if sua_iface:
-        if _is_dpc_backend:
-            return _backend.sua_iface_to_table(arg)
-        raise RuntimeError("SYCL usm array conversion to table requires the DPC backend")
-
-    if not _is_csr(arg):
-        arg = make2d(arg)
-    return _backend.to_table(arg)
-
-
-def from_table(*args, sycl_queue=None, sua_iface=None, xp=None):
-    return _apply_and_pass(
-        convert_one_from_table, *args, sycl_queue=sycl_queue, sua_iface=sua_iface, xp=xp
-    )
-
-
-def to_table(*args, sua_iface=None):
-    return _apply_and_pass(convert_one_to_table, *args, sua_iface=sua_iface)
-
-
 if _is_dpc_backend:
+
     from ..common._policy import _HostInteropPolicy
 
     def _convert_to_supported(policy, *data):
@@ -119,6 +65,43 @@ if _is_dpc_backend:
 
         return _apply_and_pass(func, *data)
 
+    def convert_one_from_table(table, sycl_queue=None, sua_iface=None, xp=None):
+        # Currently only `__sycl_usm_array_interface__` protocol used to
+        # convert into dpnp/dpctl tensors.
+        if sua_iface:
+            if (
+                sycl_queue
+                and sycl_queue.sycl_device.is_cpu
+                and table.__sycl_usm_array_interface__["syclobj"] is None
+            ):
+                # oneDAL returns tables with None sycl queue for CPU sycl queue inputs.
+                # This workaround is necessary for the functional preservation
+                # of the compute-follows-data execution.
+                # Host tables first converted into numpy.narrays and then to array from xp
+                # namespace.
+                return xp.asarray(
+                    _backend.from_table(table), usm_type="device", sycl_queue=sycl_queue
+                )
+            else:
+                xp_name = xp.__name__
+                if dpnp_available and xp_name == "dpnp":
+                    # By default DPNP ndarray created with a copy.
+                    # TODO:
+                    # investigate why dpnp.array(table, copy=False) doesn't work.
+                    # Work around with using dpctl.tensor.asarray.
+                    return xp.array(dpt.asarray(table), copy=False)
+                else:
+                    return xp.asarray(table)
+        return _backend.from_table(table)
+
+    def convert_one_to_table(arg, sua_iface=None):
+        if sua_iface:
+            return _backend.sua_iface_to_table(arg)
+
+        if not _is_csr(arg):
+            arg = make2d(arg)
+        return _backend.to_table(arg)
+
 else:
 
     def _convert_to_supported(policy, *data):
@@ -126,3 +109,32 @@ else:
             return x
 
         return _apply_and_pass(func, *data)
+
+    def convert_one_from_table(table, sycl_queue=None, sua_iface=None, xp=None):
+        # Currently only `__sycl_usm_array_interface__` protocol used to
+        # convert into dpnp/dpctl tensors.
+        if sua_iface:
+            raise RuntimeError(
+                "SYCL usm array conversion from table requires the DPC backend"
+            )
+        return _backend.from_table(table)
+
+    def convert_one_to_table(arg, sua_iface=None):
+        if sua_iface:
+            raise RuntimeError(
+                "SYCL usm array conversion to table requires the DPC backend"
+            )
+
+        if not _is_csr(arg):
+            arg = make2d(arg)
+        return _backend.to_table(arg)
+
+
+def from_table(*args, sycl_queue=None, sua_iface=None, xp=None):
+    return _apply_and_pass(
+        convert_one_from_table, *args, sycl_queue=sycl_queue, sua_iface=sua_iface, xp=xp
+    )
+
+
+def to_table(*args, sua_iface=None):
+    return _apply_and_pass(convert_one_to_table, *args, sua_iface=sua_iface)
