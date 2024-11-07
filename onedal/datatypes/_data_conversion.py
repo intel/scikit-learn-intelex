8e254692abff5bee24fc58e7ee7eab56e2401bba
Ian Faust <icfaust@gmail.com>
2024-11-07 23:46:43 +0100

    Update onedal/datatypes/_data_conversion.py
    Co-authored-by: Samir Nasibli <samir.nasibli@intel.com>

--- onedal/datatypes/_data_conversion.py
+++ onedal/datatypes/_data_conversion.py
@@ -36,7 +36,7 @@ def to_table(*args):
     """Create oneDAL tables from scalars and/or arrays.
 
     Note: this implementation can be used with contiguous scipy.sparse, numpy
-    arrays, sycl_usm_ndarrays, and scalars. Tables will use pointers to the
+    ndarrays, DPCTL/DPNP usm_ndarrays and scalars. Tables will use pointers to the
     original array data. Scalars will be copies. Arrays may be modified in-
     place by oneDAL during computation. This works for data located on CPU and
     SYCL-enabled Intel GPUs. Each array may only be of a single datatype (i.e.
