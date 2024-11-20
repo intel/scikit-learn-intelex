076ebc401b4e9fbd872f9f1bc971bad1eb095f32
Ian Faust <icfaust@gmail.com>
2024-11-20 17:19:15 +0100

    Update base.py

--- sklearnex/tests/utils/base.py
+++ sklearnex/tests/utils/base.py
@@ -36,6 +36,7 @@ from sklearn.utils.validation import check_is_fitted
 
 from onedal.datatypes import from_table, to_table
 from onedal.tests.utils._dataframes_support import _convert_to_dataframe
+from onedal.utils._array_api import _get_sycl_namespace
 from sklearnex import get_patch_map, patch_sklearn, sklearn_is_patched, unpatch_sklearn
 from sklearnex.basic_statistics import BasicStatistics, IncrementalBasicStatistics
 from sklearnex.linear_model import LogisticRegression
