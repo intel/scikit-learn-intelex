c2c23f9c70af27f3b331e1aa311c2fb719e8d0ae
Ian Faust <icfaust@gmail.com>
2024-11-23 14:09:50 +0100

    Update data_conversion.cpp

--- onedal/datatypes/data_conversion.cpp
+++ onedal/datatypes/data_conversion.cpp
@@ -155,18 +155,16 @@ dal::table convert_to_table(PyObject *obj) {
     }
     if (is_array(obj)) {
         PyArrayObject *ary = reinterpret_cast<PyArrayObject *>(obj);
-        if (array_is_behaved_C(ary) || array_is_behaved_F(ary)) {
+        if (!array_is_behaved_C(ary) && !array_is_behaved_F(ary)) {
+            // NOTE: this will make a C-contiguous deep copy of the data
+            ary = PyArray_GETCONTIGUOUS(ary);
+        }
 #define MAKE_HOMOGEN_TABLE(CType) res = convert_to_homogen_impl<CType>(ary);
-            SET_NPY_FEATURE(array_type(ary),
-                            array_type_sizeof(ary),
-                            MAKE_HOMOGEN_TABLE,
-                            throw std::invalid_argument("Found unsupported array type"));
+        SET_NPY_FEATURE(array_type(ary),
+                        array_type_sizeof(ary),
+                        MAKE_HOMOGEN_TABLE,
+                        throw std::invalid_argument("Found unsupported array type"));
 #undef MAKE_HOMOGEN_TABLE
-        }
-        else {
-            throw std::invalid_argument(
-                "[convert_to_table] Numpy input Could not convert Python object to onedal table.");
-        }
     }
     else if (strcmp(Py_TYPE(obj)->tp_name, "csr_matrix") == 0 || strcmp(Py_TYPE(obj)->tp_name, "csr_array") == 0) {
         PyObject *py_data = PyObject_GetAttrString(obj, "data");
