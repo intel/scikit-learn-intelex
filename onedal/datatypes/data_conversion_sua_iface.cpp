efce9e83eb283bf3e1d36336fa6a0d9f8e9ea2ea
Ian Faust <icfaust@gmail.com>
2024-11-23 16:30:56 +0100

    Update data_conversion_sua_iface.cpp

--- onedal/datatypes/data_conversion_sua_iface.cpp
+++ onedal/datatypes/data_conversion_sua_iface.cpp
@@ -68,7 +68,7 @@ dal::table convert_to_homogen_impl(py::object obj) {
 
     if (layout == dal::data_layout::unknown){
         py::object copy = obj.attr("copy")(); // get a contiguous copy
-        res = convert_to_homogen_impl(copy);
+        res = convert_to_homogen_impl<Type>(copy);
         copy.dec_ref();
         return res;
     }
