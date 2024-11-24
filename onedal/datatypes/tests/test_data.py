0f98a3f148577234dc104b7ee091026564bbeb74
Ian Faust <icfaust@gmail.com>
2024-11-24 09:04:18 +0100

    Update test_data.py

--- onedal/datatypes/tests/test_data.py
+++ onedal/datatypes/tests/test_data.py
@@ -384,7 +384,7 @@ def test_to_table_non_contiguous_input(dataframe, queue):
     # X expected to be non-contiguous.
     assert not X.flags.c_contiguous and not X.flags.f_contiguous
     X_t = to_table(X)
-    assert X_t and X_t.shape and X_t.has_data
+    assert X_t and X_t.shape == (10, 3) and X_t.has_data
 
 
 @pytest.mark.skipif(
