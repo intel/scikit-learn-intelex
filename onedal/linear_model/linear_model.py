282e0ac9e00749b4f5f814f323b00f7d6c081562
Faust, Ian <ian.faust@intel.com>
2024-12-04 11:20:53 +0100

    address 1d coefs

--- onedal/linear_model/linear_model.py
+++ onedal/linear_model/linear_model.py
@@ -61,7 +61,10 @@ class BaseLinearRegression(BaseEstimator, metaclass=ABCMeta):
         if np.isscalar(self.coef_):
             coefs = np.asarray(self.coef_).reshape(1, 1)
         else:
-            coefs = from_table(to_table(self.coef_))
+            ndim = self.coef_.ndim
+            # generalized atleast_2d for numpy and array_api inputs
+            coefs = self.coef_[*((2 - ndim) * [None])] if ndim < 2 else self.coef_
+            coefs = from_table(to_table(coefs))
         if np.isscalar(self.intercept_):
             intercept = np.asarray(self.intercept_).reshape(1, 1)
         else:
