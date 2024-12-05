ac6d8317f857fb3eaac8dac4ed29d93024cc9b78
Ian Faust <icfaust@gmail.com>
2024-12-05 19:26:05 +0100

    Update test_validation.py

--- sklearnex/utils/tests/test_validation.py
+++ sklearnex/utils/tests/test_validation.py
@@ -1,5 +1,5 @@
 # ==============================================================================
-# Copyright 2024 UXL Foundation Contributors
+# Copyright contributors to the oneDAL project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
