1dc7d41467bedf1b5d44f2a85733060618532f40
Faust, Ian <ian.faust@intel.com>
2024-12-10 15:29:05 +0100

    fix spmd

--- onedal/spmd/decomposition/incremental_pca.py
+++ onedal/spmd/decomposition/incremental_pca.py
@@ -16,7 +16,7 @@
 
 from daal4py.sklearn._utils import get_dtype
 
-from ...datatypes import _convert_to_supported, from_table, to_table
+from ...datatypes import from_table, to_table
 from ...decomposition import IncrementalPCA as base_IncrementalPCA
 from ...utils import _check_array
 from .._base import BaseEstimatorSPMD
@@ -77,13 +77,12 @@ class IncrementalPCA(BaseEstimatorSPMD, base_IncrementalPCA):
         self._queue = queue
 
         policy = super(base_IncrementalPCA, self)._get_policy(queue, X)
-        X = _convert_to_supported(policy, X)
+        X_table = to_table(X, queue=queue)
 
         if not hasattr(self, "_dtype"):
-            self._dtype = get_dtype(X)
-            self._params = self._get_onedal_params(X)
+            self._dtype = X_table.dtype
+            self._params = self._get_onedal_params(self._dtype)
 
-        X_table = to_table(X)
         self._partial_result = super(base_IncrementalPCA, self)._get_backend(
             "decomposition",
             "dim_reduction",
